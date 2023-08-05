cebc8b8db65780788f82a7964c9eb4ff12d5be4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:05 +0000

    Add package pyGenericSpreadSheet/0.0.15/pyGenericSpreadSheet-0.0.15.tar.gz

---
+++ packages/pyGenericSpreadSheet/pyGenericSpreadSheet-0.0.15.tar.gz/pyGenericSpreadSheet-0.0.15/spreadsheet/version.py
@@ -0,0 +1,17 @@
+'''
+Created on 2022-04-21
+
+@author: wf
+'''
+
+class Version(object):
+    '''
+    Version handling for pyGenericSpreadSheet
+    '''
+    version='0.0.15'
+    date = '2022-04-21'
+    updated = '2022-05-09'
+    name='pyGenericSpreadSheet'
+    description='python API providing generic Access to Spreadsheet backends'
+
+        
