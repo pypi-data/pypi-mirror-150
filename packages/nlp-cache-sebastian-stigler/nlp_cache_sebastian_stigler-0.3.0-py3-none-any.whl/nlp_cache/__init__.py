aaaac55e67499447325d056a120defa07596498e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:34 +0000

    Add package nlp-cache-sebastian-stigler/0.3.0/nlp_cache_sebastian_stigler-0.3.0-py3-none-any.whl

---
+++ packages/nlp-cache-sebastian-stigler/nlp_cache_sebastian_stigler-0.3.0-py3-none-any.whl/nlp_cache/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Sebastian Stigler"
+__copyright__ = "Copyright 2022, Sebastian Stigler"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "0.3.0"
+__maintainer__ = "Sebastian Stigler"
+__email__ = "sebastian.stigler@hs-aalen.de"
+__status__ = "Production"
+
+from .nlp_cache import NlpCache
