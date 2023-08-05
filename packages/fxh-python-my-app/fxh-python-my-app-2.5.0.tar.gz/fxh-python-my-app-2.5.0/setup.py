0241d01a432cfaad6d6b7010632cf4fb796c08e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:10 +0000

    Add package fxh-python-my-app/2.5.0/fxh-python-my-app-2.5.0.tar.gz

---
+++ packages/fxh-python-my-app/fxh-python-my-app-2.5.0.tar.gz/fxh-python-my-app-2.5.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fxh_python_my_app']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['loguru>=0.6.0,<0.7.0', 'pymongo>=4.1.1,<5.0.0', 'redis>=4.3.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'fxh-python-my-app',
+    'version': '2.5.0',
+    'description': '',
+    'long_description': None,
+    'author': 'juntao',
+    'author_email': '61410813@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
