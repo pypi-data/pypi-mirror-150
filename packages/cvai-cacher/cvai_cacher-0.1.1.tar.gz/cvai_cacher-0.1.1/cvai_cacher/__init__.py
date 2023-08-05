95a4adef3adae24de529443c0fc8571322f2d2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:10 +0000

    Add package cvai-cacher/0.1.1/cvai_cacher-0.1.1.tar.gz

---
+++ packages/cvai-cacher/cvai_cacher-0.1.1.tar.gz/cvai_cacher-0.1.1/cvai_cacher/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.1'
+
+from .caching import get_cache, set_cache
---
+++ packages/cvai-cacher/cvai_cacher-0.1.1.tar.gz/cvai_cacher-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cvai_cacher']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cvai-logger>=0.1.0,<0.2.0', 'redis>=4.3.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'cvai-cacher',
+    'version': '0.1.1',
+    'description': 'ClearVision AI cacher',
+    'long_description': None,
+    'author': 'Gautham Reddy',
+    'author_email': 'gauthamv93@yaho.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
