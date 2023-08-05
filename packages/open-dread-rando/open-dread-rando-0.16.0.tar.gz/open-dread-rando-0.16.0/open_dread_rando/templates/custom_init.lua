4de1485c73749cae2fc7472609f13a355718db37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:49 +0000

    Add package open-dread-rando/0.16.0/open-dread-rando-0.16.0.tar.gz

---
+++ packages/open-dread-rando/open-dread-rando-0.16.0.tar.gz/open-dread-rando-0.16.0/open_dread_rando/templates/custom_init.lua
@@ -0,0 +1,39 @@
+Game.ImportLibrary("system/scripts/init_original.lua")
+
+local initOk, errorMsg = pcall(function()
+
+Init.tNewGameInventory = TEMPLATE("new_game_inventory")
+
+Init.iNumRandoTextBoxes = TEMPLATE("textbox_count")
+Init.fEnergyPerTank = TEMPLATE("energy_per_tank")
+Init.fEnergyPerPart = TEMPLATE("energy_per_part")
+Init.bImmediateEnergyParts = TEMPLATE("immediate_energy_parts")
+Init.bDefaultXRelease = TEMPLATE("default_x_released")
+
+Game.LogWarn(0, "Inventory:")
+for k, v in pairs(Init.tNewGameInventory) do
+    Game.LogWarn(0, tostring(k) .. " = " .. tostring(v))
+end
+
+local buff = {}
+
+Init.sStartingScenario = TEMPLATE("starting_scenario")
+Init.sStartingActor = TEMPLATE("starting_actor")
+
+function Game.StartPrologue(arg1, arg2, arg3, arg4, arg5)
+    Game.LogWarn(0, string.format("Will start Game - %s / %s / %s / %s", tostring(arg1), tostring(arg2), tostring(arg3), tostring(arg4)))
+    Game.LoadScenario("c10_samus", Init.sStartingScenario, Init.sStartingActor, "", 1)
+end
+
+function Init.SaveGameAtStartingLocation()
+    Game.SaveGame("savedata", "IntroEnd", Init.sStartingActor, true)
+end
+
+Game.SetForceSkipCutscenes(true)
+Game.LogWarn(0, "Finished modded system/init.lc")
+
+end)
+if not initOk then
+    Game.LogWarn(0, "Init failed: " .. errorMsg)
+    GUI.ShowMessage("Init failed: " .. errorMsg, true, "")
+end
