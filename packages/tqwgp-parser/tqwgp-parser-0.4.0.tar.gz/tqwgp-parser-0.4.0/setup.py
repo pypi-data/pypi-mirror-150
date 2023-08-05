858161a196cc6162842edcec9a14edcc7590c6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:20:35 +0000

    Add package tqwgp-parser/0.4.0/tqwgp-parser-0.4.0.tar.gz

---
+++ packages/tqwgp-parser/tqwgp-parser-0.4.0.tar.gz/tqwgp-parser-0.4.0/setup.py
@@ -0,0 +1,31 @@
+import sys
+import re
+import ast
+from setuptools import setup
+
+readme_markdown = None
+with open("README.md") as f:
+    readme_markdown = f.read()
+
+setup(
+    name="tqwgp-parser",
+    version="0.4.0",
+    url="https://github.com/YtoTech/talk-quote-work-getpaid-parser",
+    license="AGPL-3.0",
+    author="Yoan Tournade",
+    author_email="y@yoantournade.com",
+    description="A library for parsing Talk Quote Work Get-Paid (TQWGP) text-based compliant sales and accounting documents.",
+    long_description=readme_markdown,
+    long_description_content_type="text/markdown",
+    # TODO console_scripts?
+    # https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html
+    scripts=["scripts/tqwgp"],
+    packages=["tqwgp_parser"],
+    include_package_data=True,
+    package_data={
+        "tqwgp_parser": ["*.hy"],
+    },
+    zip_safe=False,
+    platforms="any",
+    install_requires=["hy>=1.0a4", "toolz", "hyrule"],
+)
