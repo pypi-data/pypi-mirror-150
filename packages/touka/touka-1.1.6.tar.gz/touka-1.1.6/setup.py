6a8536ace02e268211b78878b040f09c49c2cab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:15 +0000

    Add package touka/1.1.6/touka-1.1.6.tar.gz

---
+++ packages/touka/touka-1.1.6.tar.gz/touka-1.1.6/setup.py
@@ -0,0 +1,55 @@
+import re
+from setuptools import setup
+
+
+requirements = [
+    "certifi==2021.10.8",
+    "charset-normalizer==2.0.12",
+    "idna==3.3",
+    "requests==2.27.1",
+    "urllib3==1.26.9"
+]
+
+version = ""
+with open("touka/__init__.py") as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError("Version is not set.")
+
+readme = ""
+with open("README.rst") as f:
+    readme = f.read()
+
+packages = [
+    "touka",
+    "touka.ext"
+]
+
+setup(
+    name="touka",
+    author="Black Reaper",
+    project_urls={
+        "Discord": "https://discord.gg/yzAJ3wg5S3"
+    },
+    version=version,
+    packages=packages,
+    license="MIT",
+    description="A Python wrapper for automation.",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    include_package_data=True,
+    install_requires=requirements,
+    python_requires=">=3.10.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Other Audience",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Home Automation",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ]
+)
---
+++ packages/touka/touka-1.1.6.tar.gz/touka-1.1.6/touka/__init__.py
@@ -0,0 +1,6 @@
+__title__ = "touka"
+__author__ = "Black Reaper"
+__license__ = "MIT"
+__version__ = "1.1.6"
+
+from .ext import *
