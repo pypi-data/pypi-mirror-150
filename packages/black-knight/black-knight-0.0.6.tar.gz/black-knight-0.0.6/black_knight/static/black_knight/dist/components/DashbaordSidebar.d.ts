1a9c3964c87b33d975e406b3ba2479df4b3ff7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:24 +0000

    Add package black-knight/0.0.6/black-knight-0.0.6.tar.gz

---
+++ packages/black-knight/black-knight-0.0.6.tar.gz/black-knight-0.0.6/black_knight/static/black_knight/dist/components/DashbaordSidebar.d.ts
@@ -0,0 +1,4 @@
+import './style/sidebar.scss';
+declare const DashboardSidebar: () => JSX.Element;
+export default DashboardSidebar;
+//# sourceMappingURL=DashbaordSidebar.d.ts.map
---
+++ packages/black-knight/black-knight-0.0.6.tar.gz/black-knight-0.0.6/black_knight/static/black_knight/dist/pages/Dashboard.d.ts
@@ -0,0 +1,4 @@
+import './style/dashboard.scss';
+declare const Dashboard: () => JSX.Element;
+export default Dashboard;
+//# sourceMappingURL=Dashboard.d.ts.map
