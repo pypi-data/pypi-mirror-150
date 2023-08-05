bfeda01edfb5b515bed7afb37f7d76db0fb14491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:52 +0000

    Add package nautobot-golden-config/1.0.2/nautobot_golden_config-1.0.2-py3-none-any.whl

---
+++ packages/nautobot-golden-config/nautobot_golden_config-1.0.2-py3-none-any.whl/nautobot_golden_config/__init__.py
@@ -0,0 +1,32 @@
+"""Plugin declaration for nautobot_golden_config."""
+
+__version__ = "1.0.0"
+
+from nautobot.extras.plugins import PluginConfig
+
+
+class GoldenConfig(PluginConfig):
+    """Plugin configuration for the nautobot_golden_config plugin."""
+
+    name = "nautobot_golden_config"
+    verbose_name = "Golden Configuration"
+    version = __version__
+    author = "Network to Code, LLC"
+    author_email = "opensource@networktocode.com"
+    description = "A plugin for managing Golden Configurations."
+    base_url = "golden-config"
+    min_version = "1.2.0"
+    max_version = "1.3.99"
+    default_settings = {
+        "enable_backup": True,
+        "enable_compliance": True,
+        "enable_intended": True,
+        "enable_sotagg": True,
+        "per_feature_bar_width": 0.3,
+        "per_feature_width": 13,
+        "per_feature_height": 4,
+        "get_custom_compliance": None,
+    }
+
+
+config = GoldenConfig  # pylint:disable=invalid-name
