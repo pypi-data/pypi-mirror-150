02fe8d32e30453355decf302b6e8bb931a849354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:52 +0000

    Add package printrevolution/0.1.1/PrintRevolution-0.1.1.tar.gz

---
+++ packages/printrevolution/PrintRevolution-0.1.1.tar.gz/PrintRevolution-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['printrevolution']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['revolution = printrevolution:main']}
+
+setup_kwargs = {
+    'name': 'printrevolution',
+    'version': '0.1.1',
+    'description': 'A soothing meditation on politics.',
+    'long_description': None,
+    'author': 'Sam Lavigne',
+    'author_email': 'splavigne@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
