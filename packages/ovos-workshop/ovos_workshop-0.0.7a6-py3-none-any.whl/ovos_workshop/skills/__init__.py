a0afc938921e22f13057ec67f9ce73042c26320c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:27 +0000

    Add package ovos-workshop/0.0.7a6/ovos_workshop-0.0.7a6-py3-none-any.whl

---
+++ packages/ovos-workshop/ovos_workshop-0.0.7a6-py3-none-any.whl/ovos_workshop/skills/__init__.py
@@ -0,0 +1,8 @@
+import inspect
+
+
+from ovos_workshop.skills.ovos import MycroftSkill, OVOSSkill, \
+    OVOSFallbackSkill
+from ovos_workshop.skills.idle_display_skill import IdleDisplaySkill
+from ovos_workshop.skills.layers import IntentLayers
+
