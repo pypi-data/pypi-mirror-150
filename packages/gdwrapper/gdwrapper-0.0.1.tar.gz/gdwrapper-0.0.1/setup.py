ba089505c66263ccc7e9d0f0688a831fd5ad3228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:29 +0000

    Add package gdwrapper/0.0.1/gdwrapper-0.0.1.tar.gz

---
+++ packages/gdwrapper/gdwrapper-0.0.1.tar.gz/gdwrapper-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import pathlib
+from setuptools import setup
+HERE = pathlib.Path(__file__).parent
+setup(
+  name="gdwrapper",
+  version="0.0.1",
+  description="A Python API for Geometry Dash",
+  author="Rylixmods SFC",
+  author_email="rylixmods@gmail.com",
+  license="MIT",
+  packages=["gdwrapper"],
+  install_requires=['requests'],
+  zip_safe=False
+)
