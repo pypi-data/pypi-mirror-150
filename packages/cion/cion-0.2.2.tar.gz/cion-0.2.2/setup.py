83e87ba316373f07e07f3f16225d1eaba98a6151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:12 +0000

    Add package cion/0.2.2/cion-0.2.2.tar.gz

---
+++ packages/cion/cion-0.2.2.tar.gz/cion-0.2.2/setup.py
@@ -0,0 +1,59 @@
+import re
+
+from setuptools import find_packages, setup
+
+with open("cion/__init__.py") as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="cion",
+    description="A data validation library",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="meizuflux",
+    project_urls={
+        "Documentation": "https://cionpy.readthedocs.io",
+        "Issue Tracker": "https://github.com/meizuflux/cion/issues",
+        "Source Code": "https://github.com/meizuflux/cion",
+    },
+    license="MIT",
+    url="https://github.com/meizuflux/cion",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Typing :: Typed",
+    ],
+    version=version,
+    install_requires=[],
+    extras_require={
+        "docs": [
+            "sphinx",
+            "sphinx-copybutton",
+        ],
+        "lint": [
+            "pyright",
+        ],
+        "build": [
+            "setuptools",
+            "wheel",
+            "build",
+            "twine",
+        ],
+        "format": [
+            "black",
+            "isort",
+        ],
+        "test": [
+            "pytest",
+            "coverage[toml]",
+            "pytest-cov",
+        ],
+    },
+    python_requires=">=3.10",
+)
