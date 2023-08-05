f632229bafa16bf55f399aef65f5dd36253f8983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:58 +0000

    Add package getting-and-setting/0.3.5/getting_and_setting-0.3.5-py3-none-any.whl

---
+++ packages/getting-and-setting/getting_and_setting-0.3.5-py3-none-any.whl/getting_and_setting/mi_cli_meta.py
@@ -0,0 +1,40 @@
+import typer
+
+from typing import List
+
+from . import MiApi
+
+from . mi_endpoints import mi_endpoint_get
+
+app = typer.Typer(help='Fetch metadata')
+
+
+@app.command()
+def transaction(endpoint: str, program: str, transaction: str):
+    endpoint = mi_endpoint_get(endpoint)
+    
+    api = MiApi(endpoint.host, endpoint.port, endpoint.usr, endpoint.pwd)
+    records = api.transaction_meta(program, transaction)
+
+    typer.echo(records)
+
+
+@app.command()
+def program(endpoint: str, program: str):
+    endpoint = mi_endpoint_get(endpoint)
+    
+    api = MiApi(endpoint.host, endpoint.port, endpoint.usr, endpoint.pwd)
+    records = api.program_meta(program)
+
+    typer.echo(records)
+
+
+
+@app.command()
+def list(endpoint: str):
+    endpoint = mi_endpoint_get(endpoint)
+    
+    api = MiApi(endpoint.host, endpoint.port, endpoint.usr, endpoint.pwd)
+    records = api.program_list()
+
+    typer.echo(records)
