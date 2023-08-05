83dc8d0e824cd706888c2de1daf01c820d4c7379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:20:34 +0000

    Add package ape-etherscan/0.2.1/ape_etherscan-0.2.1-py3-none-any.whl

---
+++ packages/ape-etherscan/ape_etherscan-0.2.1-py3-none-any.whl/ape_etherscan/__init__.py
@@ -0,0 +1,25 @@
+from ape import plugins
+
+from .explorer import Etherscan
+
+NETWORKS = {
+    "ethereum": [
+        "mainnet",
+        "ropsten",
+        "rinkeby",
+        "kovan",
+        "goerli",
+    ],
+    "fantom": [
+        "opera",
+        "testnet",
+    ],
+}
+
+
+@plugins.register(plugins.ExplorerPlugin)
+def explorers():
+    for ecosystem_name in NETWORKS:
+        for network_name in NETWORKS[ecosystem_name]:
+            yield ecosystem_name, network_name, Etherscan
+            yield ecosystem_name, f"{network_name}-fork", Etherscan
