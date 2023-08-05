1dcba3c48834343df670afbb51b0d541a44b5ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:13 +0000

    Add package gto/0.1.7/gto-0.1.7-py3-none-any.whl

---
+++ packages/gto/gto-0.1.7-py3-none-any.whl/gto/constants.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+REF = "ref"
+# COMMIT = "commit"
+TAG = "tag"
+# BRANCH = "branch"
+# FILE = "file"
+
+ACTION = "action"
+TYPE = "type"
+NAME = "name"
+PATH = "path"
+VERSION = "version"
+STAGE = "stage"
+NUMBER = "number"
+
+
+class Action(Enum):
+    REGISTER = "register"
+    PROMOTE = "promote"
