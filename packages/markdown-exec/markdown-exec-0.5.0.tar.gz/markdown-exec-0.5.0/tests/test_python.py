152dc3111e158fe6b552385bc1e065cc87d9acaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package markdown-exec/0.5.0/markdown-exec-0.5.0.tar.gz

---
+++ packages/markdown-exec/markdown-exec-0.5.0.tar.gz/markdown-exec-0.5.0/tests/test_python.py
@@ -0,0 +1,41 @@
+"""Tests for the `python` module."""
+
+from textwrap import dedent
+
+from markdown import Markdown
+
+
+def test_output_markdown(md: Markdown) -> None:
+    """Assert Markdown is converted to HTML.
+
+    Parameters:
+        md: A Markdown instance (fixture).
+    """
+    html = md.convert(
+        dedent(
+            """
+            ```python exec="yes"
+            print("**Bold!**")
+            ```
+            """
+        )
+    )
+    assert html == "<p><strong>Bold!</strong></p>"
+
+
+def test_output_html(md: Markdown) -> None:
+    """Assert HTML is injected as is.
+
+    Parameters:
+        md: A Markdown instance (fixture).
+    """
+    html = md.convert(
+        dedent(
+            """
+            ```python exec="yes" html="yes"
+            print("**Bold!**")
+            ```
+            """
+        )
+    )
+    assert html == "<p>**Bold!**\n</p>"
