d1f45bbfa78894203e69a9236186a67b2c9d8c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package markdown-exec/0.5.0/markdown_exec-0.5.0-py3-none-any.whl

---
+++ packages/markdown-exec/markdown_exec-0.5.0-py3-none-any.whl/markdown_exec/formatters/pycon.py
@@ -0,0 +1,44 @@
+"""Formatter for executing `pycon` code."""
+
+from __future__ import annotations
+
+import textwrap
+from typing import Any
+from uuid import uuid4
+
+from markdown.core import Markdown
+
+from markdown_exec.formatters.python import _run_python  # noqa: WPS450
+from markdown_exec.rendering import add_source, code_block, markdown
+
+
+def _format_pycon(  # noqa: WPS231
+    code: str,
+    md: Markdown,
+    html: bool,
+    source: str,
+    result: str,
+    tabs: tuple[str, str],
+    **options: Any,
+) -> str:
+    markdown.setup(md)
+
+    python_lines = []
+    for line in code.split("\n"):
+        if line.startswith(">>> "):
+            python_lines.append(line[4:])
+    python_code = "\n".join(python_lines)
+
+    extra = options.get("extra", {})
+    output = _run_python(python_code, **extra)
+    stash = {}
+    if html:
+        placeholder = str(uuid4())
+        stash[placeholder] = output
+        output = placeholder
+    elif result:
+        output = code_block(result, output)
+    if source:
+        source_code = textwrap.indent(python_code, ">>> ")
+        output = add_source(source=source_code, location=source, output=output, language="pycon", tabs=tabs, **extra)
+    return markdown.convert(output, stash=stash)
