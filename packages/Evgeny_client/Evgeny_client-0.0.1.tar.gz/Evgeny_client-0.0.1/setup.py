5776bc9ff25947e599e6a04f4fac93113fb310dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:36 +0000

    Add package Evgeny_client/0.0.1/Evgeny_client-0.0.1.tar.gz

---
+++ packages/Evgeny_client/Evgeny_client-0.0.1.tar.gz/Evgeny_client-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(name="Evgeny_client",
+      version="0.0.1",
+      description="mess_client",
+      author="Evgeny Monakhov",
+      author_email="iv.iv@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
