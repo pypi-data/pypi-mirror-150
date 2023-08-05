0656694b865be65c88a9ecdbb51aa0242370e9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:13 +0000

    Add package fitconnect/0.4.2/fitconnect-0.4.2.tar.gz

---
+++ packages/fitconnect/fitconnect-0.4.2.tar.gz/fitconnect-0.4.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fitconnect']
+
+package_data = \
+{'': ['*'], 'fitconnect': ['schema/*']}
+
+install_requires = \
+['jsonschema>=4.4.0,<5.0.0',
+ 'jwcrypto>=1.1,<2.0',
+ 'requests>=2.26.0,<3.0.0',
+ 'semver>=3.0.0.dev3,<4.0.0',
+ 'strictyaml>=1.6.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'fitconnect',
+    'version': '0.4.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
