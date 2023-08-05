55c605dadda6ee72d8ba204fac627c638d18e513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:23 +0000

    Add package JadeMath/2.0.11/JadeMath-2.0.11-py3-none-any.whl

---
+++ packages/JadeMath/JadeMath-2.0.11-py3-none-any.whl/JadeMath/__init__.py
@@ -0,0 +1,4 @@
+from .surfarea import cylinder, trapizoid, irregular_prism, rect_prism, cones, pyramids
+from .math import add, acceleration, subtract, divide, product, velocity
+from .volume import cube_volume, cuboid_volume, cylinder_volume, sphere_volume, sq_pyramid_volume, prism_volume, pyramid_volume
+from .random import pi, e, f_to_c, c_to_f
