53c8b6584dbf9558a5b0a2627658bfc5144f377f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:07 +0000

    Add package python-sources/0.0.1/python-sources-0.0.1.tar.gz

---
+++ packages/python-sources/python-sources-0.0.1.tar.gz/python-sources-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='python-sources',
+    versioning='dev',
+    setup_requires='setupmeta',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    include_package_data=True,
+    license='MIT',
+)
