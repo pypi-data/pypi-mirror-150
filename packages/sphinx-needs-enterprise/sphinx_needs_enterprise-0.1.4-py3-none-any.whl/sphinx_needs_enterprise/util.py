c3c83d78df8a194fee855bc121e4ce700fcca3db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:58 +0000

    Add package sphinx-needs-enterprise/0.1.4/sphinx_needs_enterprise-0.1.4-py3-none-any.whl

---
+++ packages/sphinx-needs-enterprise/sphinx_needs_enterprise-0.1.4-py3-none-any.whl/sphinx_needs_enterprise/util.py
@@ -0,0 +1,27 @@
+import logging
+import operator
+from functools import reduce  # forward compatibility for Python 3
+
+log = logging.getLogger(__name__)
+
+
+def dict_get(root, items, default=None):
+    """
+    Access a nested object in root by item sequence.
+
+    Usage::
+       data = {"nested": {"a_list": [{"finally": "target_data"}]}}
+       value = dict_get(["nested", "a_list", 0, "finally"], "Not_found")
+
+    """
+    try:
+        value = reduce(operator.getitem, items, root)
+    except (KeyError, IndexError, TypeError) as e:
+        log.debug(e)
+        return default
+    return value
+
+
+def dict_undefined_set(dict_obj, key, value):
+    if key not in dict_obj:
+        dict_obj[key] = value
