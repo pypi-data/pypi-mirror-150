d799a3f5a5ab7506e115860e31a05a5e240c6001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:18 +0000

    Add package bolton-clack/0.3.4/bolton-clack-0.3.4.tar.gz

---
+++ packages/bolton-clack/bolton-clack-0.3.4.tar.gz/bolton-clack-0.3.4/tests/conftest.py
@@ -0,0 +1,39 @@
+"""This file contains shared fixtures and pytest hooks.
+
+https://docs.pytest.org/en/6.2.x/fixture.html#conftest-py-sharing-fixtures-across-multiple-files
+"""
+
+import logging
+from typing import Iterator
+
+from freezegun import freeze_time
+from pytest import fixture
+import structlog
+
+
+pytest_plugins = ["clack.pytest_plugin", "logrus.pytest_plugin"]
+
+
+@fixture(autouse=True, scope="session")
+def frozen_time() -> Iterator[None]:
+    """Freeze time until our tests are done running."""
+    with freeze_time("2021-09-06T15:45:03.585481Z"):
+        yield
+
+
+@fixture(autouse=True)
+def clear_loggers() -> None:
+    """Remove handlers from all loggers and unconfigure structlog.
+
+    See https://github.com/pytest-dev/pytest/issues/5502 for an explanation on
+    why we need this fixture.
+    """
+    loggers = [logging.getLogger()] + list(
+        logging.Logger.manager.loggerDict.values()  # type: ignore[arg-type]
+    )
+    for logger in loggers:
+        handlers = getattr(logger, "handlers", [])
+        for handler in handlers:
+            logger.removeHandler(handler)
+
+    structlog.reset_defaults()
