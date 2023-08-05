6f6d966b26322bd94509d458656a61056198d78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:00 +0000

    Add package upswingutil/2.2.28/upswingutil-2.2.28.tar.gz

---
+++ packages/upswingutil/upswingutil-2.2.28.tar.gz/upswingutil-2.2.28/upswingutil/resource/dataflow_job.py
@@ -0,0 +1,14 @@
+from googleapiclient.discovery import build
+
+def create_dataflow_job_for_sending_emails(jobname: str, parameters: dict):
+    dataflow = build('dataflow', 'v1b3')
+    request = dataflow.projects().templates().launch(
+        projectId=ul.G_CLOUD_PROJECT,
+        gcsPath="gs://dataflow-content/Communication/EmailTrigger/templates/email_trigger",
+        body={
+            'jobName': jobname,
+            'parameters': parameters,
+        }
+    )
+    response = request.execute()
+    return response
