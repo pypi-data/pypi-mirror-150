f7ecc0f415564ba9a5f0aba503e22da4628bbbf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:19:08 +0000

    Add package polygon-api-client/1.0.0b11/polygon_api_client-1.0.0b11-py3-none-any.whl

---
+++ packages/polygon-api-client/polygon_api_client-1.0.0b11-py3-none-any.whl/polygon/rest/models/dividends.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from .common import DividendType, Frequency
+from dataclasses import dataclass
+
+
+@dataclass
+class Dividend:
+    "Dividend contains data for a historical cash dividend, including the ticker symbol, declaration date, ex-dividend date, record date, pay date, frequency, and amount."
+    cash_amount: Optional[float] = None
+    declaration_date: Optional[str] = None
+    dividend_type: Optional[DividendType] = None
+    ex_dividend_date: Optional[str] = None
+    frequency: Optional[Frequency] = None
+    pay_date: Optional[str] = None
+    record_date: Optional[str] = None
+    ticker: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d):
+        return Dividend(**d)
