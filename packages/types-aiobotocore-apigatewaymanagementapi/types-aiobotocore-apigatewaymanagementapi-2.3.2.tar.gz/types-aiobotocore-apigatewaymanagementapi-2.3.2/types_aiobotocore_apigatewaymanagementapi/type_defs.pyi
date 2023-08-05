da917b792a59b334f62ebcfe0f8b39ad3db291c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:54 +0000

    Add package types-aiobotocore-apigatewaymanagementapi/2.3.2/types-aiobotocore-apigatewaymanagementapi-2.3.2.tar.gz

---
+++ packages/types-aiobotocore-apigatewaymanagementapi/types-aiobotocore-apigatewaymanagementapi-2.3.2.tar.gz/types-aiobotocore-apigatewaymanagementapi-2.3.2/types_aiobotocore_apigatewaymanagementapi/type_defs.pyi
@@ -0,0 +1,83 @@
+"""
+Type annotations for apigatewaymanagementapi service type definitions.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/type_defs/)
+
+Usage::
+
+    ```python
+    from types_aiobotocore_apigatewaymanagementapi.type_defs import DeleteConnectionRequestRequestTypeDef
+
+    data: DeleteConnectionRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import IO, Any, Dict, Union
+
+from botocore.response import StreamingBody
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "DeleteConnectionRequestRequestTypeDef",
+    "GetConnectionRequestRequestTypeDef",
+    "GetConnectionResponseTypeDef",
+    "IdentityTypeDef",
+    "PostToConnectionRequestRequestTypeDef",
+    "ResponseMetadataTypeDef",
+)
+
+DeleteConnectionRequestRequestTypeDef = TypedDict(
+    "DeleteConnectionRequestRequestTypeDef",
+    {
+        "ConnectionId": str,
+    },
+)
+
+GetConnectionRequestRequestTypeDef = TypedDict(
+    "GetConnectionRequestRequestTypeDef",
+    {
+        "ConnectionId": str,
+    },
+)
+
+GetConnectionResponseTypeDef = TypedDict(
+    "GetConnectionResponseTypeDef",
+    {
+        "ConnectedAt": datetime,
+        "Identity": "IdentityTypeDef",
+        "LastActiveAt": datetime,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+IdentityTypeDef = TypedDict(
+    "IdentityTypeDef",
+    {
+        "SourceIp": str,
+        "UserAgent": str,
+    },
+)
+
+PostToConnectionRequestRequestTypeDef = TypedDict(
+    "PostToConnectionRequestRequestTypeDef",
+    {
+        "Data": Union[str, bytes, IO[Any], StreamingBody],
+        "ConnectionId": str,
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
