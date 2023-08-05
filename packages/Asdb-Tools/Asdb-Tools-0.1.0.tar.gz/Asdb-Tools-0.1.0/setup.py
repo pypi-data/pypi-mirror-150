3520c458f700a39a17f975748a2e0822ce04c554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:54 +0000

    Add package Asdb-Tools/0.1.0/Asdb-Tools-0.1.0.tar.gz

---
+++ packages/Asdb-Tools/Asdb-Tools-0.1.0.tar.gz/Asdb-Tools-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='Asdb-Tools',
+      version='0.1.0',
+      description='Tools For A-Soul Database',
+      author='Ava Bella Carol Diana Eileen',
+      author_email='dsoo9dbdi@mozmail.com',
+      url='https://github.com/A-Soul-Database/Asdb-Tools',
+      packages=setuptools.find_packages() 
+      )
