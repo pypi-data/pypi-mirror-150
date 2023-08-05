e61651eb522d3026378b321eba7668e03f195060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:19:35 +0000

    Add package jina-hubble-sdk/0.1.3.dev18/jina-hubble-sdk-0.1.3.dev18.tar.gz

---
+++ packages/jina-hubble-sdk/jina-hubble-sdk-0.1.3.dev18.tar.gz/jina-hubble-sdk-0.1.3.dev18/hubble/__init__.py
@@ -0,0 +1,19 @@
+"""
+The Hubble Python Client
+"""
+import asyncio
+
+from .client.client import Client  # noqa F401
+from .utils.auth import Auth  # noqa F401
+
+__version__ = '0.1.3.dev18'
+
+
+def login():
+    """This function guide user to browser log-in and get token."""
+    asyncio.run(Auth.login())
+
+
+def logout():
+    """Logout."""
+    asyncio.run(Auth.logout())
