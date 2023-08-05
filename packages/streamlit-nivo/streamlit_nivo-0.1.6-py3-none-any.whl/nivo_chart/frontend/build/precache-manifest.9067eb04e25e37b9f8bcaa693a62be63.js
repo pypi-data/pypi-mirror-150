062b69c4cbdaac327904a472ea220b03b51ccfa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:07 +0000

    Add package streamlit-nivo/0.1.6/streamlit_nivo-0.1.6-py3-none-any.whl

---
+++ packages/streamlit-nivo/streamlit_nivo-0.1.6-py3-none-any.whl/nivo_chart/frontend/build/precache-manifest.9067eb04e25e37b9f8bcaa693a62be63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31bac2a0c11f997945aa576a3b4d1a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58f2b54d266dbbe5361c",
+    "url": "./static/js/2.012d4b93.chunk.js"
+  },
+  {
+    "revision": "7ed0796b9c778b5589dc13179d486c0d",
+    "url": "./static/js/2.012d4b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd6d7e312e34fff454d",
+    "url": "./static/js/main.61b02fd9.chunk.js"
+  },
+  {
+    "revision": "e93542ed8997dbfd1c74",
+    "url": "./static/js/runtime-main.e0573c31.js"
+  }
+]);
