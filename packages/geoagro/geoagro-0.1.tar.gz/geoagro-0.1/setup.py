e8f7b783ff4964dbb0a28b21ad98cd96a3895302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:36 +0000

    Add package geoagro/0.1/geoagro-0.1.tar.gz

---
+++ packages/geoagro/geoagro-0.1.tar.gz/geoagro-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geoagro',
+      version='0.01',
+      description='A package to work with Geoagro tools',
+      url='https://github.com/GeoagrobyTEK/geoagro_package',
+      author='Fernando Tentor',
+      author_email='fernandotentor@gmail.com',
+      license='MIT',
+      packages=['geoagro_package'],
+      zip_safe=False)
