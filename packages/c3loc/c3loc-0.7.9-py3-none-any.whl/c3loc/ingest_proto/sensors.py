c5c62d01b07e4e1142def018acec58165b79257f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:15 +0000

    Add package c3loc/0.7.9/c3loc-0.7.9-py3-none-any.whl

---
+++ packages/c3loc/c3loc-0.7.9-py3-none-any.whl/c3loc/ingest_proto/sensors.py
@@ -0,0 +1,41 @@
+import attr
+import typing
+
+
+@attr.s(frozen=True)
+class SensorField:
+    tag: int = attr.ib()
+    length: int = attr.ib()
+    value: bytes = attr.ib()
+
+    @classmethod
+    def from_bytes(cls, data):
+        if len(data) < 2:
+            return None
+        tag = data[0]
+        length = data[1]
+        if length > len(data):
+            return None
+        return cls(tag, length, data[2:2+length])
+
+    def size(self):
+        return self.length + 2
+
+
+@attr.s(frozen=True)
+class SensorReport:
+    fields: typing.List[SensorField] = attr.ib(default=[])
+
+    @classmethod
+    def parse(cls, data):
+        fields = []
+        if len(data) < 2:
+            sensor = cls()
+            return sensor
+        while data:
+            field = SensorField.from_bytes(data)
+            if not field:
+                break
+            fields.append(field)
+            data = data[field.size():]
+        return cls(fields)
