3d569be6faefc82658a25fafe3835688f5019b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:44 +0000

    Add package opteryx/0.0.1/opteryx-0.0.1-cp39-cp39-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.0.1-cp39-cp39-win_amd64.whl/opteryx/storage/schemes/default_partitions.py
@@ -0,0 +1,17 @@
+from typing import Tuple, Union
+
+from opteryx.storage import BasePartitionScheme
+
+
+class DefaultPartitionScheme(BasePartitionScheme):
+    def __init__(self, format: Union[Tuple, str]):
+        if not isinstance(format, (list, set, tuple)):
+            self._format = [format]
+        else:
+            self._format = format  # type:ignore
+
+    def partition_format(self):
+        return "/".join(self._format)
+
+    def filter_blobs(self, list_of_blobs):
+        return list_of_blobs
