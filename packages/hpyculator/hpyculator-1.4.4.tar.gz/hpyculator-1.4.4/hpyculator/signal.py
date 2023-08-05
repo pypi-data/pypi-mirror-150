4723e63b0b408d2500f372cc5ab231b60e998fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:01 +0000

    Add package hpyculator/1.4.4/hpyculator-1.4.4.tar.gz

---
+++ packages/hpyculator/hpyculator-1.4.4.tar.gz/hpyculator-1.4.4/hpyculator/signal.py
@@ -0,0 +1,18 @@
+from PySide6.QtCore import Signal, QObject
+
+
+class MainWindowSignal(QObject):
+    setOutPutBox = Signal(str)
+    clearOutPutBox = Signal()
+    appendOutPutBox = Signal(str)
+
+    setStartButtonText = Signal(str)
+    setStartButtonState = Signal(bool)
+
+    setOutPutBoxCursor = Signal(str)
+
+class SettingWindowSignal(QObject):
+    pass
+
+main_window_signal = MainWindowSignal()
+setting_window_signal = SettingWindowSignal()
