f8b1f6ee9b3f91526db2b215814c9086c5ea7c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:55 +0000

    Add package tfdatamap/1.0.9/tfdatamap-1.0.9.tar.gz

---
+++ packages/tfdatamap/tfdatamap-1.0.9.tar.gz/tfdatamap-1.0.9/README.md
@@ -0,0 +1,4 @@
+# TFDatamap
+
+
+An implementation of ["Dataset Cartography: Mapping and Diagnosing Datasets with Training Dynamics"](https://arxiv.org/abs/2009.10795)
---
+++ packages/tfdatamap/tfdatamap-1.0.9.tar.gz/tfdatamap-1.0.9/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+import distutils.text_file
+from pathlib import Path
+from typing import List
+
+def _parse_requirements(filename: str) -> List[str]:
+    """Return requirements from requirements file."""
+    # Ref: https://stackoverflow.com/a/42033122/
+    return distutils.text_file.TextFile(filename=str(Path(__file__).with_name(filename))).readlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tfdatamap",
+    version="1.0.9",
+    author="Simon L. B. Sørensen",
+    author_email="siso@mmmi.sdu.dk",
+    description="A implementation of the datamap shown in \"Dataset Cartography: Mapping and Diagnosing Datasets with Training Dynamics\"",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/sdurobotics/medical/datamap",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="",
+    python_requires='>=3.6',
+    license='MIT',
+    install_requires=_parse_requirements('requirements.txt'),
+    include_package_data=True,
+    package_data={'tfdatamap': ['tfdatamap/*.js']}
+)
+
+# python setup.py sdist bdist_wheel
+# python -m twine upload dist/*
+# python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
