cf4f8a1984f754c520c600d20aa0afed928376bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:19:49 +0000

    Add package tinamit-idm/0.2.2/tinamit-idm-0.2.2.tar.gz

---
+++ packages/tinamit-idm/tinamit-idm-0.2.2.tar.gz/tinamit-idm-0.2.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tinamit_idm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['trio>=0.19.0,<0.20.0']
+
+setup_kwargs = {
+    'name': 'tinamit-idm',
+    'version': '0.2.2',
+    'description': '',
+    'long_description': None,
+    'author': 'joelz575',
+    'author_email': 'joel.harms@mail.mcgill.ca',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
