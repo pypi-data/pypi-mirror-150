48f0acde5d316f20a5cbe3c150f9522258bea69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:20 +0000

    Add package mdolab-baseclasses/1.6.2/mdolab_baseclasses-1.6.2-py3-none-any.whl

---
+++ packages/mdolab-baseclasses/mdolab_baseclasses-1.6.2-py3-none-any.whl/baseclasses/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "1.6.2"
+
+from .problems import (
+    AeroProblem,
+    TransiProblem,
+    StructProblem,
+    AeroStructProblem,
+    MissionProblem,
+    MissionProfile,
+    MissionSegment,
+    WeightProblem,
+    FuelCase,
+    FluidProperties,
+    ICAOAtmosphere,
+    EngineProblem,
+    FieldPerformanceProblem,
+    LGProblem,
+)
+
+from .solvers import BaseSolver, AeroSolver
+
+from .utils import getPy3SafeString
+
+from .testing import BaseRegTest, getTol
