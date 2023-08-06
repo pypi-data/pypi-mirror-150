1e26b6c2164143d91b8180b18bfff20d5317180f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:55 +0000

    Add package excal/0.0.15/excal-0.0.15.tar.gz

---
+++ packages/excal/excal-0.0.15.tar.gz/excal-0.0.15/excal/output.py
@@ -0,0 +1,30 @@
+import sys
+import json
+from typing import List
+from excal.offence import Offence
+
+
+class Output():
+    """Output class. Will be extended by more output options."""
+    def __init__(self):
+        self.exeptions: List[Offence] = []
+        return
+
+    def addExeptioins(self, ex: List[Offence]):
+        self.exeptions.extend(ex)
+
+    def printError(self):
+        for e in self.exeptions:
+            print(f'{e.file}: {e.start_location.line}:{e.start_location.col} : {e.id} {e.message}', file=sys.stderr)
+
+    def printJson(self):
+        out = json.loads('{"issues":[]}')
+        for o in self.exeptions:
+            a = o.__dict__
+            a["start_location"] = o.start_location.__dict__
+            a["end_location"] = o.end_location.__dict__
+            a["severity"] = o.severity.name
+            a["type"] = o.type.name
+            out["issues"].append(a)
+
+        print(json.dumps(out, indent=2))
---
+++ packages/excal/excal-0.0.15.tar.gz/excal-0.0.15/excal/pluginManager.py
@@ -0,0 +1,54 @@
+from typing import Callable, Dict, List
+import pathlib
+import json
+import importlib
+import pkg_resources
+
+from excal import visitor
+
+
+class ModuleInterface:
+    """Represents a plugin interface. A plugin has a single register function."""
+
+    @staticmethod
+    def register(PluginManager) -> None:
+        """Register the necessary items in the game character factory."""
+
+
+def import_module(name: str) -> ModuleInterface:
+    """Imports a module given a name."""
+    return importlib.import_module(name)  # type: ignore
+
+
+class PluginManager():
+    """Class to find, register and load Plugins."""
+    def __init__(self):
+        self.plugins: Dict[str, "visitor.NodeVisitor"] = {}
+
+    def register(self, plugin_name: str, plugin_init_fun: Callable[[], "visitor.NodeVisitor"]) -> None:
+        try:
+            plugin: "visitor.NodeVisitor" = plugin_init_fun()
+            self.plugins[plugin_name] = plugin
+        except Exception:
+            print(f"error loading Plugin {plugin_name}")
+
+    def getPluginList(self) -> List["visitor.NodeVisitor"]:
+        retList: List[visitor.NodeVisitor] = []
+        for key, val in self.plugins.items():
+            retList.append(val)
+        return retList
+
+    def loadPlugins(self):
+        try:
+            plugin_file = open(f"{pathlib.Path(__file__).resolve().parent.parent.parent}/plugins.json")
+            plugin_data = json.load(plugin_file)["plugins"]
+            for plugin_file in plugin_data:
+                # print(f"plugin {plugin_file} loading")
+                plugin = import_module(plugin_file)
+                plugin.register(self)
+        except Exception:
+            pass
+
+        for entry_point in pkg_resources.iter_entry_points('excal_plugins'):
+            # print(f"plugin {entry_point.name} loading")
+            entry_point.load()(self)
