6126d47c5a71f38cd0aff988926c894e0adc3479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:06 +0000

    Add package pypitools/0.0.73/pypitools-0.0.73-py3-none-any.whl

---
+++ packages/pypitools/pypitools-0.0.73-py3-none-any.whl/pypitools/process_utils.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+from typing import List, Tuple
+
+from pypitools.utils import get_logger
+from pypitools.configs import ConfigOutput
+
+
+def check_call_collect(args: List[str]) -> Tuple[str, str]:
+    """
+    Run a process and check that it returns an OK return code
+    Gather any output and return it to the caller.
+    If the process returns an error then print the output and
+    the errors.
+    :param args:
+    """
+    logger = get_logger()
+    logger.debug("running %s", args)
+    if ConfigOutput.verbose:
+        print(f"running [{args}]..")
+    with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
+        (res_stdout, res_stderr) = process.communicate()
+        if process.returncode:
+            print(res_stdout.decode(), end="", file=sys.stdout)
+            print(res_stderr.decode(), end="", file=sys.stderr)
+            raise ValueError(
+                f"exit code from [{' '.join(args)}] was [{process.returncode}]"
+            )
+        return res_stdout.decode(), res_stderr.decode()
