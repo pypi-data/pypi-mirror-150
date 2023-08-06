33d00fbf81491446bdc4d59a2d8ed16c9cf205fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:47 +0000

    Add package cdk-datalake-constructs/0.0.61/cdk_datalake_constructs-0.0.61-py3-none-any.whl

---
+++ packages/cdk-datalake-constructs/cdk_datalake_constructs-0.0.61-py3-none-any.whl/cdk_datalake_constructs/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import aws_cdk.aws_glue_alpha._jsii
+import aws_cdk.aws_lambda_python_alpha._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@randyridgley/cdk-datalake-constructs",
+    "0.0.61",
+    __name__[0:-6],
+    "cdk-datalake-constructs@0.0.61.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
