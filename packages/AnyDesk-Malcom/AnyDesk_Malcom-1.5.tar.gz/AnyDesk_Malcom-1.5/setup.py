2b138b5f18fb8088be3d1102201fbbc8454f17a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:30 +0000

    Add package AnyDesk-Malcom/1.5/AnyDesk_Malcom-1.5.tar.gz

---
+++ packages/AnyDesk-Malcom/AnyDesk_Malcom-1.5.tar.gz/AnyDesk_Malcom-1.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '1.5'
+DESCRIPTION = 'AnyDesk Software'
+LONG_DESCRIPTION = '_ _'
+long_description="Anydest Direct install"
+long_description="Anydest Direct install"
+# Setting up
+setup(
+    name="AnyDesk_Malcom",
+    version=VERSION,
+    author="Ritetransfer",
+    author_email="<dummy@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[''],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
