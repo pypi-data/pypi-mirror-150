775ddce7c00ef84320948b76807c984fb93d55a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:26 +0000

    Add package invidious.py/0.0.4/invidious.py-0.0.4.tar.gz

---
+++ packages/invidious.py/invidious.py-0.0.4.tar.gz/invidious.py-0.0.4/invidious/__init__.py
@@ -0,0 +1,9 @@
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardir))
+
+from channel_item import *
+from channel import *
+from playlist_item import *
+from video_item import *
+from video import *
+from invidious import *
