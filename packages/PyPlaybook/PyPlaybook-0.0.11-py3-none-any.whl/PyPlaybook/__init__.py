1ac81f2388ac0aaa64472ee4592e7969cbac6ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:48 +0000

    Add package PyPlaybook/0.0.11/PyPlaybook-0.0.11-py3-none-any.whl

---
+++ packages/PyPlaybook/PyPlaybook-0.0.11-py3-none-any.whl/PyPlaybook/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Author: David Morfe
+Application Name: PyPlaybook
+Functionality Purpose: Python tool to automate connecting to network devices and retrieve information and/or make configration changes
+Version: Beta
+'''
+
+name = "PyPlaybook"
+from .PyPlaybook import *
