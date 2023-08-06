e476a335f3288cd0b437a8914dd27bd7c1ec46b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:34 +0000

    Add package PrettyPrintTree/1.11/PrettyPrintTree-1.11.tar.gz

---
+++ packages/PrettyPrintTree/PrettyPrintTree-1.11.tar.gz/PrettyPrintTree-1.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PrettyPrintTree',
+    version='1.11',
+    author="Aharon Sambol",
+    author_email='email@example.com',
+    py_modules=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/AharonSambol/PrettyPrintTree',
+    keywords=['tree', 'pretty', 'print', 'pretty-print', 'display'],
+    description='A tool to print trees to the console'
+)
