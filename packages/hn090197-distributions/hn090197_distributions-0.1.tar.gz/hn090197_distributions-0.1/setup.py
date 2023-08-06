4ef539f6925b366b8ed8c5bebbe642bee5422718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:03 +0000

    Add package hn090197-distributions/0.1/hn090197_distributions-0.1.tar.gz

---
+++ packages/hn090197-distributions/hn090197_distributions-0.1.tar.gz/hn090197_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hn090197_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hn090197_distributions'],
+      zip_safe=False)
