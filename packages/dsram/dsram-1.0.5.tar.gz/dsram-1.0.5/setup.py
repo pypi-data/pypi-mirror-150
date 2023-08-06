b648c3e0281be63dfec1136636a0d57061c27218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:22:22 +0000

    Add package dsram/1.0.5/dsram-1.0.5.tar.gz

---
+++ packages/dsram/dsram-1.0.5.tar.gz/dsram-1.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="dsram",
+    version="1.0.5",
+    author="Walter Haydock",
+    author_email="walter@deploy-securely.com",
+    description="A quantitative cyber risk management calculator",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Deploying-Securely/DSRAM",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=['License :: OSI Approved :: Apache Software License'],
+)
