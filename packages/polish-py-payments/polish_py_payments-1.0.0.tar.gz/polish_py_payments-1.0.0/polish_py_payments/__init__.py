ebc3cdeabc0afe46b79dddefd33222aa191c85fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:12 +0000

    Add package polish-py-payments/1.0.0/polish_py_payments-1.0.0.tar.gz

---
+++ packages/polish-py-payments/polish_py_payments-1.0.0.tar.gz/polish_py_payments-1.0.0/polish_py_payments/__init__.py
@@ -0,0 +1,11 @@
+from .hotpay import *
+from .microsms import *
+from .lvlup import *
+
+__title__ = "polish_py_payments"
+__summary__ = "Polish payment providers in python"
+__uri__ = "https://github.com/ivall/polish-py-payments"
+__version__ = "1.0.0"
+__author__ = "ivall"
+__license__ = "MIT License"
+__copyright__ = f"Copyright 2022 {__author__}"
