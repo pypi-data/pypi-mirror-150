78bab9c19bdc043704934a3f728de702710e6e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:41 +0000

    Add package PDF4Cat/0.2.7/PDF4Cat-0.2.7-py3-none-any.whl

---
+++ packages/PDF4Cat/PDF4Cat-0.2.7-py3-none-any.whl/PDF4Cat/tools.py
@@ -0,0 +1,28 @@
+import os
+
+from .cat import PDF4Cat
+
+class Tools(PDF4Cat):
+	def __init__(self, *args, **kwargs):
+		super(Tools, self).__init__(*args, **kwargs)
+		self.pdf = self.pdf_open(self.doc_file)
+
+	@PDF4Cat.run_in_subprocess
+	def extract_pages2pdf(self, 
+		output_pdf = None,
+		pages: list = []) -> None:
+
+		self.pdf.select(pages)
+		self.pdf.save(output_pdf)
+
+	@PDF4Cat.run_in_subprocess
+	def delete_pages2pdf(self, 
+		output_pdf = None,
+		pages: list = []) -> None:
+
+		self.pdf.delete_pages(pages)
+		self.pdf.save(output_pdf)
+
+
+
+# generator decorator & save2zip soon..
