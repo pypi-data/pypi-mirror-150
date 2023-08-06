165ac5ce6d0bc9105e6090e0bcecb96c104342bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:21 +0000

    Add package colaquant/0.0.1/colaquant-0.0.1.tar.gz

---
+++ packages/colaquant/colaquant-0.0.1.tar.gz/colaquant-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='colaquant',
+    version='0.0.1',
+    description='可乐的量化分析框架',
+    author='Cola Li',
+    author_email='Indigocola121@gmail.com',
+    url = 'https://pypi.org/help/',
+    packages=['colaquant']
+)
