4fcdd1c0cfab4aa8a54faf1e865323457ad778a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:27 +0000

    Add package jsonbourne/0.21.5/jsonbourne-0.21.5.tar.gz

---
+++ packages/jsonbourne/jsonbourne-0.21.5.tar.gz/jsonbourne-0.21.5/jsonbourne/types.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from jsonbourne.protocols import (
+    Dumpable,
+    JsonInterface,
+    JsonInterfaceProperty,
+    JsonInterfaceProtocol,
+)
+
+__all__ = (
+    "JsonInterface",
+    "JsonInterfaceProperty",
+    "JsonInterfaceProtocol",
+    "Dumpable",
+)
