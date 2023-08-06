da59bd6d6d85b822f9ed4281317448780c9d3c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:39 +0000

    Add package systembridgebackend/3.0.3.dev9/systembridgebackend-3.0.3.dev9-py3-none-any.whl

---
+++ packages/systembridgebackend/systembridgebackend-3.0.3.dev9-py3-none-any.whl/systembridgebackend/server/keyboard.py
@@ -0,0 +1,43 @@
+"""System Bridge: Server Handler - Keyboard"""
+from keyboard import press_and_release, write
+from sanic.request import Request
+from sanic.response import HTTPResponse, json
+
+
+def keyboard_keypress(key: str):
+    """Press a keyboard key"""
+    press_and_release(key)
+
+
+def keyboard_text(text: str):
+    """Type text"""
+    write(text)
+
+
+async def handler_keyboard(
+    request: Request,
+) -> HTTPResponse:
+    """Send a keyboard event."""
+    if "key" in request.json:
+        keyboard_keypress(request.json["key"])
+        return json(
+            {
+                "message": "Keypress sent",
+                "key": request.json["key"],
+            }
+        )
+    if "text" in request.json:
+        keyboard_text(request.json["text"])
+        return json(
+            {
+                "message": "Text sent",
+                "text": request.json["text"],
+            }
+        )
+
+    return json(
+        {
+            "message": "No key or text provided",
+        },
+        status=400,
+    )
