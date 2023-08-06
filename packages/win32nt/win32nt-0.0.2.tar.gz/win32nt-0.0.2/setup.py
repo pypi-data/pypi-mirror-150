e35b1822c564cde4a763d6296225f1c011dd9929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:26 +0000

    Add package win32nt/0.0.2/win32nt-0.0.2.tar.gz

---
+++ packages/win32nt/win32nt-0.0.2.tar.gz/win32nt-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="win32nt",
+    version="0.0.2",
+    author="Itzsten",
+    author_email="itzsten@gmail.com",
+    description="A pywin32 compatible library to interact with the WinAPI; specifically ntdll functions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Itzsten/win32nt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Education",
+        "Development Status :: 4 - Beta"
+    ],
+)
