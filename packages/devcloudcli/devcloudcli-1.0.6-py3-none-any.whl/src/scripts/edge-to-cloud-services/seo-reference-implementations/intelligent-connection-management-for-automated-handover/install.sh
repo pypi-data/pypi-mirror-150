1650509e054645c65a65fc65b1d1ef18988789e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:50 +0000

    Add package devcloudcli/1.0.6/devcloudcli-1.0.6-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.0.6-py3-none-any.whl/src/scripts/edge-to-cloud-services/seo-reference-implementations/intelligent-connection-management-for-automated-handover/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing intelligent-connection-management-for-automated-handover ..." 
+echo 54459efe-476d-4574-b1e2-aadb9482a78a | edgesoftware install intelligent-connection-management-for-automated-handover 6241499a9654a8f4bd360ec5
+~
+
---
+++ packages/devcloudcli/devcloudcli-1.0.6-py3-none-any.whl/src/scripts/edge-to-cloud-services/seo-reference-implementations/wireless-network-ready-pcb-defect-detection/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing intelligent-connection-management-for-automated-handover ..." 
+echo c259ebe1-e326-46e3-b2fd-1f95e20476ad | edgesoftware install wireless-network-ready-pcb-defect-detection 6221e257905e50fbc05dc793
+~
+
