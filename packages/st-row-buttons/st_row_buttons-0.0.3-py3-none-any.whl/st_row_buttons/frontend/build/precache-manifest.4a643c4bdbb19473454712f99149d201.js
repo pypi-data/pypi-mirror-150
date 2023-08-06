6d2552cd2555bf24d9014a9927ed636041144ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:07 +0000

    Add package st-row-buttons/0.0.3/st_row_buttons-0.0.3-py3-none-any.whl

---
+++ packages/st-row-buttons/st_row_buttons-0.0.3-py3-none-any.whl/st_row_buttons/frontend/build/precache-manifest.4a643c4bdbb19473454712f99149d201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7dcd906a6eef5dd1df96e25a057462",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9686d52fdab41a8d79bb",
+    "url": "./static/css/main.ce6fb195.chunk.css"
+  },
+  {
+    "revision": "37d571ab8ad824034bbe",
+    "url": "./static/js/2.efa84012.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.efa84012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9686d52fdab41a8d79bb",
+    "url": "./static/js/main.ea20abc8.chunk.js"
+  },
+  {
+    "revision": "804077cbf41d6649ffc7",
+    "url": "./static/js/runtime-main.12e05911.js"
+  }
+]);
