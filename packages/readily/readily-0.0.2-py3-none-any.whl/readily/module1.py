e7d624eb10d91e2fad896bc994814f48806fbd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:08 +0000

    Add package readily/0.0.2/readily-0.0.2-py3-none-any.whl

---
+++ packages/readily/readily-0.0.2-py3-none-any.whl/readily/module1.py
@@ -0,0 +1,6 @@
+from scholarly import scholarly
+
+def scholtest():
+#	search_query = scholarly.search_author('James Gregory, Manchester')
+#	scholarly.pprint(next(search_query))
+        print('test sucessful')
