7a5c93d547dba8a374729ab03649c496843b7790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:53 +0000

    Add package dlai-grader/0.2.0/dlai-grader-0.2.0.tar.gz

---
+++ packages/dlai-grader/dlai-grader-0.2.0.tar.gz/dlai-grader-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dlai-grader',
+    version='0.2.0',    
+    description='Grading utilities for DLAI courses',
+    url='https://github.com/https-deeplearning-ai/grader',
+    author='Andres Zarta',
+    author_email='andrezb5@gmail.com',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=['nbformat>=5.1.3',
+                      'jupytext>=1.13.0',                     
+                      ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
