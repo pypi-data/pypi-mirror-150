e854372c1c032870b264bdef52d763de5d4b1094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:18 +0000

    Add package threshold-elgamal/0.0.8/threshold_elgamal-0.0.8.tar.gz

---
+++ packages/threshold-elgamal/threshold_elgamal-0.0.8.tar.gz/threshold_elgamal-0.0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from codecs import open
+
+setup(
+    name='threshold_elgamal',
+    version='0.0.8',
+    long_description_content_type="text/markdown",
+    long_description=open('README.md', 'r').read(),
+    packages=['tests', 'threshold_elgamal'],
+    url='',
+    license='',
+    author='Katarina Boras',
+    author_email='katarina.boras2@outlook.com',
+    description='Threshold ElGamal cryptosystem.'
+)
