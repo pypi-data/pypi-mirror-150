3fd596804333eee7209f42a9875dbb58988d2fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:41 +0000

    Add package mynumpytestadd/0.0.2/mynumpytestadd-0.0.2.tar.gz

---
+++ packages/mynumpytestadd/mynumpytestadd-0.0.2.tar.gz/mynumpytestadd-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="mynumpytestadd", 
+        version=VERSION,
+        author="Sasan Sharifipour",
+        author_email="<sasansharifipour@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['numpy'],        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
