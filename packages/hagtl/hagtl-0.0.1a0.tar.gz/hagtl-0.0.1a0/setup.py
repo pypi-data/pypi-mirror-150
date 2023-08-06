6f5374c61fd32145eaaeec0373f7a6d1910c29af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:45 +0000

    Add package hagtl/0.0.1a0/hagtl-0.0.1a0.tar.gz

---
+++ packages/hagtl/hagtl-0.0.1a0.tar.gz/hagtl-0.0.1a0/setup.py
@@ -0,0 +1,26 @@
+"""H&A Geotechnical Design and Python Library (GTL), 2022.
+
+Strictly for internal use.
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hagtl",
+    version="0.0.1a",
+    author="Haley & Aldrich, Inc.",
+    author_email="nmachairas@haleyaldrich.com",
+    description="H&A Geotechnical Design and Python Library (GTL)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.haleyaldrich.com/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="hagtl"),
+    python_requires=">=3.10",
+)
