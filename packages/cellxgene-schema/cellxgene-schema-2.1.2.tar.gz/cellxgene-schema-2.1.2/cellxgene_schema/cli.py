06c56ac3243d2a546c9d76b7671b5cc6fc18aa74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:45 +0000

    Add package cellxgene-schema/2.1.2/cellxgene-schema-2.1.2.tar.gz

---
+++ packages/cellxgene-schema/cellxgene-schema-2.1.2.tar.gz/cellxgene-schema-2.1.2/cellxgene_schema/cli.py
@@ -0,0 +1,60 @@
+import click
+import sys
+
+
+@click.group(
+    name="schema",
+    subcommand_metavar="COMMAND <args>",
+    short_help="Apply and validate the cellxgene data integration schema to an h5ad file.",
+    context_settings=dict(max_content_width=85, help_option_names=["-h", "--help"]),
+)
+def schema_cli():
+    pass
+
+
+@click.command(
+    name="validate",
+    short_help="Check that an h5ad follows the cellxgene data integration schema.",
+    help="Check that an h5ad follows the cellxgene data integration schema. If validation fails this command will "
+    "return an exit status of 1 otherwise 0. When the '--add-labels <FILE>' tag is present, the command will add "
+    "ontology/gene labels based on IDs and write them to a new h5ad.",
+)
+@click.argument("h5ad_file", nargs=1, type=click.Path(exists=True, dir_okay=False))
+@click.option(
+    "-a",
+    "--add-labels",
+    "add_labels_file",
+    help="When present it will add labels to genes and ontologies based on IDs",
+    required=False,
+    default=None,
+    type=click.Path(exists=False, dir_okay=False, writable=True),
+)
+@click.option(
+    "-v",
+    "--verbose",
+    help="When present will set logging level to debug",
+    is_flag=True
+)
+def schema_validate(h5ad_file, add_labels_file, verbose):
+    # Imports are very slow so we defer loading until Click arg validation has passed
+
+    print("Loading dependencies")
+    try:
+        import anndata  # noqa: F401
+    except ImportError:
+        raise click.ClickException("[cellxgene] cellxgene-schema requires anndata")
+
+    print("Loading validator modules")
+    from .validate import validate
+
+    is_valid, _, _ = validate(h5ad_file, add_labels_file, verbose=verbose)
+    if is_valid:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
+schema_cli.add_command(schema_validate)
+
+if __name__ == "__main__":
+    schema_cli()
