e53a4c52fddf276438054754f440203695e0efc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:28 +0000

    Add package deluge-card/0.4.1/deluge_card-0.4.1-py3-none-any.whl

---
+++ packages/deluge-card/deluge_card-0.4.1-py3-none-any.whl/deluge_card/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for deluge-card."""
+
+__author__ = """Chris Chamberlain"""
+__email__ = 'chrisbc@artisan.co.nz'
+__version__ = '0.4.1'
+
+from .deluge_card import DelugeCardFS, InvalidDelugeCard, list_deluge_fs
+from .deluge_sample import Sample, mv_samples
+from .deluge_song import DelugeSong
