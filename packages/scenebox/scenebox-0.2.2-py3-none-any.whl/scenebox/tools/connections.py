400c63f02f0c1ecadfde938a6fd3d125bd800b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:42 +0000

    Add package scenebox/0.2.2/scenebox-0.2.2-py3-none-any.whl

---
+++ packages/scenebox/scenebox-0.2.2-py3-none-any.whl/scenebox/tools/connections.py
@@ -0,0 +1,30 @@
+from typing import Optional, Any
+
+
+class KVStoreTemplate:
+    def __init__(self):
+        pass
+
+    def put_str(self, key, value):
+        pass
+
+    def get_str(self, key, default=None):
+        pass
+
+    def put_bytes(self, key, value):
+        pass
+
+    def get_bytes(self, key, default=None):
+        pass
+
+    def put_json(self, key, json_data):
+        pass
+
+    def get_json(self, key: str, default: Optional[Any] = None):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def clear_all(self, pattern: str):
+        pass
