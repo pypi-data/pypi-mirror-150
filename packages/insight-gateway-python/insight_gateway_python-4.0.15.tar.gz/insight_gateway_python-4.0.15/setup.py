f53c17d9b77d2a67ea81109bcf7d3bc9a1cd26fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:48 +0000

    Add package insight-gateway-python/4.0.15/insight_gateway_python-4.0.15.tar.gz

---
+++ packages/insight-gateway-python/insight_gateway_python-4.0.15.tar.gz/insight_gateway_python-4.0.15/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="insight_gateway_python",
+    author="htsc",
+    version="4.0.15",
+    author_email="insight@htsc.com",
+    description="insight_gateway_python",
+    long_description="insight_gateway_python",
+    license='insightpythonsdk',
+    project_urls={
+        'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+        'Funding': 'https://donate.pypi.org',
+        'Source': 'https://github.com/pypa/sampleproject/',
+        'Tracker': 'https://github.com/pypa/sampleproject/issues',
+    },
+
+
+    packages= ['insight_gateway_python_v400',
+               'insight_gateway_python_v400/com',
+               'insight_gateway_python_v400/com/interface',
+               'insight_gateway_python_v400/com/libs/python37/x64',
+               ],
+    package_data={'': ['insight_gateway_python_v400/com/cert/HTInsightCA.crt',
+                       'insight_gateway_python_v400/com/cert/InsightClientCert.pem']},
+    install_requires=[],
+
+    python_requires='>=3.7.*',
+)
