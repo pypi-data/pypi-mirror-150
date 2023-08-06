45fc770147957263242403b54ff54292750728c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:12 +0000

    Add package mvola-api/1.0.1/mvola-api-1.0.1.tar.gz

---
+++ packages/mvola-api/mvola-api-1.0.1.tar.gz/mvola-api-1.0.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mvola_api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.9.0,<2.0.0',
+ 'python-dotenv>=0.20.0,<0.21.0',
+ 'requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'mvola-api',
+    'version': '1.0.1',
+    'description': 'Python package for MVola API',
+    'long_description': None,
+    'author': 'tsiresymila',
+    'author_email': 'tsiresymila@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
