569fd910cb9c80dfa004fffd446906cfcd6b910b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:56 +0000

    Add package playlist2podcast/0.5.0/playlist2podcast-0.5.0-py3-none-any.whl

---
+++ packages/playlist2podcast/playlist2podcast-0.5.0-py3-none-any.whl/playlist2podcast/__init__.py
@@ -0,0 +1,6 @@
+"""Package 'playlist2podcast' level definitions."""
+CODE_VERSION_MAJOR = 0  # Current major version of this code
+CODE_VERSION_MINOR = 5  # Current minor version of this code
+CODE_VERSION_PATCH = 0  # Current patch version of this code
+
+__version__ = f"{CODE_VERSION_MAJOR}.{CODE_VERSION_MINOR}.{CODE_VERSION_PATCH}"
