cfbd5c3ab0a8d3f92392398fca90fc478a3f5081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:53 +0000

    Add package Pyrogram/2.0.20/Pyrogram-2.0.20-py3-none-any.whl

---
+++ packages/Pyrogram/Pyrogram-2.0.20-py3-none-any.whl/pyrogram/methods/messages/delete_messages.py
@@ -0,0 +1,86 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Union, List
+
+import pyrogram
+from pyrogram import raw
+
+
+class DeleteMessages:
+    async def delete_messages(
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
+        message_ids: Union[int, List[int]],
+        revoke: bool = True
+    ) -> int:
+        """Delete messages, including service messages.
+
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+
+            message_ids (``int`` | List of ``int``):
+                A list of Message identifiers to delete (integers) or a single message id.
+
+            revoke (``bool``, *optional*):
+                Deletes messages on both parts.
+                This is only for private cloud chats and normal groups, messages on
+                channels and supergroups are always revoked (i.e.: deleted for everyone).
+                Defaults to True.
+
+        Returns:
+            ``int``: Amount of affected messages
+
+        Example:
+            .. code-block:: python
+
+                # Delete one message
+                await app.delete_messages(chat_id, message_id)
+
+                # Delete multiple messages at once
+                await app.delete_messages(chat_id, list_of_message_ids)
+
+                # Delete messages only on your side (without revoking)
+                await app.delete_messages(chat_id, message_id, revoke=False)
+        """
+        peer = await self.resolve_peer(chat_id)
+        # Follow type annotation of the raw function "DeleteMessage".
+        if not isinstance(message_ids, list):
+            message_ids = [message_ids]
+
+        if isinstance(peer, raw.types.InputPeerChannel):
+            r = await self.invoke(
+                raw.functions.channels.DeleteMessages(
+                    channel=peer,
+                    id=message_ids
+                )
+            )
+        else:
+            r = await self.invoke(
+                raw.functions.messages.DeleteMessages(
+                    id=message_ids,
+                    revoke=revoke or None # Follow the type annotation.
+                )
+            )
+
+        # Deleting messages you don't have right onto won't raise any error.
+        # Check for pts_count, which is 0 in case deletes fail.
+        return r.pts_count
