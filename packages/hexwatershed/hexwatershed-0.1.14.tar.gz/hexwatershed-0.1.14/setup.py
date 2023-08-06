247b7483cc6e08404969997fcd9f367765ca271c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:25:46 +0000

    Add package hexwatershed/0.1.14/hexwatershed-0.1.14.tar.gz

---
+++ packages/hexwatershed/hexwatershed-0.1.14.tar.gz/hexwatershed-0.1.14/setup.py
@@ -0,0 +1,62 @@
+
+import io
+import os
+import subprocess
+import shutil
+
+from setuptools import setup, find_packages, Command
+
+NAME = "hexwatershed"
+DESCRIPTION = \
+    "A mesh independent flow direction model for hydrologic models"
+AUTHOR = "Chang Liao"
+AUTHOR_EMAIL = "chang.liao@pnnl.gov"
+URL = "https://github.com/changliao1025/pyhexwatershed"
+VERSION = "0.1.14"
+REQUIRES_PYTHON = ">=3.8.0"
+KEYWORDS = "hexwatershed hexagon"
+
+REQUIRED = [
+    "pyflowline"
+]
+
+CLASSIFY = [
+    "Development Status :: 4 - Beta",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: C++",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Hydrology",
+    "Topic :: Scientific/Engineering :: GIS",
+    "Topic :: Scientific/Engineering :: Physics"
+]
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(
+            HERE, "README.md"), encoding="utf-8") as f:
+        LONG_DESCRIPTION = "\n" + f.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="custom",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    keywords=KEYWORDS,
+    url=URL,
+    packages=find_packages(),    
+    install_requires=REQUIRED,
+    classifiers=CLASSIFY
+)
