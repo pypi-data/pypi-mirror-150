cd53e29b35e4ad492a667c8b5d28c556514f6185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:51 +0000

    Add package gardnersnake/0.2.3/gardnersnake-0.2.3.tar.gz

---
+++ packages/gardnersnake/gardnersnake-0.2.3.tar.gz/gardnersnake-0.2.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gardnersnake",
+    version="0.2.3",
+    author="Zach Weber",
+    author_email="zach.weber.813@gmail.com",
+    description="Utilities for writing concise snakemake workflows",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zwebbs/gardnersnake",
+    project_urls={
+        "Bug Tracker": "https://github.com/zwebbs/gardnersnake/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=['pyyaml==6.0', 'jsonschema==4.4.0', 'pandas>=1.1.0', 'numpy==1.21.6'],
+    entry_points={'console_scripts': {
+        'check_directory=scripts.check_directory:main'}
+    }
+)
