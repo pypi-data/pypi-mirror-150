4ad33bbef5e8f23ff319cf87ff423153a97b6b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:39 +0000

    Add package asamint/0.1.3/asamint-0.1.3.tar.gz

---
+++ packages/asamint/asamint-0.1.3.tar.gz/asamint-0.1.3/asamint/cmdline.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+__copyright__ = """
+   pySART - Simplified AUTOSAR-Toolkit for Python.
+
+   (C) 2021 by Christoph Schueler <cpu12.gems.googlemail.com>
+
+   All Rights Reserved
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+   s. FLOSS-EXCEPTION.txt
+"""
+
+import argparse
+
+from asamint.config import read_configuration
+
+from pyxcp.master import Master
+from pyxcp.transport.can import (
+    try_to_install_system_supplied_drivers,
+    registered_drivers,
+)
+
+try_to_install_system_supplied_drivers()
+
+CAN_DRIVERS = registered_drivers()
+
+
+class ArgumentParser:
+    """
+
+    Parameter
+    ---------
+    callout: callable
+        Process user-supplied arguments.
+    """
+
+    def __init__(self, use_xcp=True, callout=None, *args, **kws):
+        self.callout = callout
+        self.use_xcp = use_xcp
+        kws.update(formatter_class=argparse.RawDescriptionHelpFormatter, add_help=True)
+        self._parser = argparse.ArgumentParser(*args, **kws)
+        self._parser.add_argument(
+            "-p",
+            "--project-file",
+            type=argparse.FileType("r"),
+            dest="project",
+            help="General project configuration.",
+        )
+
+        self._parser.add_argument(
+            "-e",
+            "--experiment-file",
+            type=argparse.FileType("r"),
+            dest="experiment",
+            help="Experiment specific configuration.",
+        )
+
+        self._parser.add_argument(
+            "-l",
+            "--loglevel",
+            choices=["ERROR", "WARN", "INFO", "DEBUG"],
+            default="INFO",
+        )
+        self._parser.add_argument(
+            "-u",
+            "--unlock",
+            help="Unlock protected resources",
+            dest="unlock",
+            action="store_true",
+        )
+        self._args = self.parser.parse_args()
+        args = self.args
+        self.project = read_configuration(args.project)
+        self.experiment = read_configuration(args.experiment)
+        self.project["LOGLEVEL"] = args.loglevel
+
+    @property
+    def args(self):
+        return self._args
+
+    def run(self):
+        """ """
+        if self.use_xcp:
+            if "TRANSPORT" not in self.project:
+                raise AttributeError("TRANSPORT must be specified in config!")
+            transport = self.project["TRANSPORT"].lower()
+            master = Master(transport, config=self.project) if self.use_xcp else None
+            if self.callout:
+                self.callout(master, self.args)
+            return master
+
+    @property
+    def parser(self):
+        return self._parser
---
+++ packages/asamint/asamint-0.1.3.tar.gz/asamint-0.1.3/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The setup script."""
+import os
+from glob import glob
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open("docs/README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+
+with open(os.path.join("asamint", "version.py"), "r") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.split("=")[-1].strip().strip('"')
+            break
+
+
+requirements = [
+    "asammdf",
+    "objutils",
+    "pya2ldb",
+    "pyxcp",
+    "babel",
+    "lz4",
+    "sortedcontainers",
+]
+
+setup_requirements = [
+    "pytest-runner",
+]
+
+test_requirements = [
+    "pytest",
+]
+
+setup(
+    author="Christoph Schueler",
+    author_email="cpu12.gems@googlemail.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GPLv2",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="Adds high-level, convenience, integration related functions for several opensource projects.",
+    install_requires=requirements,
+    license="GPLv2",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="ASAM Autosar ECU Calibration Measurement",
+    name="asamint",
+    # packages=find_packages(include=['asamint']),
+    packages=find_packages(),
+    package_data={
+        "dtds": glob("asamint/data/dtds/*.*"),
+        "templates": glob("asamint/data/templates/*.*"),
+    },
+    entry_points={
+        "console_scripts": ["xcp-log = asamint.scripts.xcp_log:main"],
+    },
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/christoph2/asamint",
+    version=version,
+    zip_safe=False,
+)
