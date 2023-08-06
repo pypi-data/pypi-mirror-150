993f3178c5fb56893f9480e60838d9ad55d3be64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:42 +0000

    Add package oai-repo/0.3.3/oai_repo-0.3.3.tar.gz

---
+++ packages/oai-repo/oai_repo-0.3.3.tar.gz/oai_repo-0.3.3/src/oai_repo/helpers.py
@@ -0,0 +1,180 @@
+"""
+Helper functions which may prove useful when creating your DataInterface instance
+"""
+import json
+from datetime import datetime
+from io import BytesIO
+import requests
+import jsonpath_ng
+from lxml import etree
+from .exceptions import OAIRepoInternalException, OAIRepoExternalException
+from .transform import Transform
+
+def bytes_to_xml(bdata: bytes|BytesIO) -> etree._Element:
+    """
+    Given a bytes or BytesIO, parse and return an lxml.etree._Element.
+    If passed an lxml.etree._Element, then will return it unchanged.
+    Args:
+        bdata (bytes|BytesIO): The bytes data to parse
+    Returns:
+        The loaded XML element.
+    Raises:
+        etree.XMLSyntaxError: On XML parse error
+    """
+    if not isinstance(bdata, etree._Element):
+        if isinstance(bdata, BytesIO):
+            bdata.seek(0)
+            bdata = bdata.read()
+        bdata = etree.fromstring(bdata)
+    return bdata
+
+def datestamp_short(timestamp: datetime) -> str:
+    """Convert a datetime to short form datestamp: YYYY-MM-DD"""
+    return timestamp.strftime("%Y-%m-%d")
+
+def datestamp_long(timestamp: datetime) -> str:
+    """Convert a datetime to long form datestamp: YYYY-MM-DDThh:mm:ssZ"""
+    return timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def granularity_format(granularity: str, timestamp: datetime) -> str:
+    """
+    Format the timestamp according to the OAI granularity and return it.
+    """
+    return datestamp_short(timestamp) \
+        if granularity == "YYYY-MM-DD" \
+        else datestamp_long(timestamp)
+
+def jsonpath_find(data: dict|list, path: str):
+    """
+    Get matching values for a given JSONPath
+    Args:
+        data: The already loaded JSON data
+        path: The JSONPath to find
+    Returns:
+        A list of matching values
+    Raises:
+        jsonpath_ng.exceptions.JSONPathError on jsonpath failure
+    """
+    pattern = jsonpath_ng.parse(path)
+    matches = pattern.find(data)
+    return [match.value for match in matches]
+
+def jsonpath_find_first(data: dict|list, path: str):
+    """
+    Get the first matching value for a given JSONPath
+    Args:
+        data: The already loaded JSON data
+        path: The JSONPath to find
+    Returns:
+        The matched value, or empty string if not found
+    Raises:
+        jsonpath_ng.exceptions.JSONPathError on jsonpath failure
+    """
+    matches = jsonpath_find(data, path)
+    return next(iter(matches)) if matches else None
+
+def xpath_find(xmlr: etree.Element, path: str):
+    """
+    Get matching values for a given XPath
+    Args:
+        xmlr: The root xml object to query
+        path: The xpath query
+    Returns:
+        A list of matching values
+    Raises:
+        lxml.etree.XPathError on xpath failure
+    """
+    return xmlr.xpath(path, namespaces=xmlr.nsmap)
+
+def xpath_find_first(xmlr: etree.Element, path: str):
+    """
+    Get the first matching value for a given XPath
+    Args:
+        xmlr: The root xml object to query
+        path: The xpath query
+    Returns:
+        The matched value, or empty string if not found
+    Raises:
+        lxml.etree.XPathError on xpath failure
+    """
+    matches = xpath_find(xmlr, path)
+    return next(iter(matches)) if matches else None
+
+# Exact repeat API calls will be pulled from here
+__APICALL_CACHE = {}
+
+def apicall_querypath(
+    url: str = None,
+    jsonpath: str = None,
+    xpath: str = None
+):
+    """
+    Perform an API call and then either an jsonpath or xpath query,
+    returning the first matching result.
+    Calls are cached. Subsequent calls to the same URL will used previous results.
+    args:
+        TODO
+    returns:
+        TODO
+    raises:
+        TODO
+    """
+    if not url:
+        raise OAIRepoInternalException("API call querypath without a URL provided.")
+    if not jsonpath and not xpath:
+        raise OAIRepoInternalException("API call querypath without a jsonpath or xpath provided.")
+
+    if url not in __APICALL_CACHE:
+        try:
+            resp = requests.get(url, timeout=10)
+        except requests.RequestException as exc:
+            raise OAIRepoExternalException(f"Call to API failed: {url}") from exc
+        if not resp.status_code == 200:
+            raise OAIRepoExternalException(f"Call to API returned {resp.status_code}: {url}")
+        __APICALL_CACHE[url] = resp
+    resp = __APICALL_CACHE[url]
+
+    match = None
+    if jsonpath:
+        try:
+            loaded = json.loads(resp.text)
+            match = jsonpath_find_first(loaded, jsonpath)
+        except jsonpath_ng.exceptions.JSONPathError as exc:
+            raise OAIRepoInternalException(f"JSONPath is not valid: {jsonpath}") from exc
+    elif xpath:
+        try:
+            loaded = etree.fromstring(resp.content)
+            match = xpath_find_first(loaded, xpath)
+        except etree.XPathError as exc:
+            raise OAIRepoInternalException(f"XPath is not valid: {xpath}") from exc
+        except etree.XMLSyntaxError as exc:
+            raise OAIRepoInternalException(
+                f"Response to API call was not valid XML: {url}"
+            ) from exc
+    return match
+
+def apicall_getxml(url: str = None):
+    """
+    Perform API call to URL and load the response as XML.
+    args:
+        url (str): A URL path to call
+    returns:
+        A lxml.etree._Element containing the root of the loaded XML
+    raises:
+        OAIRepoExternalException when the URL call fails or returns non-success response
+        OAIRepoInternalException when call to URL does not return valid XML or no URL was provided
+    """
+    if not url:
+        raise OAIRepoInternalException("API call getxml without a URL provided.")
+    try:
+        resp = requests.get(url, timeout=10)
+    except requests.RequestException as exc:
+        raise OAIRepoExternalException(f"Call to API failed: {url}") from exc
+    if not resp.status_code == 200:
+        raise OAIRepoExternalException(f"Call to API returned {resp.status_code}: {url}")
+
+    try:
+        loaded = etree.fromstring(resp.content)
+    except etree.XMLSyntaxError as exc:
+        raise OAIRepoInternalException(f"Response to API call was not valid XML: {url}") from exc
+    return loaded
---
+++ packages/oai-repo/oai_repo-0.3.3.tar.gz/oai_repo-0.3.3/src/oai_repo/repository.py
@@ -0,0 +1,118 @@
+"""
+OAIRepository functionality
+"""
+from typing import NamedTuple
+from datetime import datetime, timezone
+from .getrecord import GetRecordRequest, GetRecordResponse
+from .identify import IdentifyRequest, IdentifyResponse
+from .listidentifiers import ListIdentifiersRequest, ListIdentifiersResponse
+from .listmetadataformats import ListMetadataFormatsRequest, ListMetadataFormatsResponse
+from .listrecords import ListRecordsRequest, ListRecordsResponse
+from .listsets import ListSetsRequest, ListSetsResponse
+from .exceptions import (
+    OAIError, OAIErrorBadVerb, OAIErrorIdDoesNotExist,
+    OAIErrorCannotDisseminateFormat, OAIErrorBadArgument
+)
+from .error import OAIErrorResponse
+from .request import OAIRequest
+from .response import OAIResponse
+from .interface import DataInterface
+
+from .transform import Transform
+
+class VerbClasses(NamedTuple):
+    """Named access to verb classes"""
+    request: OAIRequest
+    response: OAIResponse
+
+VERBS = {
+    'GetRecord': VerbClasses(GetRecordRequest, GetRecordResponse),
+    'Identify': VerbClasses(IdentifyRequest, IdentifyResponse),
+    'ListIdentifiers': VerbClasses(ListIdentifiersRequest, ListIdentifiersResponse),
+    'ListMetadataFormats': VerbClasses(ListMetadataFormatsRequest, ListMetadataFormatsResponse),
+    'ListRecords': VerbClasses(ListRecordsRequest, ListRecordsResponse),
+    'ListSets': VerbClasses(ListSetsRequest, ListSetsResponse)
+}
+
+class OAIRepository:
+    """
+    The primary OAI repository class which loads the config and handles OAI requests
+    """
+    def __init__(self, data: DataInterface):
+        self.data = data
+
+    def process(self, request: dict) -> OAIResponse:
+        """
+        Given a request, route to appropriate action and return a response
+        """
+        try:
+            request = self.create_request(request)
+            response = self.create_response(request)
+        except OAIError as exc:
+            response = OAIErrorResponse(self, exc)
+        return response
+
+    @staticmethod
+    def create_request(args: dict) -> OAIRequest:
+        """Given arguments, create an appropriate new OAI request object"""
+        try:
+            verb = args.pop('verb')
+            request = VERBS[verb].request()
+            request.parse(args)
+            return request
+        except KeyError:
+            raise OAIErrorBadVerb(
+                "The value of the 'verb' argument in the request is not legal."
+            ) from None
+
+    def create_response(self, request: OAIRequest) -> OAIResponse:
+        """Given a request, create an appropriate OAI response object"""
+        return VERBS[request.verb].response(self, request)
+
+    def identifier(self, localid: str) -> str:
+        """Convert from local id value to OAI identifier"""
+        return Transform(self.config.localid).reverse(localid)
+
+    def localid(self, identifier: str) -> str:
+        """Convert from OAI identifier to local id value"""
+        return Transform(self.config.localid).forward(identifier)
+
+    def localmetadataid(self, metadataprefix: str) -> str:
+        """Convert a metadataprefix into a local metadata id value"""
+        if metadataprefix not in [mdf["metadataPrefix"] for mdf in self.config.metadataformats]:
+            raise OAIErrorCannotDisseminateFormat(
+                "The requested metadataPrefix is not valid for "\
+                "either the given record or the repository."
+            )
+        return Transform(self.config.localmetadataid).forward(metadataprefix)
+
+    def setname(self, localsetname: str) -> str:
+        """Convert a local setname into an OAI setname value"""
+        return Transform(self.config.localsetname).forward(localsetname)
+
+    def valid_date(self, datestr: str):
+        """
+        Parse an argument provided datestr into a datetime object;
+        Args:
+            datestr (str|None): An unvalidated date string
+        Returns:
+            A datetime.datetime object, or None if datestr was None.
+        Raises:
+            OAIErrorBadArgument If an invalid date is passed
+                or if date was not valid according to the repository
+                granularity.
+        """
+        allowed_datefmts = ["%Y-%m-%d"]
+        if self.data.get_identify().granularity == "YYYY-MM-DDThh:mm:ssZ":
+            allowed_datefmts.append("%Y-%m-%dT%H:%M:%SZ")
+
+        date = None
+        if datestr is not None:
+            try:
+                for datefmt in allowed_datefmts:
+                    date = datetime.strptime(datestr, datefmt).replace(tzinfo=timezone.utc)
+            except (TypeError, ValueError):
+                raise OAIErrorBadArgument(
+                    "A date passed in not in a valid format. See Identify for granularity."
+                ) from None
+        return date
