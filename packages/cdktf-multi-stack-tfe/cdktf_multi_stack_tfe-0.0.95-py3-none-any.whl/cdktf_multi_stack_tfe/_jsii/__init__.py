3e99fdde694708abdbd1a082ba4e93f6c3ee2934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:49 +0000

    Add package cdktf-multi-stack-tfe/0.0.95/cdktf_multi_stack_tfe-0.0.95-py3-none-any.whl

---
+++ packages/cdktf-multi-stack-tfe/cdktf_multi_stack_tfe-0.0.95-py3-none-any.whl/cdktf_multi_stack_tfe/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import cdktf._jsii
+import cdktf_cdktf_provider_tfe._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdktf-multi-stack-tfe",
+    "0.0.95",
+    __name__[0:-6],
+    "cdktf-multi-stack-tfe@0.0.95.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
