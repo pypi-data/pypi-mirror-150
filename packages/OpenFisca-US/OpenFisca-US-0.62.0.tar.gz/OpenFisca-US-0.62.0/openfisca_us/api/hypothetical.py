a09e0734b503665422e7ed6903140007b57431b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:21 +0000

    Add package OpenFisca-US/0.62.0/OpenFisca-US-0.62.0.tar.gz

---
+++ packages/OpenFisca-US/OpenFisca-US-0.62.0.tar.gz/OpenFisca-US-0.62.0/openfisca_us/api/hypothetical.py
@@ -0,0 +1,10 @@
+from openfisca_tools import IndividualSim as GeneralIndividualSim
+from openfisca_us import CountryTaxBenefitSystem
+from openfisca_us.entities import entities
+from openfisca_us.data import CPS
+
+
+class IndividualSim(GeneralIndividualSim):
+    tax_benefit_system = CountryTaxBenefitSystem
+    entities = {entity.key: entity for entity in entities}
+    default_dataset = CPS
