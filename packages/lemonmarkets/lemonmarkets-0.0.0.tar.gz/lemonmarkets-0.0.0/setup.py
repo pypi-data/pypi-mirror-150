bdeafc7a8323ae1e9b91d6e6fcde44081e8ecd3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:21 +0000

    Add package lemonmarkets/0.0.0/lemonmarkets-0.0.0.tar.gz

---
+++ packages/lemonmarkets/lemonmarkets-0.0.0.tar.gz/lemonmarkets-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name="lemonmarkets",
+	version="0.0.0",
+	description="SDK for lemon.markets API",
+	author='lemon.markets',
+	author_email='cto@lemon.markets',
+	url='http://lemon.markets',
+	packages=find_packages(),
+	package_dir={'': "."}
+)
