c8c76126a9d06afdf8bcc0ab7d78c38dea8c4f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:47 +0000

    Add package pyOnlineSpreadsheetEditing/0.0.8/pyOnlineSpreadsheetEditing-0.0.8-py3-none-any.whl

---
+++ packages/pyOnlineSpreadsheetEditing/pyOnlineSpreadsheetEditing-0.0.8-py3-none-any.whl/onlinespreadsheet/version.py
@@ -0,0 +1,15 @@
+'''
+Created on 2022-03-06
+
+@author: wf
+'''
+
+class Version(object):
+    '''
+    Version handling for pyOnlineSpreadsheetEditing
+    '''
+    name="pyOnlineSpreadsheetEditing"
+    version='0.0.8'
+    date = '2021-12-11'
+    updated = '2022-05-09'
+    description='python Online SpreadSheet Editing tool with configurable enhancer/importer and check phase'
