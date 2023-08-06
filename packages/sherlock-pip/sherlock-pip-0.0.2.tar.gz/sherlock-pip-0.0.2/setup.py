17cb91c4cedccecd92317c99c9edd8af2a1c8caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:48 +0000

    Add package sherlock-pip/0.0.2/sherlock-pip-0.0.2.tar.gz

---
+++ packages/sherlock-pip/sherlock-pip-0.0.2.tar.gz/sherlock-pip-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sherlock-pip",
+    version="0.0.2",
+    author="Necrownyx",
+    author_email="example@example.com",
+    description="sherlock-project/sherlock as a command line application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Necrownyx/sherlock",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "certifi>=2019.6.16",
+        "colorama>=0.4.1",
+        "PySocks>=1.7.0",
+        "requests>=2.22.0",
+        "requests-futures>=1.0.0",
+        "stem>=1.8.0 ",
+        "torrequest>=0.1.0"
+    ],
+    package_dir={"": "src"},
+    python_requires='>=3.6',
+)
