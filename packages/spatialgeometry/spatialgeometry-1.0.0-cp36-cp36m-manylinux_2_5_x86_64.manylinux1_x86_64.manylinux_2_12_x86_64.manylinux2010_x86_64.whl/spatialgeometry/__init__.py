fe07d6a895e761acf9039594fc0208bb61f7a813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:08 +0000

    Add package spatialgeometry/1.0.0/spatialgeometry-1.0.0-cp36-cp36m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/spatialgeometry/spatialgeometry-1.0.0-cp36-cp36m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl/spatialgeometry/__init__.py
@@ -0,0 +1,25 @@
+from spatialgeometry.SceneNode import SceneNode
+from spatialgeometry.SceneGroup import SceneGroup
+from spatialgeometry.Shape import Shape, Axes, Arrow
+from spatialgeometry.CollisionShape import (
+    CollisionShape,
+    Mesh,
+    Cylinder,
+    Cuboid,
+    Box,
+    Sphere,
+)
+
+__all__ = [
+    "Shape",
+    "CollisionShape",
+    "Mesh",
+    "Cylinder",
+    "Cuboid",
+    "Box",
+    "Sphere",
+    "Axes",
+    "Arrow",
+    "SceneNode",
+    "SceneGroup",
+]
