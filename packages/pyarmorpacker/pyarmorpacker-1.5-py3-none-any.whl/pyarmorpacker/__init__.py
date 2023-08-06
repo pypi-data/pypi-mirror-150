648cb8824d3246f96b12fe769c16be250c8deb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:56 +0000

    Add package pyarmorpacker/1.5/pyarmorpacker-1.5-py3-none-any.whl

---
+++ packages/pyarmorpacker/pyarmorpacker-1.5-py3-none-any.whl/pyarmorpacker/__init__.py
@@ -0,0 +1,8 @@
+"""
+ Copyright (C) 2022, Akridata, Inc - All Rights Reserved.
+ Unauthorized copying of this file, via any medium is strictly prohibited
+"""
+# Package pyarmorpacker
+import sys
+from os.path import abspath, dirname
+sys.path.insert(0, abspath(dirname(__file__)))
