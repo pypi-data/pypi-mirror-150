7044f66601cd945bdecdb94951a1eb4f3d481718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:16 +0000

    Add package intelligen/0.13.0/intelligen-0.13.0-py2.py3-none-any.whl

---
+++ packages/intelligen/intelligen-0.13.0-py2.py3-none-any.whl/intelligen/__init__.py
@@ -0,0 +1,29 @@
+"""Top-level package for intelligen."""
+
+__author__ = """Diego Bouchet"""
+__email__ = 'diegobouchet88@gmail.com'
+__version__ = '0.13.0'
+
+submodules = [
+        'AI',
+        'constants',
+        'integrate',
+        'intelligen',
+        'interpolate',
+        'linregress',
+        'numeric',
+        'signals',
+        'stats'
+    ]
+
+__all__ = submodules
+
+from . import AI
+from . import constants
+from . import integrate
+from . import intelligen
+from . import interpolate
+from . import linregress
+from . import numeric
+from . import signals
+from . import stats
