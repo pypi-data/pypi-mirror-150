e19ef2c8fcd3eabf9649a02f9b792a36c801b2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:52 +0000

    Add package jsonbourne/0.21.6/jsonbourne-0.21.6.tar.gz

---
+++ packages/jsonbourne/jsonbourne-0.21.6.tar.gz/jsonbourne-0.21.6/jsonbourne/httpx.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Jsonbourne wrapper around httpx clients -- lets you do response.JSON()"""
+
+from typing import Any
+
+from httpx import AsyncClient, Client, Cookies, Response
+
+from jsonbourne import JSON
+
+__all__ = (
+    "Response",
+    "AsyncClient",
+    "Client",
+    "Cookies",
+)
+
+
+def _JSON(self: Response, **kwargs: Any) -> Any:
+    return JSON(self.json())
+
+
+Response.JSON = _JSON
