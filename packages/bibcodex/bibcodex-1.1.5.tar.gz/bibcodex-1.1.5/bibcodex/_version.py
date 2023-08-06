457a98a5f42f928be09fbfbfbd18880b19a11dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:22:18 +0000

    Add package bibcodex/1.1.5/bibcodex-1.1.5.tar.gz

---
+++ packages/bibcodex/bibcodex-1.1.5.tar.gz/bibcodex-1.1.5/bibcodex/_version.py
@@ -0,0 +1,13 @@
+__version__ = "1.1.5"
+
+"""
+Version 1.1.5: Fix, cache between chunks error on missing key
+Version 1.1.4: Cache between chunks
+Version 1.1.3: Fix empty dataframe bug.
+Version 1.1.2: Lazy load in torch to keep NLP portion separate
+Version 1.1.1: Fix bug from requirements not in setup.py
+Version 1.1.0: Semantic embeddings (SPECTER) available.
+Version 1.0.2: Fix bug in validate. Progres bar not shown when empty.
+Version 1.0.1: Add tqdm status bar to downloads.
+Version 1.0.0: First release. Basic downloading enabled.
+"""
