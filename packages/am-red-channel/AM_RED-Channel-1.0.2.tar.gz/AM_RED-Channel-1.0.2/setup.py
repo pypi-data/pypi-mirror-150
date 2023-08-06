0b20eff7f9dadba7d199e248f8b7f0dfdf46af30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:52 +0000

    Add package am-red-channel/1.0.2/AM_RED-Channel-1.0.2.tar.gz

---
+++ packages/am-red-channel/AM_RED-Channel-1.0.2.tar.gz/AM_RED-Channel-1.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.2'
+DESCRIPTION = 'Making connection between bython and Node-RED'
+LONG_DESCRIPTION = 'This package send and recieve data with Node-RED'
+
+# Setting up
+setup(
+    name="AM_RED-Channel",
+    version=VERSION,
+    author="Amr mostafa (Amr-MAM)",
+    author_email="<amr.mostafa.abbas@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
