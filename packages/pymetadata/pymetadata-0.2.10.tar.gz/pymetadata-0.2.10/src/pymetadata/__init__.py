569833f374f4b97073b3412909da2425d82b6363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:29 +0000

    Add package pymetadata/0.2.10/pymetadata-0.2.10.tar.gz

---
+++ packages/pymetadata/pymetadata-0.2.10.tar.gz/pymetadata-0.2.10/src/pymetadata/__init__.py
@@ -0,0 +1,21 @@
+"""pymetadata - Python utilities for metadata."""
+from pathlib import Path
+
+__author__ = "Matthias Koenig"
+__version__ = "0.2.10"
+
+
+from depinfo import print_dependencies  # type: ignore
+
+
+def show_versions() -> None:
+    """Print dependency information."""
+    print_dependencies("pymetadata")
+
+
+program_name: str = "pymetadata"
+RESOURCES_DIR: Path = Path(__file__).parent / "resources"
+ENUM_DIR: Path = Path(__file__).parent / "metadata"
+
+CACHE_USE: bool = False
+CACHE_PATH: Path = RESOURCES_DIR / "cache"
