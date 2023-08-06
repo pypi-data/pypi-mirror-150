21891188adeb915d0d73d468161c2e89c741215f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:22 +0000

    Add package python-colorful-font-output/1.0.1/python_colorful_font_output-1.0.1.tar.gz

---
+++ packages/python-colorful-font-output/python_colorful_font_output-1.0.1.tar.gz/python_colorful_font_output-1.0.1/python_colorful_font_output/__init__.py
@@ -0,0 +1,9 @@
+'''
+这是一个用来改善Python终端输出的默认颜色
+（黑色、白色）.让你的文字更加好看、想要突
+出的地方更加明显！
+This is a default color (black, white)
+used to improve Python terminal output.
+Make your words more beautiful and the
+places you want to highlight more obvious!
+'''
