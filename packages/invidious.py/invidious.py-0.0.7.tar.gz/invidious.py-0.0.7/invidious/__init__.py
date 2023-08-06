6be0ff42579d87f2cacdadd33c8627f3a781b2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:47:39 +0000

    Add package invidious.py/0.0.7/invidious.py-0.0.7.tar.gz

---
+++ packages/invidious.py/invidious.py-0.0.7.tar.gz/invidious.py-0.0.7/invidious/__init__.py
@@ -0,0 +1,7 @@
+from .channel_item import *
+from .channel import *
+from .playlist_item import *
+from .video_item import *
+from .video import *
+from .recomended_video import *
+from .invidious import *
