f40cc7af11e63292501072fa48f69e38ca2e544f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:02 +0000

    Add package koala-pdf/1.1/koala_pdf-1.1.tar.gz

---
+++ packages/koala-pdf/koala_pdf-1.1.tar.gz/koala_pdf-1.1/README.md
@@ -0,0 +1,1 @@
+This is an experimental package from the koala family.
---
+++ packages/koala-pdf/koala_pdf-1.1.tar.gz/koala_pdf-1.1/koala_pdf/pdf2text.py
@@ -0,0 +1,15 @@
+""" This application converts PDF file to text. """
+
+class Converter:
+    """ This is a simple converter class. """
+
+    def convert(path):
+        """
+        Convert a PDF file to text.
+
+        Parameter:
+        path (str): The path to a PDF file.
+
+        Returns:
+        str: The content of a PDF file as text.
+        """
