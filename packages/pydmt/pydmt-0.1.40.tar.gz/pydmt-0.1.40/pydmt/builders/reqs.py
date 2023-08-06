2dca08254ae8e6f3edf08dee7668d8a5fcc84814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:15 +0000

    Add package pydmt/0.1.40/pydmt-0.1.40.tar.gz

---
+++ packages/pydmt/pydmt-0.1.40.tar.gz/pydmt-0.1.40/pydmt/builders/reqs.py
@@ -0,0 +1,33 @@
+"""
+This module build python virtual envrionments
+"""
+
+
+from pydmt.api.one_source_one_target import OneSourceOneTarget
+from pydmt.utils.subprocess import check_call
+from pydmt.utils.filesystem import mkdir_touch
+from pydmt.utils.python import collect_reqs, get_install_args
+
+
+class BuilderReqs(OneSourceOneTarget):
+    """
+    This is a review of how to build a python virtual environment:
+    # create the virtualenv
+    virtualenv [folder]
+    # activate it
+    source [folder]/bin/activate
+    # install packages from file
+    pip install -r requirements.txt
+    # OR
+    python -m pip install -r requirements.txt
+    # OR if you want to install a list of packages:
+    pip install -r [list of packages]
+    # OR
+    python -m pip install [list of packages]
+    """
+    def build(self) -> None:
+        args = []
+        get_install_args(args)
+        if collect_reqs(args):
+            check_call(args)
+        mkdir_touch(self.target)
