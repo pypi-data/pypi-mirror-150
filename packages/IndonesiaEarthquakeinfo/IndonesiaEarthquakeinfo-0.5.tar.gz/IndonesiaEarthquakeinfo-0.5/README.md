5cf770c7e0e488961d0b7af4026494ceb1a114c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:44 +0000

    Add package IndonesiaEarthquakeinfo/0.5/IndonesiaEarthquakeinfo-0.5.tar.gz

---
+++ packages/IndonesiaEarthquakeinfo/IndonesiaEarthquakeinfo-0.5.tar.gz/IndonesiaEarthquakeinfo-0.5/README.md
@@ -0,0 +1,16 @@
+# latest information earthquake indonesia
+<b>This package informs about the latest earthquake from Meteorology Climatology and Geophysics Agency(BMKG)</b>
+
+## HOW IT WORK?
+
+<h3>This package can scrape from bmkg(bmkg.go.id)</h3> 
+<br>
+<b>This package uses technology from beautifulsoup4 and requests</b>
+
+## HOW TO USE?
+```
+if __name__ == "__main__":
+    print("Latest earthquake information")
+    result = ekstrak()
+    showdata(result)
+```
---
+++ packages/IndonesiaEarthquakeinfo/IndonesiaEarthquakeinfo-0.5.tar.gz/IndonesiaEarthquakeinfo-0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="IndonesiaEarthquakeinfo",
+    version="0.5",
+    author="Muhammad Maulana B.",
+    author_email="muhammadmaulanabdz@gmail.com",
+    description="This package informs about the latest earthquake from Meteorology Climatology and "
+                "Geophysics Agency(BMKG)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bravelid/latestinformation-earthquake-indonesia",
+    project_urls={
+        "Github": "https://github.com/Bravelid/latestinformation-earthquake-indonesia",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+
+    ],
+    # package_dir={"": "src"},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
