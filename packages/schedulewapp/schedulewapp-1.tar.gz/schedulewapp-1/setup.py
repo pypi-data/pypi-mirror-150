1423834446206aef5c00cf08043879b7b343c78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:32 +0000

    Add package schedulewapp/1/schedulewapp-1.tar.gz

---
+++ packages/schedulewapp/schedulewapp-1.tar.gz/schedulewapp-1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+import setuptools
+
+
+VERSION = '1'
+DESCRIPTION = 'Basic Whatsapp Scheduler'
+
+# Setting up
+setup(
+    name="schedulewapp",
+    version=VERSION,
+    author="aisgupta (Aishwarya Gupta)",
+    author_email="<aisgupta06@gmail.com>",
+    description=DESCRIPTION,
+    packages=setuptools.find_packages(),
+    install_requires=['pyautogui', 'date', 'time', 'webbrowser'],
+    keywords=['python', 'whatsapp', 'message', 'scheduler', 'schedule', 'msg'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
