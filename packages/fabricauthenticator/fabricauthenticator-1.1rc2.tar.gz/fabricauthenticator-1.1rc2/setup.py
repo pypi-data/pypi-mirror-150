70f58c20e9454564047196ecb6ba20d073c61640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:14 +0000

    Add package fabricauthenticator/1.1rc2/fabricauthenticator-1.1rc2.tar.gz

---
+++ packages/fabricauthenticator/fabricauthenticator-1.1rc2.tar.gz/fabricauthenticator-1.1rc2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+__VERSION__ = "1.1rc2"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fabricauthenticator",
+    version=__VERSION__,
+    author="Erica Fu, Komal Thareja",
+    author_email="ericafu@renci.org, kthare10@renci.org",
+    description="Fabric Authenticator for Jupyterhub",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fabric-testbed/fabricauthenticator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
