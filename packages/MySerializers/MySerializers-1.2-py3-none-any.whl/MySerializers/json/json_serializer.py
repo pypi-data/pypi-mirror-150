8085b477f28b84b1ceb820260ba5691235cedc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:45 +0000

    Add package MySerializers/1.2/MySerializers-1.2-py3-none-any.whl

---
+++ packages/MySerializers/MySerializers-1.2-py3-none-any.whl/MySerializers/json/json_serializer.py
@@ -0,0 +1,21 @@
+from MySerializers.json.parser import json_parser
+from MySerializers.src.serializer import serialize, deserialize
+
+
+class JsonSerializer:
+
+    @staticmethod
+    def dumps(item):
+        return str(serialize(item)).replace("\n", "\\n")
+
+    @staticmethod
+    def dump(item, file):
+        file.write(JsonSerializer.dumps(item))
+
+    @staticmethod
+    def loads(item):
+        return deserialize(json_parser(item.replace("\\n", "\n")))
+
+    @staticmethod
+    def load(file):
+        return JsonSerializer.loads(file.read())
