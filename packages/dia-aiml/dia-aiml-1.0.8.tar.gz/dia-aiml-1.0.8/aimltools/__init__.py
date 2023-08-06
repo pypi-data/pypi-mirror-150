eda88192af591f703464240fc87451b2165d715b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:48 +0000

    Add package dia-aiml/1.0.8/dia-aiml-1.0.8.tar.gz

---
+++ packages/dia-aiml/dia-aiml-1.0.8.tar.gz/dia-aiml-1.0.8/aimltools/__init__.py
@@ -0,0 +1,11 @@
+"""
+Version as of November 18, 2020.
+
+This module is work in progress.
+
+For bug reports/suggestions/correspondence, please
+reach out to us at tauro_henson@lilly.com and
+gabbanini_francesco@lilly.com
+"""
+
+__version__ = '1.0.8'
