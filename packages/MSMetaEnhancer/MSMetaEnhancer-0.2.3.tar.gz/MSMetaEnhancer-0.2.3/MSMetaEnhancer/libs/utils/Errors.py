e9fa3846a5f458201b9c7c4a9da7c3b1889d0f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:35 +0000

    Add package MSMetaEnhancer/0.2.3/MSMetaEnhancer-0.2.3.tar.gz

---
+++ packages/MSMetaEnhancer/MSMetaEnhancer-0.2.3.tar.gz/MSMetaEnhancer-0.2.3/MSMetaEnhancer/libs/utils/Errors.py
@@ -0,0 +1,34 @@
+class ConversionNotSupported(Exception):
+    pass
+
+
+class TargetAttributeNotRetrieved(Exception):
+    pass
+
+
+class UnknownConverter(Exception):
+    pass
+
+
+class UnknownSpectraFormat(Exception):
+    pass
+
+
+class SourceAttributeNotAvailable(Exception):
+    pass
+
+
+class ServiceNotAvailable(Exception):
+    pass
+
+
+class UnknownResponse(Exception):
+    pass
+
+
+class InvalidAttributeFormat(Exception):
+    pass
+
+
+class DataAlreadyPresent(Exception):
+    pass
---
+++ packages/MSMetaEnhancer/MSMetaEnhancer-0.2.3.tar.gz/MSMetaEnhancer-0.2.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(name='MSMetaEnhancer',
+      version='0.2.3',
+      description='Repository for tool that adds more annotations '
+                  '(e.g. SMILES, InChI, CAS number) to MSP files (Python version).',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      url='https://github.com/RECETOX/MSMetaEnhancer',
+      author='Matej Trojak',
+      author_email='matej.trojak@recetox.muni.cz',
+      maintainer="RECETOX",
+      maintainer_email="GalaxyToolsDevelopmentandDeployment@space.muni.cz",
+      license='MIT',
+      packages=find_packages(exclude=['*tests*']),
+      zip_safe=False,
+      test_suite="tests",
+      python_requires='>=3.7',
+      install_requires=[
+            "matchms",
+            "requests",
+            "pandas",
+            "aiohttp",
+            "asyncstdlib",
+            "frozendict",
+            "tabulate"
+      ],
+      extras_require={
+        'test': [
+            'pytest',
+            'pytest-cov'
+        ]
+      },
+      )
