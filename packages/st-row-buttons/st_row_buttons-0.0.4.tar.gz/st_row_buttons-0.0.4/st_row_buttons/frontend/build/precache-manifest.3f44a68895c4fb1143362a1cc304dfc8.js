3fe8e57a53d01a9b755d424090e72f902fa385a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:57 +0000

    Add package st-row-buttons/0.0.4/st_row_buttons-0.0.4.tar.gz

---
+++ packages/st-row-buttons/st_row_buttons-0.0.4.tar.gz/st_row_buttons-0.0.4/st_row_buttons/frontend/build/precache-manifest.3f44a68895c4fb1143362a1cc304dfc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a34803569c11efc4a8f90a15b3e9dbe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f20d303bd5717c1e60f4",
+    "url": "./static/css/main.783992f3.chunk.css"
+  },
+  {
+    "revision": "37d571ab8ad824034bbe",
+    "url": "./static/js/2.efa84012.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.efa84012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20d303bd5717c1e60f4",
+    "url": "./static/js/main.ea20abc8.chunk.js"
+  },
+  {
+    "revision": "804077cbf41d6649ffc7",
+    "url": "./static/js/runtime-main.12e05911.js"
+  }
+]);
