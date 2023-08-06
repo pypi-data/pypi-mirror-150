89395730e4876671fe21892a0e40a409d49a68db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:31 +0000

    Add package gym-janggi/0.9.0/gym_janggi-0.9.0-py3-none-any.whl

---
+++ packages/gym-janggi/gym_janggi-0.9.0-py3-none-any.whl/gym_janggi/constants.py
@@ -0,0 +1,13 @@
+from janggi import constants, PieceType
+
+NUM_PIECE_TYPE = len(PieceType)
+
+MIN_ROW = constants.MIN_ROW
+MAX_ROW = constants.MAX_ROW
+MIN_COL = constants.MIN_COL
+MAX_COL = constants.MAX_COL
+
+NUM_ROWS = MAX_ROW - MIN_ROW + 1
+NUM_COLS = MAX_COL - MIN_COL + 1
+
+ACTION_SPACE = 9999
