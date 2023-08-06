6d9dd99bfc71245bdcfa3029ec8590c732380c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:41 +0000

    Add package textio/6.0/textio-6.0.tar.gz

---
+++ packages/textio/textio-6.0.tar.gz/textio-6.0/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "textio",
+    version = "6.0",
+    author = "Monil Darediya",
+    author_email = "monildarediya1@gmail.com",
+    description = "A Text Library",
+    long_description = """# About Module
+Hey This Is A Module For Python It Makes Easy To Do Some Animated Text first do: `import textio`
+textio has an alphs variable which has a list of alphabets
+to import use: `textio.alphs` and textio also has a `renderText()` function which is different from `print()` you are thinking what is different from `print()` ? because this function has animated text it uses 2 args 1 is `string` 2 is `writetime` you can put `writetime` 0.1 to 0.10 now don't think more about `writetime`
+put any number from this. then second function is `deleteText()` which will delete the `renderText()` you have written before it takes 1 arg wait which is how many seconds after it should delete the text thats the brief explaination of TextIO.
+""",
+    long_description_content_type = "text/markdown",
+    license = "BSD",
+    keywords = "TextEdits",
+    url = "",
+    packages=['textio'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License"],
+)
---
+++ packages/textio/textio-6.0.tar.gz/textio-6.0/textio/__init__.py
@@ -0,0 +1,25 @@
+import os,time,sys
+from distutils.log import error
+
+alphs = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+def renderText(string, writetime):
+        os.system('clear')
+        os.system('clear')
+        for char in f"{string}\n":
+                sys.stdout.write(f"{char}")
+                sys.stdout.flush()
+                time.sleep(writetime)
+
+def deleteText(wait):
+        time.sleep(wait)
+        os.system('clear')
+
+def PythonRunner():
+        for char in 'User@Desktop $ bash -f python.sh':
+                sys.stdout.write(char)
+                sys.stdout.flush()
+                time.sleep(0.1)
+        time.sleep(0)
+        getboy = input('\nEnter Your Filename: ')
+        exec(open(getboy).read())
+	
