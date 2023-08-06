0fa43e276715d549baf93a126a22b5250783ea14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:49 +0000

    Add package feios/0.1.0/feios-0.1.0.tar.gz

---
+++ packages/feios/feios-0.1.0.tar.gz/feios-0.1.0/src/feios/feios.py
@@ -0,0 +1,7 @@
+import sys,os,time
+import feios_utils.funcs as utils
+
+################################
+utils.outline("Starting system...")
+utils.outline("The system is still indev.Now it will halt.")
+exit()
