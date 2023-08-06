b8125d08087e212e6e8499a03a7a64912f8bfb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:01 +0000

    Add package gradio/3.0b1/gradio-3.0b1-py3-none-any.whl

---
+++ packages/gradio/gradio-3.0b1-py3-none-any.whl/gradio/templates/frontend/assets/color.84f6af37.js
@@ -0,0 +1,1 @@
+import{ab as o}from"./index.cab033fe.js";const t=r=>o[r%o.length];export{t as g};
---
+++ packages/gradio/gradio-3.0b1-py3-none-any.whl/gradio/templates/frontend/assets/index.e1edbf8f.js
@@ -0,0 +1,1 @@
+import{S as p}from"./StatusTracker.2867674d.js";import"./index.cab033fe.js";const t=["static"];export{p as Component,t as modes};
