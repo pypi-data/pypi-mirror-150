7835ef4e461ad360843193da8547066a87451ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:54 +0000

    Add package resotoworker/2.2.0/resotoworker-2.2.0.tar.gz

---
+++ packages/resotoworker/resotoworker-2.2.0.tar.gz/resotoworker-2.2.0/resotoworker/collect.py
@@ -0,0 +1,104 @@
+import multiprocessing
+import resotolib.proc
+from time import time
+from concurrent import futures
+from resotoworker.resotocore import send_to_resotocore
+from resotolib.baseplugin import BaseCollectorPlugin
+from resotolib.baseresources import GraphRoot
+from resotolib.graph import Graph, sanitize
+from resotolib.logger import log, setup_logger
+from resotolib.args import ArgumentParser
+from argparse import Namespace
+from typing import List, Optional
+from resotolib.config import Config, RunningConfig
+from resotolib.core.ca import TLSData
+
+
+def collect_and_send(
+    collectors: List[BaseCollectorPlugin], tls_data: Optional[TLSData] = None
+) -> None:
+    def collect(collectors: List[BaseCollectorPlugin]) -> Graph:
+        graph = Graph(root=GraphRoot("root", {}))
+
+        max_workers = (
+            len(collectors)
+            if len(collectors) < Config.resotoworker.pool_size
+            else Config.resotoworker.pool_size
+        )
+        if max_workers == 0:
+            log.error(
+                "No workers configured or no collector plugins loaded - skipping collect"
+            )
+            return
+        pool_args = {"max_workers": max_workers}
+        if Config.resotoworker.fork_process:
+            pool_args["mp_context"] = multiprocessing.get_context("spawn")
+            pool_args["initializer"] = resotolib.proc.initializer
+            pool_executor = futures.ProcessPoolExecutor
+            collect_args = {
+                "args": ArgumentParser.args,
+                "running_config": Config.running_config,
+            }
+        else:
+            pool_executor = futures.ThreadPoolExecutor
+            collect_args = {}
+
+        with pool_executor(**pool_args) as executor:
+            wait_for = [
+                executor.submit(
+                    collect_plugin_graph,
+                    collector,
+                    **collect_args,
+                )
+                for collector in collectors
+            ]
+            for future in futures.as_completed(wait_for):
+                cluster_graph = future.result()
+                if not isinstance(cluster_graph, Graph):
+                    log.error(f"Skipping invalid cluster_graph {type(cluster_graph)}")
+                    continue
+                graph.merge(cluster_graph)
+        sanitize(graph)
+        return graph
+
+    send_to_resotocore(collect(collectors), tls_data=tls_data)
+
+
+def collect_plugin_graph(
+    collector_plugin: BaseCollectorPlugin,
+    args: Namespace = None,
+    running_config: RunningConfig = None,
+) -> Optional[Graph]:
+    collector: BaseCollectorPlugin = collector_plugin()
+    collector_name = f"collector_{collector.cloud}"
+    resotolib.proc.set_thread_name(collector_name)
+
+    if args is not None:
+        ArgumentParser.args = args
+        setup_logger("resotoworker")
+    if running_config is not None:
+        Config.running_config.apply(running_config)
+
+    log.debug(f"Starting new collect process for {collector.cloud}")
+    start_time = time()
+    collector.start()
+    collector.join(Config.resotoworker.timeout)
+    elapsed = time() - start_time
+    if not collector.is_alive():  # The plugin has finished its work
+        if not collector.finished:
+            log.error(
+                f"Plugin {collector.cloud} did not finish collection"
+                " - ignoring plugin results"
+            )
+            return None
+        if not collector.graph.is_dag_per_edge_type():
+            log.error(
+                f"Graph of plugin {collector.cloud} is not acyclic"
+                " - ignoring plugin results"
+            )
+            return None
+        log.info(f"Collector of plugin {collector.cloud} finished in {elapsed:.4f}s")
+        return collector.graph
+    else:
+        log.error(f"Plugin {collector.cloud} timed out - discarding Plugin graph")
+        return None
---
+++ packages/resotoworker/resotoworker-2.2.0.tar.gz/resotoworker-2.2.0/resotoworker/resotocore.py
@@ -0,0 +1,123 @@
+import json
+import requests
+import tempfile
+from datetime import datetime
+from resotolib.args import ArgumentParser
+from resotolib.logger import log
+from resotolib.jwt import encode_jwt_to_headers
+from resotolib.graph import Graph, GraphExportIterator
+from resotolib.config import Config
+from resotolib.core import resotocore
+from resotolib.core.ca import TLSData
+from typing import Optional
+
+
+def send_to_resotocore(graph: Graph, tls_data: Optional[TLSData] = None):
+    if not ArgumentParser.args.resotocore_uri:
+        return
+
+    log.info("resotocore Event Handler called")
+
+    base_uri = resotocore.http_uri
+    resotocore_graph = Config.resotoworker.graph
+    dump_json = Config.resotoworker.debug_dump_json
+    tempdir = Config.resotoworker.tempdir
+    graph_merge_kind = Config.resotoworker.graph_merge_kind
+
+    verify = getattr(tls_data, "verify", None)
+    create_graph(base_uri, resotocore_graph, verify=verify)
+    update_model(graph, base_uri, dump_json=dump_json, tempdir=tempdir, verify=verify)
+
+    graph_export_iterator = GraphExportIterator(
+        graph,
+        delete_tempfile=not dump_json,
+        tempdir=tempdir,
+        graph_merge_kind=graph_merge_kind,
+    )
+    #  The graph is not required any longer and can be released.
+    del graph
+    graph_export_iterator.export_graph()
+    send_graph(graph_export_iterator, base_uri, resotocore_graph, verify=verify)
+
+
+def create_graph(
+    resotocore_base_uri: str, resotocore_graph: str, verify: Optional[str] = None
+):
+    graph_uri = f"{resotocore_base_uri}/graph/{resotocore_graph}"
+
+    log.debug(f"Creating graph {resotocore_graph} via {graph_uri}")
+
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+    }
+    if getattr(ArgumentParser.args, "psk", None):
+        encode_jwt_to_headers(headers, {}, ArgumentParser.args.psk)
+    r = requests.post(graph_uri, data="", headers=headers, verify=verify)
+    if r.status_code != 200:
+        log.error(r.content)
+        raise RuntimeError(f"Failed to create graph: {r.content}")
+
+
+def update_model(
+    graph: Graph,
+    resotocore_base_uri: str,
+    dump_json: bool = False,
+    tempdir: str = None,
+    verify: Optional[str] = None,
+) -> None:
+    model_uri = f"{resotocore_base_uri}/model"
+
+    log.debug(f"Updating model via {model_uri}")
+
+    model_json = json.dumps(graph.export_model(), indent=4)
+
+    if dump_json:
+        ts = datetime.now().strftime("%Y-%m-%d-%H-%M")
+        with tempfile.NamedTemporaryFile(
+            prefix=f"resoto-model-{ts}-",
+            suffix=".json",
+            delete=not dump_json,
+            dir=tempdir,
+        ) as model_outfile:
+            log.info(f"Writing model json to file {model_outfile.name}")
+            model_outfile.write(model_json.encode())
+
+    headers = {
+        "Content-Type": "application/json",
+    }
+    if getattr(ArgumentParser.args, "psk", None):
+        encode_jwt_to_headers(headers, {}, ArgumentParser.args.psk)
+
+    r = requests.patch(model_uri, data=model_json, headers=headers, verify=verify)
+    if r.status_code != 200:
+        log.error(r.content)
+        raise RuntimeError(f"Failed to create model: {r.content}")
+
+
+def send_graph(
+    graph_export_iterator: GraphExportIterator,
+    resotocore_base_uri: str,
+    resotocore_graph: str,
+    verify: Optional[str] = None,
+):
+    merge_uri = f"{resotocore_base_uri}/graph/{resotocore_graph}/merge"
+
+    log.debug(f"Sending graph via {merge_uri}")
+
+    headers = {
+        "Content-Type": "application/x-ndjson",
+        "Resoto-Worker-Nodes": str(graph_export_iterator.number_of_nodes),
+        "Resoto-Worker-Edges": str(graph_export_iterator.number_of_edges),
+    }
+    if getattr(ArgumentParser.args, "psk", None):
+        encode_jwt_to_headers(headers, {}, ArgumentParser.args.psk)
+
+    r = requests.post(
+        merge_uri, data=graph_export_iterator, headers=headers, verify=verify
+    )
+    if r.status_code != 200:
+        log.error(r.content)
+        raise RuntimeError(f"Failed to send graph: {r.content}")
+    log.debug(f"resotocore reply: {r.content.decode()}")
+    log.debug(f"Sent {graph_export_iterator.total_lines} items to resotocore")
