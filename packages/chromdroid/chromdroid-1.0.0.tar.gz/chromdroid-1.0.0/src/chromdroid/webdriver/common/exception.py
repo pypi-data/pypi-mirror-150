b7ec8f1f09034a999d0c260bf3307250d4bf69f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:55 +0000

    Add package chromdroid/1.0.0/chromdroid-1.0.0.tar.gz

---
+++ packages/chromdroid/chromdroid-1.0.0.tar.gz/chromdroid-1.0.0/src/chromdroid/webdriver/common/exception.py
@@ -0,0 +1,11 @@
+class WebDriverException(Exception):
+	pass
+
+class NoSuchElementException(WebDriverException):
+	pass
+
+class NoSuchPageSourceException(WebDriverException):
+	pass
+
+class InvalidElementStateException(WebDriverException):
+	pass
---
+++ packages/chromdroid/chromdroid-1.0.0.tar.gz/chromdroid-1.0.0/src/chromdroid/webdriver/remote/command.py
@@ -0,0 +1,21 @@
+class Command:
+    CLOSE = "close"
+    CLICK = "click"
+    CLEAR = "clear"
+    CURRENT_URL = "current url"
+    EXECUTE_SCRIPT = "execute script"
+    FOCUS = "focus"
+    FIND_ELEMENT = "find element"
+    FIND_ELEMENTS = "find elements"
+    GET = "get"
+    GET_ATTRIBUTE = "get attribute"
+    INIT = "init"
+    IS_READ_ONLY = "is read only"
+    IS_DISABLED = "is disabled"
+    PAGE_SOURCE = "page source"
+    SEND_KEYS = "send keys"
+    SET_VALUE = "set value"
+    SET_DISABLED = "set disabled"
+    TITLE = "title"
+    SET_COOKIE = 'setCookie'
+    GET_COOKIE = 'getCookie'
