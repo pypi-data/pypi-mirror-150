72f7611356601120fc293014a7268d865cff62e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:31 +0000

    Add package testflows.core/1.9.220512.1150702/testflows.core-1.9.220512.1150702.tar.gz

---
+++ packages/testflows.core/testflows.core-1.9.220512.1150702.tar.gz/testflows.core-1.9.220512.1150702/testflows/_core/__init__.py
@@ -0,0 +1,35 @@
+# Copyright 2019-2021 Katteli Inc.
+# TestFlows.com Open-Source Software Testing Framework (http://testflows.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+__author__ = "Vitaliy Zakaznikov"
+__version__ = "1.9.220512.1150702"
+__repository__ = "https://github.com/testflows/TestFlows-Core.git"
+__commit__ = "811d44e22999630397a7f5b44138300b45298d0d"
+__branch__ = "master"
+__license__ = f"""
+Copyright 2019-2021 Katteli Inc.
+TestFlows.com Open-Source Software Testing Framework (http://testflows.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License
+"""
