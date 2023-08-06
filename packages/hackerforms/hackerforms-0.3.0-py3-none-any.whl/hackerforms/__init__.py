8b001b059c00ca01011f4761adfb6b62ffe9edf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:34 +0000

    Add package hackerforms/0.3.0/hackerforms-0.3.0-py3-none-any.whl

---
+++ packages/hackerforms/hackerforms-0.3.0-py3-none-any.whl/hackerforms/__init__.py
@@ -0,0 +1,6 @@
+from .page import *
+from .inputs import *
+from .outputs import *
+from .stdio import *
+from .auth import *
+from .parameters import url_params
---
+++ packages/hackerforms/hackerforms-0.3.0-py3-none-any.whl/hackerforms/input_types.py
@@ -0,0 +1,163 @@
+from abc import abstractmethod, ABC
+from typing import List, Union, Dict
+from datetime import date
+
+from .type_classes import FileResponse, PhoneResponse
+
+class Input(ABC):
+    type: str
+
+    def __init__(self, key: str) -> None:
+        super().__init__()
+        self.key = key
+
+    @abstractmethod
+    def json():
+        pass
+
+    def convertAnswer(self, answer):
+        return answer
+  
+class TextInput(Input):
+    type = 'text-input'
+    def __init__(self, key: str, message: str):
+        super().__init__(key)
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.message,
+        }
+
+class DateInput(Input):
+    type = 'date-input'
+    def __init__(self, key: str, message: str):
+        super().__init__(key)
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.message,
+        }
+
+    def convertAnswer(self, answer: str):
+        '''Convert answer from string to date
+
+        Args:
+            answer (str): Date format YYYY-MM-DD
+        '''
+        split_answer = answer.split('-')
+        year = int(split_answer[0])
+        month = int(split_answer[1])
+        day = int(split_answer[2])
+        return date(year, month, day)
+
+class FileInput(Input):
+    type = 'file-input'
+    def __init__(self, key: str, message: str):
+        super().__init__(key)
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.message,
+        }
+
+    def convertAnswer(self, answer):
+        return FileResponse(answer)
+
+class MultipleChoiceInput(Input):
+    type = 'multiple-choice-input'
+    def __init__(self, key: str, message: str, options: Union[List[str], List[Dict]], multiple: bool = False):
+        super().__init__(key)
+        self.message = message
+        self.options = options
+        self.multiple = multiple
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.message,
+            'options': self.options,
+            'multiple': self.multiple,
+        }
+
+class DropdownInput(Input):
+    type = 'dropdown-input'
+    def __init__(self, key: str, name: str, options: Union[List[str], List[Dict]], multiple: bool = False):
+        super().__init__(key)
+        self.name = name
+        self.options = options
+        self.multiple = multiple
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.name,
+            'options': self.options,
+            'multiple': self.multiple,
+        }
+
+class TextareaInput(Input):
+    type = 'textarea-input'
+    def __init__(self, key: str, message: str):
+        super().__init__(key)
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.message,
+        }
+
+class NumberInput(Input):
+    type = 'number-input'
+    def __init__(self, key: str, message: str):
+        super().__init__(key)
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.message,
+        }
+
+class EmailInput(Input):
+    type = 'email-input'
+    def __init__(self, key: str, message: str):
+        super().__init__(key)
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.message,
+        }
+
+
+class PhoneInput(Input):
+    type = 'phone-input'
+    def __init__(self, key: str, message: str):
+        super().__init__(key)
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'key': self.key,
+            'message': self.message,
+        }
+
+    def convertAnswer(self, answer):
+        return PhoneResponse(raw=answer['raw'],masked=answer['masked'])
