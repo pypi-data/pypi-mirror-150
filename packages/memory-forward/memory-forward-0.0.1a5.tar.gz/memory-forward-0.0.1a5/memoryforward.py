c19d0d5c96971c1318ca796a59b850553b42fdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:44 +0000

    Add package memory-forward/0.0.1a5/memory-forward-0.0.1a5.tar.gz

---
+++ packages/memory-forward/memory-forward-0.0.1a5.tar.gz/memory-forward-0.0.1a5/memoryforward.py
@@ -0,0 +1,10 @@
+from typer import Typer
+from app import hello
+from app import goodbye
+
+app = Typer()
+app.command()(hello.hello)
+app.command()(goodbye.goodbye)
+
+def cli():
+    app()
---
+++ packages/memory-forward/memory-forward-0.0.1a5.tar.gz/memory-forward-0.0.1a5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.1a5'
+DESCRIPTION = 'A CLI tool for batch editing files'
+LONG_DESCRIPTION = 'A CLI tool developed for memory forward to help batch edit files, and their metadata.'
+
+# Setting up
+setup(
+    name="memory-forward",
+    version=VERSION,
+    author="Justin Robertson",
+    author_email="justin.robertson@auctane.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    py_modules=["app", "memoryforward"],
+    install_requires=["typer==0.4.1"],
+    python_requires=">=3.7",
+    keywords=['python', 'memory-forward'],
+    classifiers=[
+        "Intended Audience :: Other Audience",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    entry_points = '''
+        [console_scripts]
+        memory-forward=memoryforward:cli
+    '''
+)
