ad58c88576bb8b615fd4153dc9a7b55ed4715954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:48 +0000

    Add package fief-client/0.9.0/fief-client-0.9.0.tar.gz

---
+++ packages/fief-client/fief-client-0.9.0.tar.gz/fief-client-0.9.0/fief_client/__init__.py
@@ -0,0 +1,26 @@
+"Fief client for Python."
+from fief_client.client import (
+    Fief,
+    FiefAccessTokenExpired,
+    FiefAccessTokenInfo,
+    FiefAccessTokenInvalid,
+    FiefAccessTokenMissingScope,
+    FiefAsync,
+    FiefError,
+    FiefIdTokenInvalid,
+    FiefTokenResponse,
+)
+
+__version__ = "0.9.0"
+
+__all__ = [
+    "Fief",
+    "FiefAsync",
+    "FiefTokenResponse",
+    "FiefAccessTokenInfo",
+    "FiefError",
+    "FiefAccessTokenExpired",
+    "FiefAccessTokenMissingScope",
+    "FiefAccessTokenInvalid",
+    "FiefIdTokenInvalid",
+]
