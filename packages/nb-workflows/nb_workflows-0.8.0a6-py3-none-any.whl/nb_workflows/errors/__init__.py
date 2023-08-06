07f53c11daa81140e5441ca4a94898e4405e6d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:44 +0000

    Add package nb-workflows/0.8.0a6/nb_workflows-0.8.0a6-py3-none-any.whl

---
+++ packages/nb-workflows/nb_workflows-0.8.0a6-py3-none-any.whl/nb_workflows/errors/__init__.py
@@ -0,0 +1,9 @@
+from .client import LoginError, ProjectCreateError
+from .generics import (
+    AuthValidationFailed,
+    CommandExecutionException,
+    PrivateKeyNotFound,
+    ProjectNotFound,
+    WorkflowDisabled,
+    WorkflowNotFound,
+)
