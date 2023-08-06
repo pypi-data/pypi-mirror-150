ad2cf2047a9c0175b9cd330600a157c9ade35dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:13 +0000

    Add package lickport-array-interface/1.0.7/lickport_array_interface-1.0.7-py2.py3-none-any.whl

---
+++ packages/lickport-array-interface/lickport_array_interface-1.0.7-py2.py3-none-any.whl/lickport_array_interface/__init__.py
@@ -0,0 +1,11 @@
+from lickport_array_interface.__about__ import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __summary__,
+    __url__,
+    __version__,
+)
+
+from .lickport_array_interface import LickportArrayInterface
