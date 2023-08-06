36e5e23a71e2e71b1b9e05f56a97159d4a4101c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:31 +0000

    Add package themata/1.14/themata-1.14.tar.gz

---
+++ packages/themata/themata-1.14.tar.gz/themata-1.14/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    zip_safe=False,
+    install_requires=["sphinx>=1.1"],
+    name="themata",
+    version=1.14,
+    author="Adewale Azeez",
+    author_email="iamthecarisma@gmail.com",
+    description="Set of Highly customizable sphinx themes. hackish, milkish, fandango, clear, fluid, garri, water and sugar.",
+    long_description=long_description,
+    license="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    packages=[
+        "themata",
+        "themata.hackish",
+        "themata.milkish",
+        "themata.fandango",
+        "themata.clear",
+        "themata.fluid",
+        "themata.garri",
+        "themata.water",
+        "themata.sugar"
+    ],
+    include_package_data=True,
+    keywords="sphinx, extension, theme, thecarisma, hackish, milkish, fandango, clear, fluid, garri, water, sugar",
+    url="https://github.com/Thecarisma/themata/",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Common Public License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+    entry_points = {
+        'sphinx.html_themes': [
+            'themata = themata',
+            'hackish = themata.hackish',
+            'milkish = themata.milkish',
+            'fandango = themata.fandango',
+            'clear = themata.clear',
+            'fluid = themata.fluid',
+            'garri = themata.garri',
+            'water = themata.water',
+            'sugar = themata.sugar',
+        ]
+    },
+)
---
+++ packages/themata/themata-1.14.tar.gz/themata-1.14/themata/garri/__init__.py
@@ -0,0 +1,16 @@
+import os
+import themata
+
+def get_path():
+    """
+    Return the path to the garri theme
+    """
+    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+def setup(app):
+    if hasattr(app, "add_html_theme"):
+        theme_path = os.path.abspath(os.path.dirname(__file__))
+        app.add_html_theme("garri", theme_path)
+    app.connect("html-page-context", themata.update_context)
+    app.connect('build-finished', themata.copy_custom_files)
+    return {"version": themata.__version}
