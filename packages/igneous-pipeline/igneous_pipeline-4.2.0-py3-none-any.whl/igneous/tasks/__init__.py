427be878282fc4b6f9f48bdbc47eacea07d2c937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:49 +0000

    Add package igneous-pipeline/4.2.0/igneous_pipeline-4.2.0-py3-none-any.whl

---
+++ packages/igneous-pipeline/igneous_pipeline-4.2.0-py3-none-any.whl/igneous/tasks/__init__.py
@@ -0,0 +1,23 @@
+from .skeleton import (
+  SkeletonTask, UnshardedSkeletonMergeTask, 
+  ShardedSkeletonMergeTask, DeleteSkeletonFilesTask,
+  TransferSkeletonFilesTask, ShardedFromUnshardedSkeletonMergeTask
+)
+from .mesh import (
+  MeshTask, MeshManifestPrefixTask, 
+  MeshManifestFilesystemTask,
+  GrapheneMeshTask,
+  MultiResUnshardedMeshMergeTask,
+  MultiResShardedMeshMergeTask,
+  MultiResShardedFromUnshardedMeshMergeTask,
+  TransferMeshFilesTask, DeleteMeshFilesTask
+)
+from .image import (
+  HyperSquareConsensusTask, #HyperSquareTask,
+  DownsampleTask, QuantizeTask, 
+  TransferTask, WatershedRemapTask, DeleteTask, 
+  LuminanceLevelsTask, ContrastNormalizationTask,
+  MaskAffinitymapTask, InferenceTask, BlackoutTask,
+  TouchTask, ImageShardTransferTask, ImageShardDownsampleTask
+)
+from .spatial_index import SpatialIndexTask
