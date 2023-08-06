52cf4cdf7da5996915f88be5191ee41ac2021ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:41 +0000

    Add package gengli/0.0.1/gengli-0.0.1.tar.gz

---
+++ packages/gengli/gengli-0.0.1.tar.gz/gengli-0.0.1/gengli/__init__.py
@@ -0,0 +1,7 @@
+"""
+gengli
+======
+	It is package to generate transient noise bursts from LIGO with a Generative Adversarial Neural Networks. gengli can also adapt the generated glitches according to the user's needs.
+
+"""
+from .glitch_generator import glitch_generator
