01cfd4645d01ce66154b82cdcd610c8b9be9504c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:21 +0000

    Add package exhbma/0.1.7/exhbma-0.1.7-py3-none-any.whl

---
+++ packages/exhbma/exhbma-0.1.7-py3-none-any.whl/exhbma/__init__.py
@@ -0,0 +1,23 @@
+import pkg_resources
+
+from .exhaustive_search import ExhaustiveLinearRegression
+from .integrate import (
+    integrate_log_values_in_line,
+    integrate_log_values_in_square,
+    validate_list_dimension,
+)
+from .linear_regression import (
+    ConstantRegression,
+    LinearRegression,
+    MarginalLinearRegression,
+    MarginalNullRegression,
+)
+from .plot import feature_posterior, sigma_posterior, weight_diagram
+from .probabilities import RandomVariable, gamma, uniform
+from .scaler import StandardScaler
+
+try:
+    __version__ = pkg_resources.get_distribution("exhbma").version
+except pkg_resources.DistributionNotFound:
+    # Only for document generation
+    __version__ = "dev"
