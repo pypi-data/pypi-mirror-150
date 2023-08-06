cc351cd25afc6f4f6b4dcf5129c72e23da465062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:08 +0000

    Add package effect-edc/0.1.3/effect-edc-0.1.3.tar.gz

---
+++ packages/effect-edc/effect-edc-0.1.3.tar.gz/effect-edc-0.1.3/effect_subject/models/lab_results/blood_results_fbc.py
@@ -0,0 +1,35 @@
+from edc_crf.crf_with_action_model_mixin import CrfWithActionModelMixin
+from edc_lab.model_mixins import CrfWithRequisitionModelMixin
+from edc_lab_results import BLOOD_RESULTS_FBC_ACTION
+from edc_lab_results.model_mixins import (
+    BloodResultsModelMixin,
+    HaemoglobinModelMixin,
+    NeutrophilModelMixin,
+    PlateletsModelMixin,
+    RbcModelMixin,
+    WbcModelMixin,
+)
+from edc_model import models as edc_models
+
+from effect_labs.panels import fbc_panel
+
+
+class BloodResultsFbc(
+    CrfWithActionModelMixin,
+    CrfWithRequisitionModelMixin,
+    HaemoglobinModelMixin,
+    RbcModelMixin,
+    WbcModelMixin,
+    PlateletsModelMixin,
+    NeutrophilModelMixin,
+    BloodResultsModelMixin,
+    edc_models.BaseUuidModel,
+):
+    action_name = BLOOD_RESULTS_FBC_ACTION
+    tracking_identifier_prefix = "FB"
+
+    lab_panel = fbc_panel
+
+    class Meta(CrfWithActionModelMixin.Meta, edc_models.BaseUuidModel.Meta):
+        verbose_name = "Blood Result: FBC"
+        verbose_name_plural = "Blood Results: FBC"
