d6a9ded5ff89ef1b3dd27621e5777263b992e1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:56 +0000

    Add package scipion-em-prody/3.0.8/scipion-em-prody-3.0.8.tar.gz

---
+++ packages/scipion-em-prody/scipion-em-prody-3.0.8.tar.gz/scipion-em-prody-3.0.8/prody2/protocols/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# Module to declare protocols
+# Find documentation here: https://scipion-em.github.io/docs/docs/developer/creating-a-protocol
+# **************************************************************************
+from .protocol_anm import ProDyANM
+# from .protocol_rtb import ProDyRTB
+
+from .protocol_atoms import ProDySelect, ProDyAlign
+from .protocol_compare import ProDyCompare
+from .protocol_edit import ProDyEdit
+from .protocol_defvec import ProDyDefvec
+from .protocol_import import ProDyImportModes
