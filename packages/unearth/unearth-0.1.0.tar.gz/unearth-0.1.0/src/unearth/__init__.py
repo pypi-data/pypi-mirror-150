41c139244a3234573a56305803fd373da9ff8985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:55 +0000

    Add package unearth/0.1.0/unearth-0.1.0.tar.gz

---
+++ packages/unearth/unearth-0.1.0.tar.gz/unearth-0.1.0/src/unearth/__init__.py
@@ -0,0 +1,10 @@
+"""
+    unearth
+
+    A utility to fetch and download python packages
+    :author: Frost Ming <mianghong@gmail.com>
+    :license: MIT
+"""
+from unearth.finder import PackageFinder
+
+__all__ = ["PackageFinder"]
