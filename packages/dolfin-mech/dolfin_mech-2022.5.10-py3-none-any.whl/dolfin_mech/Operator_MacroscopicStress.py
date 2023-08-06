9d62119e1ae4aa17d0a0244efb8acb0dbe86fa4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:00 +0000

    Add package dolfin-mech/2022.5.10/dolfin_mech-2022.5.10-py3-none-any.whl

---
+++ packages/dolfin-mech/dolfin_mech-2022.5.10-py3-none-any.whl/dolfin_mech/Operator_MacroscopicStress.py
@@ -0,0 +1,34 @@
+#coding=utf8
+
+################################################################################
+###                                                                          ###
+### Created by Martin Genet, 2018-2022                                       ###
+###                                                                          ###
+### École Polytechnique, Palaiseau, France                                   ###
+###                                                                          ###
+###                                                                          ###
+### And Mahdi Manoochehrtayebi, 2021-2022                                    ###
+###                                                                          ###
+### École Polytechnique, Palaiseau, France                                   ###
+###                                                                          ###
+################################################################################
+
+import dolfin
+
+import dolfin_mech as dmech
+from .Operator import Operator
+
+################################################################################
+
+class MacroscopicStressOperator(Operator):
+
+    def __init__(self,
+            sigma_bar,
+            sigma_bar_test,
+            material,
+            measure):
+
+        self.material = material
+        self.measure  = measure
+
+        self.res_form = dolfin.inner(sigma_bar - self.material.sigma, sigma_bar_test) * self.measure # MG20220426: Need to compute <sigma> properly, including fluid pressure
