9b484b8736c0ad0c465187d8c3c1fb387ff9f657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:57 +0000

    Add package st-row-buttons/0.0.1/st_row_buttons-0.0.1.tar.gz

---
+++ packages/st-row-buttons/st_row_buttons-0.0.1.tar.gz/st_row_buttons-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="st_row_buttons",
+    version="0.0.1",
+    author="kubix",
+    description="Streamlit Button Selection Component",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/twelvefootball/st-btn-select-custom",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    license="MIT",
+    license_files=('LICENSE'),
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+)
+
+
+#pypi-AgENdGVzdC5weXBpLm9yZwIkYWY3YWJjMDQtN2FkOC00NzQ1LWJmMTktZmQ3NzQyYTIyOGQ5AAIleyJwZXJtaXNzaW9ucyI6ICJ1c2VyIiwgInZlcnNpb24iOiAxfQAABiBPFtqXgqwkt_ch2QcvUZLFY9VF-7ZXGJqt1uVBTogP7w
