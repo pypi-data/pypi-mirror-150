f972b99efd0e08764fe161d99e4da3c0eea9590b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:08 +0000

    Add package edx-toggles/5.0.0/edx-toggles-5.0.0.tar.gz

---
+++ packages/edx-toggles/edx-toggles-5.0.0.tar.gz/edx-toggles-5.0.0/edx_toggles/toggles/internal/waffle/__init__.py
@@ -0,0 +1,5 @@
+"""
+Waffle classes in the context of edx-platform and other IDAs.
+
+For detailed usage see https://github.com/edx/edx-toggles/blob/master/docs/how_to/implement_the_right_toggle_type.rst
+"""
