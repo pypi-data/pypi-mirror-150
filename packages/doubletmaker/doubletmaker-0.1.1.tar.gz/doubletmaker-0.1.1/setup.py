623660211434a670ea8c30a20ccb90af7b07b148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:49 +0000

    Add package doubletmaker/0.1.1/doubletmaker-0.1.1.tar.gz

---
+++ packages/doubletmaker/doubletmaker-0.1.1.tar.gz/doubletmaker-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['doubletmaker']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'doubletmaker',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Patrick Lynch',
+    'author_email': 'p@tricklynch.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
