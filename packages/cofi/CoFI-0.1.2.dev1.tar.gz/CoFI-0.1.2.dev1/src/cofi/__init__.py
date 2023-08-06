d804a08298a0ad0cc74db940f097b043291355c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:27:31 +0000

    Add package cofi/0.1.2.dev1/CoFI-0.1.2.dev1.tar.gz

---
+++ packages/cofi/CoFI-0.1.2.dev1.tar.gz/CoFI-0.1.2.dev1/src/cofi/__init__.py
@@ -0,0 +1,21 @@
+from .base_problem import BaseProblem
+from .inversion_options import InversionOptions
+from .inversion import Inversion, InversionResult
+
+from . import solvers
+
+
+try:
+    from . import _version
+
+    __version__ = _version.__version__
+except ImportError:
+    pass
+
+
+__all__ = [
+    "BaseProblem",  # public API, basic usage
+    "InversionOptions",  # public API, basic usage
+    "Inversion",  # public API, basic usage
+    "InversionResult",  # public API, for advanced usage (own solver)
+]
