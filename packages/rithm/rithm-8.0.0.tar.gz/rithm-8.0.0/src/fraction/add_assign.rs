6d5126a7c91efd53f0f09e4f06493df55367f6b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:25 +0000

    Add package rithm/8.0.0/rithm-8.0.0.tar.gz

---
+++ packages/rithm/rithm-8.0.0.tar.gz/rithm-8.0.0/src/fraction/add_assign.rs
@@ -0,0 +1,42 @@
+use std::ops::AddAssign;
+
+use crate::traits::{
+    AdditiveMonoid, DivisivePartialMagma, GcdMagma, MultiplicativeMonoid, Oppositive,
+};
+
+use super::types::{normalize_components_moduli, Fraction};
+
+impl<
+        Component: AdditiveMonoid
+            + Clone
+            + DivisivePartialMagma
+            + GcdMagma
+            + MultiplicativeMonoid
+            + Oppositive,
+    > AddAssign for Fraction<Component>
+{
+    fn add_assign(&mut self, other: Self) {
+        (self.numerator, self.denominator) = normalize_components_moduli(
+            self.numerator.clone() * other.denominator.clone()
+                + other.numerator * self.denominator.clone(),
+            self.denominator.clone() * other.denominator,
+        );
+    }
+}
+
+impl<
+        Component: AdditiveMonoid
+            + Clone
+            + DivisivePartialMagma
+            + GcdMagma
+            + MultiplicativeMonoid
+            + Oppositive,
+    > AddAssign<Component> for Fraction<Component>
+{
+    fn add_assign(&mut self, other: Component) {
+        (self.numerator, self.denominator) = normalize_components_moduli(
+            self.numerator.clone() + other * self.denominator.clone(),
+            self.denominator.clone(),
+        );
+    }
+}
---
+++ packages/rithm/rithm-8.0.0.tar.gz/rithm-8.0.0/src/fraction/trunc.rs
@@ -0,0 +1,18 @@
+use crate::traits::{Ceil, CheckedDivEuclid, Floor, MultiplicativeMonoid, Oppositive, Trunc};
+
+use super::types::Fraction;
+
+impl<
+        Component: Clone + CheckedDivEuclid<Output = Option<Component>> + MultiplicativeMonoid + Oppositive,
+    > Trunc for Fraction<Component>
+{
+    type Output = Component;
+
+    fn trunc(self) -> Self::Output {
+        if self.is_negative() {
+            self.ceil()
+        } else {
+            self.floor()
+        }
+    }
+}
