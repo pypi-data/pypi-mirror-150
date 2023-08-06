94f418a6f63eb5bb8277aef85f9fb0cfb8dbe789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:52 +0000

    Add package MeUtils/2022.5.16.14.23.40/MeUtils-2022.5.16.14.23.40-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2022.5.16.14.23.40-py3-none-any.whl/meutils/str_utils/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : __init__.py
+# @Time         : 2022/5/12 下午2:02
+# @Author       : yuanjie
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+
+from meutils.pipe import *
+from meutils.request_utils.crawler import Crawler
+
+"""todo
+url 拼接
+
+"""
+
+
+def str_replace(s: str, dic: dict):
+    """多值替换
+        str_replace('abcd', {'a': '8', 'd': '88'})
+    """
+    return s.translate(str.maketrans(dic))
+
+
+def unquote(s='%E6%9C%80%E6%96%B0%E6%9C%8D%E5%8A%A1'):
+    """http字符串解码"""
+    from urllib import parse
+
+    return parse.unquote(s)
+
+
+@lru_cache()
+def arabic2chinese(arabic=123):
+    c = Crawler(f'https://szjrzzwdxje.bmcx.com/{arabic}__szjrzzwdxje')
+    return c.xpath('//span//text()')[-3:-1]
+
+
+if __name__ == '__main__':
+    print(str_replace('abcd', {'a': '8', 'd': '88'}))
+    print(unquote())
+    print(arabic2chinese())
