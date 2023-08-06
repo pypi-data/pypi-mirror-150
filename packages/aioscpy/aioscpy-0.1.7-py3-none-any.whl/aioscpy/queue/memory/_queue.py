e92a12b3bc78151046ffb609f92476df129937e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:12 +0000

    Add package aioscpy/0.1.7/aioscpy-0.1.7-py3-none-any.whl

---
+++ packages/aioscpy/aioscpy-0.1.7-py3-none-any.whl/aioscpy/queue/memory/_queue.py
@@ -0,0 +1,45 @@
+from asyncio import Queue
+
+from aioscpy.queue import BaseQueue
+
+
+class PriorityQueue(BaseQueue):
+
+    def __init__(self, server, serializer="pickle"):
+        super().__init__(server)
+        self.serializer = self.__compat__[serializer]
+
+    def qsize(self) -> int:
+        """Return the length of the queue"""
+        return self.server.qsize()
+
+    async def push(self, request: dict):
+        data = self._encode_request(request)
+        await self.server.put(data)
+
+    async def pop(self, timeout: int = 0) -> dict:
+        _item = await self.server.get()
+        return self._decode_request(_item)
+
+
+def memory_queue() -> PriorityQueue:
+    server = Queue()
+    return PriorityQueue(server=server)
+
+
+spider_queue = memory_queue
+
+
+"""
+async def run():
+    queue = memery_queue('message:queue')
+    await queue.push({"url": "https://www.baidu.com/?kw=1", "task_id": '123'})
+    print(await queue.pop())
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(run())
+
+"""
+
