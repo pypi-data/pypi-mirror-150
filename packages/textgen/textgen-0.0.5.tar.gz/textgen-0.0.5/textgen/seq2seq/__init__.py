b07f407a4ec6ddedc83bca8a34d0e6dcac364eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:42 +0000

    Add package textgen/0.0.5/textgen-0.0.5.tar.gz

---
+++ packages/textgen/textgen-0.0.5.tar.gz/textgen-0.0.5/textgen/seq2seq/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: refer https://github.com/ThilinaRajapakse/simpletransformers
+"""
+
+from textgen.config.model_args import Seq2SeqArgs
+from textgen.seq2seq.bert_seq2seq_model import BertSeq2SeqModel
+from textgen.seq2seq.seq2seq_model import Seq2SeqModel
+from textgen.seq2seq.conv_seq2seq_model import ConvSeq2SeqModel
