df241d68a7c97838f518bfdcd51a26bfa228be24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:38 +0000

    Add package devcloudcli/1.0.5/devcloudcli-1.0.5-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.0.5-py3-none-any.whl/src/scripts/edge-to-cloud-services/seo-reference-implementations/wireless-network-ready-intelligent-traffic-management/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing wireless-network-ready-intelligent-traffic-management ..." 
+echo 04b8af94-ecea-4377-bf8c-19592d3ac4f7 | edgesoftware install wireless-network-ready-intelligent-traffic-management 623c98999654a8f4bd94f55b
+
+
+~
+
