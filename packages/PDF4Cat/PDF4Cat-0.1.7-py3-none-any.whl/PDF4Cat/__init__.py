c42a1dce5c60c8370e035f41bec7e27617a171e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:40 +0000

    Add package PDF4Cat/0.1.7/PDF4Cat-0.1.7-py3-none-any.whl

---
+++ packages/PDF4Cat/PDF4Cat-0.1.7-py3-none-any.whl/PDF4Cat/__init__.py
@@ -0,0 +1,8 @@
+from .splitter import Splitter
+from .merger import Merger
+from .crypt import Crypter
+from .effects import Effects
+from .compress import PdfOptimizer
+from .converter import Converter
+
+from .doc import Doc
