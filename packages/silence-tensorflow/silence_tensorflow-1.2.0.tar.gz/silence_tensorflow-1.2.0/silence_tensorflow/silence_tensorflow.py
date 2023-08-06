4d0445c993cf78c99e3ffbb1821bd4b4701f4176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:48 +0000

    Add package silence-tensorflow/1.2.0/silence_tensorflow-1.2.0.tar.gz

---
+++ packages/silence-tensorflow/silence_tensorflow-1.2.0.tar.gz/silence_tensorflow-1.2.0/silence_tensorflow/silence_tensorflow.py
@@ -0,0 +1,13 @@
+"""Module providing tools to shut up tensorflow useless warnings, letting you focus on the actual problems."""
+import os
+import logging
+
+
+def silence_tensorflow():
+    """Silence every unnecessary warning from tensorflow."""
+    logging.getLogger('tensorflow').setLevel(logging.ERROR)
+    os.environ["KMP_AFFINITY"] = "noverbose"
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    import tensorflow as tf
+    tf.get_logger().setLevel('ERROR')
+    tf.autograph.set_verbosity(3)
