76dfa32ba9ac6b15bf5a34c349d3aa792cad10c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:47:42 +0000

    Add package expressmoney-points/0.6.5/expressmoney-points-0.6.5.tar.gz

---
+++ packages/expressmoney-points/expressmoney-points-0.6.5.tar.gz/expressmoney-points-0.6.5/expressmoney_points/sync/auth.py
@@ -0,0 +1,24 @@
+__all__ = ('UserPoint',)
+
+from expressmoney.api import *
+
+SERVICE = 'sync'
+
+
+class UserReadContract(Contract):
+    id = serializers.IntegerField(min_value=1)
+    last_login = serializers.DateTimeField()
+    username = serializers.CharField()
+    email = serializers.EmailField(allow_blank=True)
+    date_joined = serializers.DateTimeField()
+
+
+class UserID(ID):
+    _service = SERVICE
+    _app = 'auth'
+    _view_set = 'user'
+
+
+class UserPoint(ListPointMixin, ContractPoint):
+    _point_id = UserID()
+    _read_contract = UserReadContract
