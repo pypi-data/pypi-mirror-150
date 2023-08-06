3c1bcd57d363a1b5abed03f1bf060387710c9686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:46:48 +0000

    Add package invidious.py/0.0.2/invidious.py-0.0.2.tar.gz

---
+++ packages/invidious.py/invidious.py-0.0.2.tar.gz/invidious.py-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A Python wrapper for Invidious API'
+LONG_DESCRIPTION = 'A package that make work with Invidious API so easy.'
+
+# Setting up
+setup(
+    name="invidious.py",
+    version=VERSION,
+    author="loliconshik3",
+    author_email="loliconshik3@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'invidious', 'youtube', 'video', 'api'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
