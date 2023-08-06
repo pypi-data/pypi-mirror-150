a368ab2466a63c3ce58f39ed020495ffec5b3f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:58 +0000

    Add package unicorn-binance-trailing-stop-loss/0.7.1/unicorn-binance-trailing-stop-loss-0.7.1.tar.gz

---
+++ packages/unicorn-binance-trailing-stop-loss/unicorn-binance-trailing-stop-loss-0.7.1.tar.gz/unicorn-binance-trailing-stop-loss-0.7.1/setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# File: setup.py
+#
+# Part of ‘UNICORN Binance Trailing Stop Loss’
+# Project website: https://www.lucit.tech/unicorn-binance-trailing-stop-loss.html
+# Github: https://github.com/LUCIT-Systems-and-Development/unicorn-binance-trailing-stop-loss
+# Documentation: https://unicorn-binance-trailing-stop-loss.docs.lucit.tech
+# PyPI: https://pypi.org/project/unicorn-binance-trailing-stop-loss
+#
+# Author: LUCIT Systems and Development
+#
+# Copyright (c) 2022-2022, LUCIT Systems and Development (https://www.lucit.tech) and Oliver Zehentleitner
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+import setuptools
+from unicorn_binance_trailing_stop_loss.manager import BinanceTrailingStopLossManager
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='unicorn-binance-trailing-stop-loss',
+     version=str(BinanceTrailingStopLossManager.get_version()),
+     author="LUCIT Systems and Development",
+     author_email='info@lucit.tech',
+     url="https://github.com/LUCIT-Systems-and-Development/unicorn-binance-trailing-stop-loss",
+     description="",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     license='MIT License',
+     install_requires=['requests', 'unicorn-binance-websocket-api', 'unicorn-binance-rest-api', 'ubtsl'],
+     keywords='binance, ',
+     project_urls={
+        'Documentation': 'https://lucit-systems-and-development.github.io/unicorn-binance-trailing-stop-loss',
+        'Wiki': 'https://github.com/LUCIT-Systems-and-Development/unicorn-binance-trailing-stop-loss/wiki',
+        'Author': 'https://www.lucit.tech',
+        'Changes': 'https://unicorn-binance-trailing-stop-loss.docs.lucit.tech//CHANGELOG.html',
+        'Issue Tracker': 'https://github.com/LUCIT-Systems-and-Development/unicorn-binance-trailing-stop-loss/issues',
+        'Chat': 'https://gitter.im/unicorn-binance-suite/unicorn-binance-trailing-stop-loss',
+        'Telegram': 'https://t.me/unicorndevs',
+     },
+     python_requires='>=3.7.0',
+     packages=setuptools.find_packages(exclude=["images", "pypi", "sphinx", "docs", ".github", "bot", "cli"]),
+     entry_points={
+         "console_scripts": [
+             "ubtsl = unicorn_binance_trailing_stop_loss.__main__:main",
+         ]},
+     classifiers=[
+         "Development Status :: 4 - Beta",
+         "Programming Language :: Python :: 3.7",
+         "Programming Language :: Python :: 3.8",
+         "Programming Language :: Python :: 3.9",
+         "Programming Language :: Python :: 3.10",
+         "License :: OSI Approved :: MIT License",
+         'Intended Audience :: Developers',
+         "Intended Audience :: Financial and Insurance Industry",
+         "Intended Audience :: Information Technology",
+         "Intended Audience :: Science/Research",
+         "Operating System :: OS Independent",
+         "Topic :: Office/Business :: Financial :: Investment",
+         'Topic :: Software Development :: Libraries :: Python Modules',
+     ],
+)
