010d447cf6ad59ddaf178623e64fd20fb2fe316e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:34 +0000

    Add package popug-sdk/0.0.5/popug_sdk-0.0.5.tar.gz

---
+++ packages/popug-sdk/popug_sdk-0.0.5.tar.gz/popug_sdk-0.0.5/popug_sdk/application.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+from typing import Any
+
+from fastapi import (
+    APIRouter,
+    FastAPI,
+)
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.openapi.utils import get_openapi
+
+from popug_sdk.conf import settings
+from popug_sdk.schemas.pong import Pong
+
+__all__ = ("app",)
+
+
+def custom_openapi(app: FastAPI) -> dict[str, Any]:
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title=settings.project,
+        version=settings.version or "0.0.1",
+        routes=app.routes,
+    )
+    openapi_schema["x-tagGroups"] = settings.tag_groups
+    app.openapi_schema = openapi_schema
+
+    return app.openapi_schema
+
+
+healthcheck_router = APIRouter()
+
+
+@healthcheck_router.get("/", response_model=Pong)
+def ping() -> dict[str, Any]:
+    """Healthcheck endpoint"""
+
+    return {
+        "project": settings.project,
+        "version": settings.version,
+        "datetime": datetime.utcnow().isoformat(),
+    }
+
+
+app = FastAPI(title=settings.project)
+
+
+if settings.use_https:
+    app.add_middleware(HTTPSRedirectMiddleware)
+
+app.openapi = lambda: custom_openapi(app)  # type: ignore
+app.include_router(healthcheck_router, prefix="")
+
+
+@app.on_event("startup")
+def startup() -> None:
+    if settings.database.enabled:
+        from popug_sdk.db import init_db  # noqa
+
+        init_db()
+
+    if settings.redis_configs:
+        from popug_sdk.redis.redis_pool import init_redis_pool  # noqa
+
+        init_redis_pool(settings.redis_configs, settings.redis)
+
+
+@app.on_event("shutdown")
+def shutdown() -> None:
+    if settings.redis:
+        from popug_sdk.redis.redis_pool import close_redis_pool  # noqa
+
+        close_redis_pool(settings.redis_configs)
---
+++ packages/popug-sdk/popug_sdk-0.0.5.tar.gz/popug_sdk-0.0.5/popug_sdk/db/__init__.py
@@ -0,0 +1,63 @@
+from contextlib import contextmanager
+from typing import (
+    Any,
+    Generator,
+)
+
+from sqlalchemy import create_engine as _engine_create
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import (
+    Session,
+    sessionmaker,
+)
+
+from popug_sdk.conf import settings
+from popug_sdk.db.registry import mapper_registry  # noqa
+
+__all__ = (
+    "create_engine",
+    "create_session",
+    "init_db",
+    "mapper_registry",
+)
+
+
+_engine: Engine | None = None
+_Session: sessionmaker | None = None
+
+
+def create_engine() -> Engine:
+    global _engine
+
+    if not _engine:
+        _engine = _engine_create(settings.database_dsn, pool_pre_ping=True)
+
+    return _engine
+
+
+def init_db() -> sessionmaker:
+    global _Session
+    create_engine()
+
+    if not _Session:
+        _Session = sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=_engine,
+            expire_on_commit=False,
+        )
+
+    return _Session
+
+
+@contextmanager
+def create_session(
+    session: Session | None = None, **kwargs: Any
+) -> Generator[Session, None, None]:
+    if not session:
+        _session = init_db()
+
+        with _session(**kwargs) as session:
+            yield session
+    else:
+        yield session
