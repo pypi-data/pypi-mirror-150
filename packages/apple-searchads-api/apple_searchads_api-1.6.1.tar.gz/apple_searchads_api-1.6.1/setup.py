7521dba543bdc051f6e1423646b49c88c24d8e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:52 +0000

    Add package apple-searchads-api/1.6.1/apple_searchads_api-1.6.1.tar.gz

---
+++ packages/apple-searchads-api/apple_searchads_api-1.6.1.tar.gz/apple_searchads_api-1.6.1/setup.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='apple_searchads_api',
+    description='Apple SearchAds API non-official python library',
+    version='1.6.1',
+    url='https://github.com/dapanas/searchads_api',
+    author='Abdul Majeed Alkattan',
+    author_email='alkattan@phiture.com',
+    packages=["searchads_api"],
+    keywords=['python', 'searchads', 'library'],
+    install_requires=['requests>=2.27.1', 'PyJWT==2.4.0', 'cryptography==3.4.8'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
