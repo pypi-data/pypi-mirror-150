6b50727452b1bdc62a7af34392ba34dc0e35d123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:53 +0000

    Add package pikachuwechat/0.1.2/pikachuwechat-0.1.2.tar.gz

---
+++ packages/pikachuwechat/pikachuwechat-0.1.2.tar.gz/pikachuwechat-0.1.2/pikachuwechat/pikachuwechat.py
@@ -0,0 +1,54 @@
+'''
+Function:
+    微信小助手
+Author:
+    Charles
+微信公众号:
+    Charles的皮卡丘
+'''
+import warnings
+if __name__ == '__main__':
+    from modules import *
+    from __init__ import __version__
+else:
+    from .modules import *
+    from .__init__ import __version__
+warnings.filterwarnings('ignore')
+
+
+'''微信小助手'''
+class pikachuwechat():
+    def __init__(self, **kwargs):
+        print(self)
+        for key, value in kwargs.items(): setattr(self, key, value)
+        self.supported_funcs = self.initializeallfuncs()
+    '''开发者调用对应的功能'''
+    def execute(self, func_type=None, config={}):
+        assert func_type in self.supported_funcs, 'unsupport func_type %s...' % func_type
+        client = self.supported_funcs[func_type](**config)
+        client.run()
+    '''初始化所有功能'''
+    def initializeallfuncs(self):
+        supported_funcs = {
+            'AutoReply': AutoReply,
+            'AntiWithdrawal': AntiWithdrawal,
+            'AnalysisFriends': AnalysisFriends,
+        }
+        return supported_funcs
+    '''获得所有支持的功能信息'''
+    def getallsupported(self):
+        all_supports = {}
+        for key, value in self.supported_funcs.items():
+            all_supports[value.func_name] = key
+        return all_supports
+    '''repr'''
+    def __repr__(self):
+        return 'Welcome to use PikachuWeChat!\nYou can visit https://github.com/CharlesPikachu/pikachuwechat for more details.'
+
+
+'''run'''
+if __name__ == '__main__':
+    import random
+    wechat_helper = pikachuwechat()
+    all_supports = wechat_helper.getallsupported()
+    wechat_helper.execute(random.choice(list(all_supports.values())))
