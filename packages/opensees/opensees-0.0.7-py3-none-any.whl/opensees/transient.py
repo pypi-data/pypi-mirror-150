c4c86044295fb194b46f3b482beb17f2100dfa4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:38 +0000

    Add package opensees/0.0.7/opensees-0.0.7-py3-none-any.whl

---
+++ packages/opensees/opensees-0.0.7-py3-none-any.whl/opensees/transient.py
@@ -0,0 +1,10 @@
+
+integrator GeneralizedAlpha $alphaM $alphaF <$gamma $beta>
+    args = [
+        Num("alphaM"  about=r"$\alpha_M$ factor"),
+        Num("alphaF"  about=r"$\alpha_F$ factor"),
+        Num("gamma"   about=r"$\gamma$ factor"),
+        Num("beta"    about=r"$\beta$ factor")
+    ]
+
+
