03c6deee68a49d67b6a43d0d3d6a59002bab70b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:35 +0000

    Add package invenio-communities/2.8.0.dev24/invenio_communities-2.8.0.dev24-py2.py3-none-any.whl

---
+++ packages/invenio-communities/invenio_communities-2.8.0.dev24-py2.py3-none-any.whl/invenio_communities/assets/semantic-ui/js/invenio_communities/members/invitations/invitationsModal/MemberSearchBar.js
@@ -0,0 +1,166 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2022 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+import { i18next } from "@translations/invenio_communities/i18next";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { Image, withCancel } from "react-invenio-forms";
+import { Dropdown, List } from "semantic-ui-react";
+
+export class MembersSearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isFetching: false,
+      error: false,
+      suggestions: [],
+    };
+  }
+
+  serializeUsersForDropdown = (users) => {
+    return users.map((person) => {
+      const name = person.profile.full_name || person.id;
+
+      return {
+        text: name,
+        value: person.id,
+        key: person.id,
+        content: (
+          <List divided relaxed>
+            <List.Item>
+              <Image size="mini" src={person.links.avatar} avatar />
+              <List.Content>
+                <List.Header as="a">{name}</List.Header>
+                <List.Description>
+                  {person.profile.affiliations}
+                </List.Description>
+              </List.Content>
+            </List.Item>
+          </List>
+        ),
+      };
+    });
+  };
+
+  serializeGroupsForDropdown = (groups) => {
+    return groups.map((group) => {
+      return {
+        text: group.id,
+        value: group.id,
+        key: group.id,
+        content: (
+          <List divided relaxed>
+            <List.Item>
+              <Image size="mini" src={group.links.avatar} avatar />
+              <List.Content>
+                <List.Header as="a">{group.id}</List.Header>
+
+              </List.Content>
+            </List.Item>
+          </List>
+        ),
+      };
+    });
+  };
+
+  optionsGenerator = (suggestions) => {
+    const { searchType } = this.props;
+    const serializer = {
+      user: this.serializeUsersForDropdown,
+      group: this.serializeGroupsForDropdown,
+    };
+    return serializer[searchType](suggestions);
+  };
+
+  onChange = (event, data) => {
+    const { handleChange, selectedMembers, searchType } = this.props;
+    const { suggestions } = this.state;
+
+    const memberAlreadySelected = data.value in selectedMembers;
+
+    if (memberAlreadySelected) return;
+
+    const newSelectedMember = suggestions.find(
+      (item) => item.id === data.value
+    );
+
+    const serializedSelectedMember = {
+      id: newSelectedMember.id,
+      type: searchType,
+      avatar: newSelectedMember?.links?.avatar,
+    };
+
+    if(searchType === "group") {
+      serializedSelectedMember["name"] = newSelectedMember.id;
+    }
+    else {
+      serializedSelectedMember["name"] =
+        newSelectedMember.profile?.full_name ||
+        newSelectedMember.name ||
+        newSelectedMember.id;
+    }
+    selectedMembers[serializedSelectedMember.id] = serializedSelectedMember;
+    handleChange(selectedMembers);
+  };
+
+  onSearchChange = async (event, data) => {
+    const { fetchMembers } = this.props;
+    try {
+      this.setState({ isFetching: true });
+
+      const cancellableSuggestions = withCancel(fetchMembers(data.searchQuery));
+      const suggestions = await cancellableSuggestions.promise;
+
+      this.setState({
+        isFetching: false,
+        suggestions: suggestions.data.hits.hits,
+        error: false,
+      });
+    } catch (e) {
+      console.error(e);
+      this.setState({
+        isFetching: false,
+        error: true,
+      });
+    }
+  };
+
+  render() {
+    const { isFetching, error, suggestions } = this.state;
+    const { placeholder } = this.props;
+    return (
+      <Dropdown
+        selectOnBlur={false}
+        error={error}
+        fluid
+        loading={isFetching}
+        onSearchChange={this.onSearchChange}
+        onChange={this.onChange}
+        search
+        options={this.optionsGenerator(suggestions)}
+        selection
+        value=""
+        icon="search"
+        placeholder={placeholder}
+      />
+    );
+  }
+}
+
+MembersSearchBar.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  selectedMembers: PropTypes.object.isRequired,
+  fetchMembers: PropTypes.func.isRequired,
+  searchType: PropTypes.oneOf(["group", "user"]),
+  placeholder: PropTypes.string,
+};
+
+MembersSearchBar.defaultProps = {
+  placeholder: i18next.t("Search ..."),
+};
