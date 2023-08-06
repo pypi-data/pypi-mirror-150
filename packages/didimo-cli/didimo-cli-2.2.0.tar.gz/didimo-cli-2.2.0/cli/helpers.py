dff03a576ecee475f5c1fd9030911fda4fa53ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:17 +0000

    Add package didimo-cli/2.2.0/didimo-cli-2.2.0.tar.gz

---
+++ packages/didimo-cli/didimo-cli-2.2.0.tar.gz/didimo-cli-2.2.0/cli/helpers.py
@@ -0,0 +1,77 @@
+import click
+import sys
+
+from .network import DidimoAuth, http_get
+from urllib import parse as urlparse
+
+
+class URL(click.ParamType):
+    name = "url"
+
+    def convert(self, value, param, ctx):
+        if not isinstance(value, tuple):
+            value_parsed = urlparse.urlparse(value)
+            if value_parsed.scheme not in ("http", "https"):
+                self.fail("invalid URL scheme", param, ctx)
+            if value_parsed.netloc == "":
+                self.fail("host is empty", param, ctx)
+        return value
+
+
+def get_didimo_status(config, id):
+    api_path = "/v3/didimos/" + id
+    url = config.api_host + api_path
+    r = http_get(url, auth=DidimoAuth(config, api_path))
+    return r.json()
+
+
+def download_didimo(config, id, package_type, output_path):
+    api_path = "/v3/didimos/" + id
+    url = config.api_host + api_path
+    r = http_get(url, auth=DidimoAuth(config, api_path))
+
+
+    for package_itm in r.json()['transfer_formats']:
+        s3url = ""
+        output_path = id+"_"+package_itm["name"] + ".zip"
+
+        if package_type == None or len(package_type) == 0:
+            s3url = package_itm["__links"]["self"]
+        else:
+            if package_itm["name"] == package_type:
+                s3url = package_itm["__links"]["self"]
+
+        if s3url != "":
+            print ("downloading....")
+            with http_get(s3url, auth=DidimoAuth(config, api_path)) as r:
+                r.raise_for_status()
+                zipsize = int(r.headers.get('content-length', 0))
+                with click.open_file(output_path, 'wb') as f:
+                    label = "Downloading %s" % id
+                    with click.progressbar(length=zipsize, label=label) as bar:
+                        for chunk in r.iter_content(chunk_size=2048):
+                            size = f.write(chunk)
+                            bar.update(size)
+            click.secho('Downloaded to %s' % output_path, fg='blue', err=True)
+
+        # else:
+            # print ("Unable to download")
+
+
+def download_asset(config, asset_url, api_path, output_path):
+
+    if asset_url != "":
+        print ("downloading....")
+        with http_get(asset_url, auth=DidimoAuth(config, api_path)) as r:
+            r.raise_for_status()
+            zipsize = int(r.headers.get('content-length', 0))
+            with click.open_file(output_path, 'wb') as f:
+                label = "Downloading %s" % id
+                with click.progressbar(length=zipsize, label=label) as bar:
+                    for chunk in r.iter_content(chunk_size=2048):
+                        size = f.write(chunk)
+                        bar.update(size)
+        click.secho('Downloaded to %s' % output_path, fg='blue', err=True)
+
+    else:
+        print ("Unable to download")
---
+++ packages/didimo-cli/didimo-cli-2.2.0.tar.gz/didimo-cli-2.2.0/cli/network.py
@@ -0,0 +1,73 @@
+import click
+import requests
+import hmac
+import time
+import sys
+import platform
+from hashlib import sha256
+
+from ._version import __version__
+
+
+class DidimoAuth(requests.auth.AuthBase):
+    def __init__(self, config, path):
+        self.config = config
+        self.path = path
+
+    def __call__(self, r):
+        access_key = self.config.access_key
+        r.headers["didimo-api-key"] = access_key
+        r.headers["User-Agent"] = "didimo-cli/%s (%s, %s)" % (__version__,
+                                                              platform.python_version(),
+                                                              platform.system())
+
+        return r
+
+
+def http_get(url, **kwargs):
+    try:
+        r = requests.get(url, **kwargs)
+        if r.status_code == 200:
+            return r
+        else:
+            click.secho('Error %d' % r.status_code, err=True, fg='red')
+            click.echo(r.text)
+            sys.exit(1)
+    except:
+        click.echo("A Network Error Has Occured")
+        sys.exit(1)
+
+
+def http_post(url, **kwargs):
+    r = requests.post(url, **kwargs)
+    if r.status_code == 200:
+        return r
+    else:
+        click.secho('Error %d' % r.status_code, err=True, fg='red')
+        click.echo(r.text)
+        sys.exit(1)
+
+
+def http_post_withphoto(url, access_key, payload, photo, photo_depth):
+
+    if photo_depth != None:
+        files = [
+            ('photo', (photo, open(photo, 'rb'), 'image/jpeg')),
+            ('depth', (photo_depth, open(photo_depth, 'rb'), 'image/png'))
+        ]
+    else:
+        files = [('photo', (photo, open(photo, 'rb'), 'image/jpeg'))]
+
+    headers = {
+        'DIDIMO-API-KEY': access_key
+    }
+
+    r = requests.request("POST", url, headers=headers,
+                         data=payload, files=files)
+
+    if r.status_code == 201:
+        return r
+    else:
+        click.secho('Error %d' % r.status_code, err=True, fg='red')
+        click.echo("An error has occured. Please check your API key")
+        sys.exit(1)
