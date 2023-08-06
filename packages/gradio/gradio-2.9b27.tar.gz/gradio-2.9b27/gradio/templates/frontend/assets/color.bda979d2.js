ab15da516254f0e9ef712551b23c061765f18320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:12 +0000

    Add package gradio/2.9b27/gradio-2.9b27.tar.gz

---
+++ packages/gradio/gradio-2.9b27.tar.gz/gradio-2.9b27/gradio/templates/frontend/assets/color.bda979d2.js
@@ -0,0 +1,1 @@
+import"./index.7d6e0911.js";import{o}from"./colors.621f0c00.js";const n=r=>o[r%o.length];export{n as g};
---
+++ packages/gradio/gradio-2.9b27.tar.gz/gradio-2.9b27/gradio/templates/frontend/assets/index.63a5618d.js
@@ -0,0 +1,1 @@
+import{S as p}from"./StatusTracker.3d1cbfdb.js";import"./index.7d6e0911.js";const t=["static"];export{p as Component,t as modes};
