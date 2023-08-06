81349181ee8e689b0104ecc44ef32596639dc709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:24 +0000

    Add package pymyku/0.2.3/pymyku-0.2.3.tar.gz

---
+++ packages/pymyku/pymyku-0.2.3.tar.gz/pymyku-0.2.3/pymyku/type.py
@@ -0,0 +1,8 @@
+from enum import Enum, EnumMeta
+from typing import Any, Dict, List, Optional, Union
+
+from requests import Response
+
+class ClientType(object):
+    '''Abstract class for client'''
+    pass
