f073124bd63482889201d856ab527493a39a7f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:09 +0000

    Add package pxi4jss/0.2.5/pxi4jss-0.2.5.tar.gz

---
+++ packages/pxi4jss/pxi4jss-0.2.5.tar.gz/pxi4jss-0.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name="pxi4jss",
+    packages=find_packages(),
+    install_requires=['snakes', 'snakes-utils>=2.0.2',
+                      'dimod', 'dwave-neal', 'pandas'],
+    version="0.2.5",
+    license="MIT",
+    author='kanekou',
+    author_email='k208580@ie.u-ryukyu.ac.jp',
+    url='https://github.com/kanekou/pxi4jss',
+    description='It is modeling tool for automatically generating Ising models from JSS Petri nets.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
