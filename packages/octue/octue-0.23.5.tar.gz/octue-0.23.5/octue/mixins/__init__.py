478fe1fe6b901d60f3054f43bf86b504fb13364a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:52 +0000

    Add package octue/0.23.5/octue-0.23.5.tar.gz

---
+++ packages/octue/octue-0.23.5.tar.gz/octue-0.23.5/octue/mixins/__init__.py
@@ -0,0 +1,22 @@
+from .base import MixinBase
+from .cool_nameable import CoolNameable
+from .filterable import Filterable
+from .hashable import Hashable
+from .identifiable import Identifiable
+from .labelable import Labelable
+from .metadata import Metadata
+from .serialisable import Serialisable
+from .taggable import Taggable
+
+
+__all__ = (
+    "CoolNameable",
+    "Filterable",
+    "Hashable",
+    "Identifiable",
+    "Labelable",
+    "Metadata",
+    "MixinBase",
+    "Serialisable",
+    "Taggable",
+)
