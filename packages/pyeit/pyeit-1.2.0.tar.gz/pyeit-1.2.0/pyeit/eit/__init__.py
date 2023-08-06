b5cc3330b1ffddd80e9213bb9f5ce3319f0fadc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:52 +0000

    Add package pyeit/1.2.0/pyeit-1.2.0.tar.gz

---
+++ packages/pyeit/pyeit-1.2.0.tar.gz/pyeit-1.2.0/pyeit/eit/__init__.py
@@ -0,0 +1,18 @@
+"""
+Reconstruction algorithms for EIT.
+
+Arithmetic
+----------
+  - fem: Finite-element method
+  - base: EitBase class
+  - bp: Back-projection
+  - jac: Jacobian matrix based method
+  - greit: The GREIT algorithm
+  - utils: EIT related helper function
+  - interp2d: Spatial interpolation for EIT
+"""
+from .bp import BP
+
+__all__ = [
+    "BP",
+]
---
+++ packages/pyeit/pyeit-1.2.0.tar.gz/pyeit-1.2.0/pyeit/eit/svd.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+# pylint: disable=invalid-name, no-member, arguments-differ
+""" dynamic EIT solver using SVD """
+# Copyright (c) Benyuan Liu. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+from __future__ import division, absolute_import, print_function
+
+import numpy as np
+
+from .jac import JAC
+
+
+class SVD(JAC):
+    """implementing a sensitivity-based EIT imaging class"""
+
+    def setup(self, n: int = 25, rcond: float = 1e-2, method: str = "svd") -> None:
+        """
+        Setup of SVD solver, singular value decomposition based reconstruction.
+
+        Parameters
+        ----------
+        n : int, optional
+            largest n eigenvalues to be kept, by default 25
+        rcond : float, optional
+            r-condition number of pinv, by default 1e-2
+        method : str, optional
+            reconstruction method, by default "svd"
+            'svd': SVD truncation,
+            'pinv': pseudo inverse
+        """
+        # correct n_ord
+        self.J, self.v0 = self.fwd.compute_jac()
+        nm, ne = self.J.shape
+        n_ord = np.min([nm, ne, n])
+
+        # passing imaging parameters
+        self.params = {"n": n_ord, "rcond": rcond, "method": method}
+
+        # pre-compute H0 for dynamical imaging
+        if method == "pinv":
+            self.H = np.linalg.pinv(self.J, rcond=rcond)
+
+        elif method == "svd":
+            JtJ = np.dot(self.J.T, self.J)
+
+            # using svd
+            # U, s, Ut = np.linalg.svd(JtJ)
+            # U = U[:, :n_ord]
+            # s = s[:n_ord]
+
+            # using eigh (more faster for large, symmetric matrix)
+            s, U = np.linalg.eigh(JtJ)
+            idx = np.argsort(s)[::-1]
+            s = s[idx[:n_ord]]
+            U = U[:, idx[:n_ord]]
+
+            # pseudo inverse
+            JtJ_inv = np.dot(U, np.dot(np.diag(s**-1), U.T))
+            self.H = np.dot(JtJ_inv, self.J.T)
+        self.is_ready = True
+
+    def gn(self):
+        """deactivate gn"""
+        raise NotImplementedError()
+
+    def solve_gs(self):
+        """deactivate solve_gs"""
+        raise NotImplementedError()
+
+    def jt_solve(self):
+        """deactivate jt_solve"""
+        raise NotImplementedError()
