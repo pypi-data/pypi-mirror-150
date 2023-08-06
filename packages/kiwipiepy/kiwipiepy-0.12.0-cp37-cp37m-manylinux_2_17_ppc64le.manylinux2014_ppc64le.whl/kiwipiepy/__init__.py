36926dcdeda5a39e61c5d26d41961cd7fc04fa91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:45 +0000

    Add package kiwipiepy/0.12.0/kiwipiepy-0.12.0-cp37-cp37m-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/kiwipiepy/kiwipiepy-0.12.0-cp37-cp37m-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/kiwipiepy/__init__.py
@@ -0,0 +1,11 @@
+"""
+.. include:: ./documentation.md
+"""
+from _kiwipiepy import Token
+from kiwipiepy._version import __version__
+from kiwipiepy._wrap import Kiwi, Sentence
+import kiwipiepy.utils as utils
+from kiwipiepy.const import Match, Option
+
+Kiwi.__module__ = 'kiwipiepy'
+Sentence.__module__ = 'kiwipiepy'
