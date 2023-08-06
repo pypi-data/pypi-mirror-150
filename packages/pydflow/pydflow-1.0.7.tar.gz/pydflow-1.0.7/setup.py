8faab961c9270d8d430d094cb827beafa4ced56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:35 +0000

    Add package pydflow/1.0.7/pydflow-1.0.7.tar.gz

---
+++ packages/pydflow/pydflow-1.0.7.tar.gz/pydflow-1.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='pydflow',
+    version='1.0.7',
+    description='Dflow is a concurrent learning framework based on Argo Workflows.',
+    author="Xinzijian Liu",
+    license="LGPLv3",
+    package_dir={'': 'src'},
+    packages=[
+        "dflow",
+        "dflow/python",
+        "dflow/client"
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "six",
+        "python-dateutil",
+        "urllib3",
+        "certifi",
+        "typeguard",
+        "argo-workflows==5.0.0",
+        "jsonpickle",
+        "minio",
+        "kubernetes",
+        "pyyaml"
+    ]
+)
