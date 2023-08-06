61cb1ebcc1aa85fa38f28e30bd2b09ac598199ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:46:49 +0000

    Add package pyqt-checkbox-list-widget/0.0.1/pyqt-checkbox-list-widget-0.0.1.tar.gz

---
+++ packages/pyqt-checkbox-list-widget/pyqt-checkbox-list-widget-0.0.1.tar.gz/pyqt-checkbox-list-widget-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='pyqt-checkbox-list-widget',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PyQt QListWidget for checkable items',
+    url='https://github.com/yjg30737/pyqt-checkbox-list-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'PyQt5>=5.8',
+        'pyqt-tooltip-list-widget>=0.0.1'
+    ]
+)
