aee341b97a7972d3fe26d2ba3723c9475f161aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:53 +0000

    Add package gradio/3.0b6/gradio-3.0b6.tar.gz

---
+++ packages/gradio/gradio-3.0b6.tar.gz/gradio-3.0b6/gradio/templates/frontend/assets/color.a159a634.js
@@ -0,0 +1,1 @@
+import{a9 as o}from"./index.b05fc228.js";const t=r=>o[r%o.length];export{t as g};
---
+++ packages/gradio/gradio-3.0b6.tar.gz/gradio-3.0b6/gradio/templates/frontend/assets/index.d2d42252.js
@@ -0,0 +1,1 @@
+import{M as s}from"./index.b05fc228.js";const o=["static"];export{s as Component,o as modes};
