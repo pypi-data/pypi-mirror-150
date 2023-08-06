636c5fcd18464a14bc0d8fc6e1d4af14eff6474c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:46 +0000

    Add package asking-more-questions/0.1.1/asking-more-questions-0.1.1.tar.gz

---
+++ packages/asking-more-questions/asking-more-questions-0.1.1.tar.gz/asking-more-questions-0.1.1/asking_more_questions/ask.py
@@ -0,0 +1,13 @@
+import pyodbc as odbc
+import pandas as pd
+
+from typing import Union
+from . connections import Names
+from . connections import connection_get
+
+def query(sql: str, name: Union[Names, str]) -> pd.DataFrame:
+    name = name.value if isinstance(name, Names) else name 
+    connection = connection_get(name)
+
+    with odbc.connect(*connection) as conn:
+        return pd.read_sql(sql, conn)
---
+++ packages/asking-more-questions/asking-more-questions-0.1.1.tar.gz/asking-more-questions-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['asking_more_questions']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['sqcli = asking_more_questions.client:app']}
+
+setup_kwargs = {
+    'name': 'asking-more-questions',
+    'version': '0.1.1',
+    'description': 'A collection of tools that make ad-hoc queries faster',
+    'long_description': None,
+    'author': 'Kim TImothy Engh',
+    'author_email': 'kimothy@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
