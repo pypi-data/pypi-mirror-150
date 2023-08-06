02dbb0d4da12f43486e90faa0dc72ddeb4392015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:46 +0000

    Add package install-name/0.0.3/install_name-0.0.3-py3-none-any.whl

---
+++ packages/install-name/install_name-0.0.3-py3-none-any.whl/import_name/__init__.py
@@ -0,0 +1,9 @@
+# from .main import main
+# from .main import main_func
+from .model import model
+__version__ = 777
+
+def hello_world():
+    print('hello_world')
+
+print('update_002')
---
+++ packages/install-name/install_name-0.0.3-py3-none-any.whl/import_name/run_main.py
@@ -0,0 +1,9 @@
+
+
+
+import sys
+sys.path.append('../')
+
+from import_name.main import main
+main()
+print('update_001')
