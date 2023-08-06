de1742c5bf92ba83cb28ca15a062c78a178941c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:06 +0000

    Add package cusfbamboo/0.2.4/cusfbamboo-0.2.4.tar.gz

---
+++ packages/cusfbamboo/cusfbamboo-0.2.4.tar.gz/cusfbamboo-0.2.4/setup.py
@@ -0,0 +1,33 @@
+"""
+Allows installation via pip, e.g. by navigating to this directory with the command prompt, and using 'pip install .'
+"""
+
+import sys
+from setuptools import setup, find_packages
+
+# Make sure to include the readme
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='cusfbamboo',
+    author = 'Daniel Gibbons',                 
+    author_email = 'daniel.u.gibbons@gmail.com',        
+    version = '0.2.4',
+    license = '	AGPL-3.0',
+    packages = find_packages(),
+    install_requires = ['numpy', 'matplotlib', 'scipy'],
+    description = 'Cooling system modelling for liquid rocket engines',
+    keywords = ['rocket', 'engine', 'liquid', 'cooling', 'spaceflight', 'thermal'],
+    download_url = 'https://github.com/cuspaceflight/bamboo/archive/refs/tags/0.2.4.tar.gz',
+    url = 'https://github.com/cuspaceflight/bamboo',
+    classifiers = [
+        'Development Status :: 4 - Beta',     
+        'Intended Audience :: Science/Research',    
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU Affero General Public License v3',   
+        'Programming Language :: Python :: 3'],
+    long_description = long_description,
+    long_description_content_type='text/markdown'
+)
