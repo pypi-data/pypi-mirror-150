0197b4fe6bfd4d48111e6dcce9a1a79186d17af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:50 +0000

    Add package pmps/0.2/pmps-0.2.tar.gz

---
+++ packages/pmps/pmps-0.2.tar.gz/pmps-0.2/pmps/api/add.py
@@ -0,0 +1,40 @@
+"""
+add module
+"""
+from pmps.core.add import add as core_add
+
+
+def add(num1: int, num2: int) -> int:
+    """
+    Add up two integer numbers.
+
+    This function simply wraps the ``+`` operator, and does not
+    do anything interesting, except for illustrating what
+    the docstring of a very simple function looks like.
+
+    Parameters
+    ----------
+    num1
+        First number to add.
+    num2
+        Second number to add.
+
+    Returns
+    -------
+    int
+        The sum of ``num1`` and ``num2``.
+
+    See Also
+    --------
+    subtract : Subtract one integer from another.
+
+    Examples
+    --------
+    >>> add(2, 2)
+    4
+    >>> add(25, 0)
+    25
+    >>> add(10, -10)
+    0
+    """
+    return core_add(num1, num2)
