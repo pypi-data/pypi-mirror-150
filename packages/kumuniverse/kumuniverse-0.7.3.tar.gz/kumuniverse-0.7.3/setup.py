45ccd335e70b25d332ea1cf860d07b2464874a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:08 +0000

    Add package kumuniverse/0.7.3/kumuniverse-0.7.3.tar.gz

---
+++ packages/kumuniverse/kumuniverse-0.7.3.tar.gz/kumuniverse-0.7.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+
+VERSION = "0.7.3"
+
+DESCRIPTION = "Data team shared library for accessing services"
+
+setup(
+    name="kumuniverse",
+    version=VERSION,
+    description=DESCRIPTION,
+    author="Renz Abergos",
+    author_email="renz@kumu.ph",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "pymongo",
+        "pymongo[srv]",
+        "pymongo[aws]",
+        "requests",
+        "UnleashClient",
+        "boto3",
+        "pandas",
+    ],
+    extras_require={"databricks": ["pyspark"], "sagemaker": ["sagemaker", "redis"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
