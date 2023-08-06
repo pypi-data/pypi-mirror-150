f12a30169dccbae80f4c542356ba4b672a4413cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:14 +0000

    Add package larky/0.1.0/larky-0.1.0-py2.py3-none-any.whl

---
+++ packages/larky/larky-0.1.0-py2.py3-none-any.whl/larky/__init__.py
@@ -0,0 +1,5 @@
+"""larky - VGS edition of Google's safe and hermetically sealed Starlark language - a non-Turing complete and less dynamic subset of Python 3"""
+
+__version__ = '0.1.0'
+__author__ = 'Very Good Security, Inc. <dev@verygoodsecurity.com>'
+__all__ = []
