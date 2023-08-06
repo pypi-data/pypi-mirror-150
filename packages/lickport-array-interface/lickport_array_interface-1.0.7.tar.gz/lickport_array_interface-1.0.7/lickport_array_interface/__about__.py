69ed0d709e46b471c0aeba8bbbf35a136ee67f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:13 +0000

    Add package lickport-array-interface/1.0.7/lickport_array_interface-1.0.7.tar.gz

---
+++ packages/lickport-array-interface/lickport_array_interface-1.0.7.tar.gz/lickport_array_interface-1.0.7/lickport_array_interface/__about__.py
@@ -0,0 +1,7 @@
+__version__ = '1.0.7'
+__summary__ = 'Python interface to the Janelia Dudman lab mouse lickport array.'
+__license__ = 'BSD 3-Clause License'
+__url__ = 'https://github.com/janelia-pypi/lickport_array_interface_python'
+__author__ = 'Peter Polidoro'
+__email__ = 'peter@polidoro.io'
+__copyright__ = '2022 Howard Hughes Medical Institute'
