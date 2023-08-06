240c40a403a2cb49ff6061c6ee70dc78647097a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:20 +0000

    Add package antistasi_sqf_tools/0.3.3/antistasi_sqf_tools-0.3.3.tar.gz

---
+++ packages/antistasi_sqf_tools/antistasi_sqf_tools-0.3.3.tar.gz/antistasi_sqf_tools-0.3.3/antistasi_sqf_tools/__main__.py
@@ -0,0 +1,48 @@
+"""
+WiP.
+
+Soon.
+"""
+
+# region [Imports]
+
+
+from pathlib import Path
+
+
+import click
+from antistasi_sqf_tools.doc_creating import add_doc_sub_group
+# endregion[Imports]
+
+# region [TODO]
+
+
+# endregion [TODO]
+
+# region [Logging]
+
+
+# endregion[Logging]
+
+# region [Constants]
+
+THIS_FILE_DIR = Path(__file__).parent.absolute()
+
+# endregion[Constants]
+
+
+@click.group(name="antistasi-sqf-tools")
+@click.help_option("-h", "--help")
+def antistasi_sqf_tools_cli():
+    ...
+
+
+add_doc_sub_group(antistasi_sqf_tools_cli)
+
+
+# region[Main_Exec]
+
+if __name__ == '__main__':
+    pass
+
+# endregion[Main_Exec]
