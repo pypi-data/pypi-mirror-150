30f1819029ee8a813dc4ae5af0d8dc8784802c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:24 +0000

    Add package clibf/1.1.1/clibf-1.1.1.tar.gz

---
+++ packages/clibf/clibf-1.1.1.tar.gz/clibf-1.1.1/clibf/__main__.py
@@ -0,0 +1,22 @@
+from _clibf import run
+from questionary import path
+from argparse import ArgumentParser
+def main():
+    parser = ArgumentParser(description = 'Brainfuck input')
+    group = parser.add_mutually_exclusive_group(required = True)
+    group.add_argument('-g', '--gui', action='store_true')
+    group.add_argument('-s', '--string', metavar='', type = str)
+    group.add_argument('-f', '--file', metavar='', type = str)
+    group.add_argument('-F', '--file-prompt', action='store_true')
+    args = parser.parse_args()
+    string = args.string
+    file = args.file if not args.file_prompt else path('Brainfuck file').ask()
+    if args.gui:
+        from tkinter import Tk
+        from tkinter.filedialog import askopenfilename
+        Tk().withdraw()
+        file = askopenfilename(filetypes = [("Brainfuck files", "*.b")])
+    if file is None: run(string)
+    else: run(open(file).read())
+    print() # Newline
+if __name__ == '__main__': main()
