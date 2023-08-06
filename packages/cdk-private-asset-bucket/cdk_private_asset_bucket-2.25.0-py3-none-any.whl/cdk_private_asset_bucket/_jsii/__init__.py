fa6fe2443ed3f37039ff465c4741b650d824bd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:53 +0000

    Add package cdk-private-asset-bucket/2.25.0/cdk_private_asset_bucket-2.25.0-py3-none-any.whl

---
+++ packages/cdk-private-asset-bucket/cdk_private_asset_bucket-2.25.0-py3-none-any.whl/cdk_private_asset_bucket/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-private-asset-bucket",
+    "2.25.0",
+    __name__[0:-6],
+    "cdk-private-asset-bucket@2.25.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
