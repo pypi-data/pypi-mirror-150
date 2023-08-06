60859d3636f08a0aa601594711bff246ac8304aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:04 +0000

    Add package extaasy/0.1.0/extaasy-0.1.0.tar.gz

---
+++ packages/extaasy/extaasy-0.1.0.tar.gz/extaasy-0.1.0/connect_lib/flows/request_flow.py
@@ -0,0 +1,21 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect EaaS Extension Library.
+#
+# Copyright (c) 2022 Ingram Micro. All Rights Reserved.
+#
+
+"""
+This module implements the Request base flow
+"""
+
+from .base_flow import BaseFlow
+from ..request import Request
+
+
+class RequestFlow(BaseFlow):
+
+    def __init__(self, request, client, logger, config):
+        super().__init__(request, client, logger, config)
+
+        # Deserialize request
+        self.request_data = Request(**request)
---
+++ packages/extaasy/extaasy-0.1.0.tar.gz/extaasy-0.1.0/connect_lib/flows/validation_flow.py
@@ -0,0 +1,16 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect EaaS Extension Library.
+#
+# Copyright (c) 2022 Ingram Micro. All Rights Reserved.
+#
+
+"""
+This module implements the Validation base flow
+"""
+
+from .request_flow import RequestFlow
+
+
+class ValidationFlow(RequestFlow):
+    def __init__(self, request, client, logger, config):
+        super().__init__(request, client, logger, config)
