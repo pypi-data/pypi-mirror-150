c772cd48becef78ad956ed3075a7920f32dd695f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:47:40 +0000

    Add package maestral/1.6.3.dev0/maestral-1.6.3.dev0.tar.gz

---
+++ packages/maestral/maestral-1.6.3.dev0.tar.gz/maestral-1.6.3.dev0/src/maestral/fsevents/__init__.py
@@ -0,0 +1,22 @@
+"""
+This module provides a custom polling file system event emitter for the
+:obj:`watchdog` package that sorts file system events in an order which can be applied
+to reproduce the new state from the old state. This is only required for the polling
+emitter which uses period directory snapshots and compares them with a
+:class:`watchdog.utils.dirsnapshot.DirectorySnapshotDiff` to generate file system
+events.
+"""
+
+from watchdog.utils import platform
+from watchdog.utils import UnsupportedLibc
+
+
+if platform.is_linux():
+    try:
+        from watchdog.observers.inotify import InotifyObserver as Observer
+    except UnsupportedLibc:
+        from .polling import OrderedPollingObserver as Observer
+else:
+    from watchdog.observers import Observer
+
+__all__ = ["Observer"]
