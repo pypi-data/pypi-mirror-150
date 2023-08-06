07f34822cfcc246680b45216c78ff1de7cfbff8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:03 +0000

    Add package yolo-as-one/0.4/yolo_as_one-0.4.tar.gz

---
+++ packages/yolo-as-one/yolo_as_one-0.4.tar.gz/yolo_as_one-0.4/src/installation_scripts/yolov5.sh
@@ -0,0 +1,16 @@
+
+
+git clone https://github.com/ultralytics/yolov5.git
+
+cd yolov5
+
+python3 -m venv linvenv
+
+. linvenv/bin/activate
+
+pip3 install --upgrade pip
+
+pip3 install -r requirements.txt
+
+pip3 install pycocotools
+
---
+++ packages/yolo-as-one/yolo_as_one-0.4.tar.gz/yolo_as_one-0.4/src/yolo_as_one/install_models.py
@@ -0,0 +1,22 @@
+
+import argparse
+from subprocess import call
+
+model_names = ['yolox', 'yolor', 'yolov5']
+
+def parse_args():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--model_name', default='None')
+    return parse.parse_args()
+
+
+def install_models(model_name):
+    if model_name in model_names:
+        call(f"{model_name}.sh", shell=True)
+    else:
+        print('model not currently available')
+    pass
+
+if __name__=='__main__':
+    args = parse_args()
+    install_models(args.model_name)
