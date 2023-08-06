841451e9d7ee5a4724ffdb99d1c034a233b4ee8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:15 +0000

    Add package ds-io-utilities/0.0.3/ds_io_utilities-0.0.3-py3-none-any.whl

---
+++ packages/ds-io-utilities/ds_io_utilities-0.0.3-py3-none-any.whl/dsioutilities/tabular/filesystem.py
@@ -0,0 +1,10 @@
+from bdaserviceutils import get_cmd_arg
+
+# def get_output_dataset_path():
+#     return get_cmd_arg("output-dataset")
+    
+# def get_input_dataset_path():
+#     return get_cmd_arg("input-dataset")
+
+def get_path(name):
+    return get_cmd_arg(name)
