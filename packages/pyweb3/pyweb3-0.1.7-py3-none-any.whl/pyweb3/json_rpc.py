a61f9c51bfa5771abd4fcfbfa722c016c430c38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:22 +0000

    Add package pyweb3/0.1.7/pyweb3-0.1.7-py3-none-any.whl

---
+++ packages/pyweb3/pyweb3-0.1.7-py3-none-any.whl/pyweb3/json_rpc.py
@@ -0,0 +1,126 @@
+# -*- coding: utf8 -*-
+
+# pyWeb3 : JSON RPC
+# Copyright (C) 2021-2022 BitLogiK
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have receive a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+"""JSON RPC for pyWeb3"""
+
+from time import sleep
+from logging import getLogger
+from json import dumps, loads
+from .http_client import HttpClient
+from .websocket import WebSocketClient
+
+
+class JSONRPCexception(Exception):
+    """Exception when the Web3 call has no result, but an error field."""
+
+
+logger = getLogger(__name__)
+
+DEFAULT_USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) " "Gecko/20100101 Firefox/96.0"
+)
+
+
+# ---- Helpers about messages encoding
+
+
+def json_encode(dataobj):
+    """Compact JSON encoding."""
+    return dumps(dataobj, separators=(",", ":"))
+
+
+# ---- JSON RPC functions to pack/unpack
+
+
+def json_rpc_pack_response(idmsg, result_obj):
+    """Build a JSON-RPC response."""
+    request_obj = {
+        "jsonrpc": "2.0",
+        "id": idmsg,
+        "result": result_obj,
+    }
+    return json_encode(request_obj).encode("utf8")
+
+
+def json_rpc_unpack(buffer):
+    """Decode a JSON-RPC call query : id, method, params."""
+    try:
+        resp_obj = loads(buffer)
+    except Exception as exc:
+        raise Exception(f"Error : not JSON response : {buffer}") from exc
+    if resp_obj["jsonrpc"] != "2.0":
+        raise Exception(f"Server is not JSONRPC 2.0 but {resp_obj.jsonrpc}")
+    if "error" in resp_obj:
+        raise JSONRPCexception(resp_obj["error"])
+    return resp_obj["id"], resp_obj["result"]
+
+
+class JSONRPCclient:
+    """WebSocket and HTTPS JSON-RPC client"""
+
+    def __init__(self, url_api, user_agent, retries):
+        if user_agent is None:
+            user_agent = DEFAULT_USER_AGENT
+        if url_api.startswith("wss:"):
+            self.cnx = WebSocketClient(url_api, user_agent)
+        elif url_api.startswith("https:"):
+            self.cnx = HttpClient(url_api, user_agent)
+        else:
+            raise Exception("Only accept HTTPS and WebSocket connection scheme")
+        self.retry = retries
+        self.req_id = 0
+
+    def send_request(self, method_name, params=None):
+        """Send a JSON RPC request"""
+        if params is None:
+            params = []
+        self.req_id += 1
+        request_obj = {
+            "jsonrpc": "2.0",
+            "id": self.req_id,
+            "method": method_name,
+            "params": params,
+        }
+        logger.log(5, "Sending RPC request method:%s with data:%s", method_name, params)
+        self.cnx.send_message(json_encode(request_obj).encode("utf8"))
+
+    def get_response(self):
+        """Listen to response, expect same id as the latest request sent"""
+        self.cnx.get_messages()
+        msg = self.cnx.received_messages.pop()
+        reqid, result = json_rpc_unpack(msg)
+        logger.log(5, "Received RPC result: %s", result)
+        if reqid != self.req_id:
+            raise Exception("JSON RPC response id mismatch")
+        return result
+
+    def request(self, method_name, params=None):
+        """Send a RPC query and listen to response"""
+        if params is None:
+            params = []
+        for nret in range(self.retry + 1):
+            try:
+                self.send_request(method_name, params)
+                resp = self.get_response()
+                return resp
+            except KeyboardInterrupt as exc:
+                raise exc
+            except Exception as exc:
+                if self.retry and nret < self.retry:
+                    logger.log(5, "Retry %i", nret + 1)
+                    sleep(0.5)
+                else:
+                    raise exc
---
+++ packages/pyweb3/pyweb3-0.1.7-py3-none-any.whl/pyweb3/web3client.py
@@ -0,0 +1,76 @@
+# -*- coding: utf8 -*-
+
+# pyWeb3 : Web3 client
+# Copyright (C) 2021-2022 BitLogiK
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have receive a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+"""Web3 RPC client"""
+
+
+from .json_rpc import JSONRPCclient
+
+
+class Web3Client:
+    """Web3 RPC client"""
+
+    def __init__(self, node_url, user_agent=None, retries=2):
+        self.jsonrpc = JSONRPCclient(node_url, user_agent, retries)
+
+    def get_balance(self, address, state="latest"):
+        """Get native token balance"""
+        balraw = self.jsonrpc.request("eth_getBalance", [address, state])
+        if balraw and len(balraw) >= 2 and balraw[:2] == "0x":
+            return int(balraw[2:], 16)
+        return 0
+
+    def call(self, contract, command_code, data="", state="latest"):
+        """eth call query"""
+        # https://eth.wiki/json-rpc/API#eth_call
+        # The following state options are possible :
+        #   HEX String - an integer block number
+        #   String "earliest" for the earliest/genesis block
+        #   String "latest" for the latest mined block
+        #   String "pending" for the pending state/transactions
+        datab = f"0x{command_code}{data}"
+        return self.jsonrpc.request(
+            "eth_call", [{"to": contract, "data": datab}, state]
+        )
+
+    def pushtx(self, txhex):
+        """Upload a transaction"""
+        return self.jsonrpc.request("eth_sendRawTransaction", ["0x" + txhex])
+
+    def get_tx_num(self, addr, state="latest"):
+        """Read number of transaction done by this address"""
+        tx_count_raw = self.jsonrpc.request(
+            "eth_getTransactionCount", ["0x" + addr, state]
+        )
+        if tx_count_raw and len(tx_count_raw) >= 2 and tx_count_raw[:2] == "0x":
+            return int(tx_count_raw[2:], 16)
+        raise Exception("Bad data when reading getTransactionCount")
+
+    def get_gasprice(self):
+        """Get the gas price in wei units"""
+        gas_price_raw = self.jsonrpc.request("eth_gasPrice")
+        if gas_price_raw and len(gas_price_raw) >= 2 and gas_price_raw[:2] == "0x":
+            return int(gas_price_raw[2:], 16)
+        raise Exception("Bad data when reading gasPrice")
+
+    def get_logs(self, param):
+        return self.jsonrpc.request("eth_getLogs", [param])
+
+    def set_filter(self, param):
+        return self.jsonrpc.request("eth_newFilter", [param])
+
+    def get_filter(self, filter_id):
+        return self.jsonrpc.request("eth_getFilterLogs", [filter_id])
