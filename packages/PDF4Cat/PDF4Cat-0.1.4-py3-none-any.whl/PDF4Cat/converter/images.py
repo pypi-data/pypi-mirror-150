4775c51b4192eb86b27e2c92772904e7bf867277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:08 +0000

    Add package PDF4Cat/0.1.4/PDF4Cat-0.1.4-py3-none-any.whl

---
+++ packages/PDF4Cat/PDF4Cat-0.1.4-py3-none-any.whl/PDF4Cat/converter/images.py
@@ -0,0 +1,45 @@
+import os
+import io
+import zipfile
+from PIL import Image
+
+from ..cat import Base
+
+class Img2Pdf(Base):
+	def __init__(self, *args, **kwargs):
+		super(Img2Pdf, self).__init__(*args, **kwargs)
+
+	@Base.run_in_subprocess
+	def img2pdf(self, output_pdf = None):
+		if not output_pdf:
+			output_pdf = os.path.join(self.doc_path, self.doc_name+"_out.pdf")
+
+		Image.open(self.doc_file).convert('RGB').save(output_pdf)
+
+	# Generate name with BytesIO object (it is faster)
+	def gen_images(self, fimages, start_from) -> tuple:
+		for num, img in enumerate(self.input_doc_list): ###
+			io_data = io.BytesIO()
+			img_ext = os.path.splitext(img)[1][1:]
+			Image.open(img).convert('RGB').save(io_data, format=img_ext)
+
+			imfn = fimages.format(name=os.path.basename(img), num=num+start_from)
+			imfi = io_data
+			yield imfn, imfi
+
+	@Base.run_in_subprocess
+	def imgs2pdf_zip(self, 
+		out_zip_file: str, 
+		fimages: str = '{name}_{num}.pdf',
+		start_from: int = 0) -> None:
+
+		# Compression level: zipfile.ZIP_DEFLATED (8) and disable ZIP64 ext.
+		with zipfile.ZipFile(out_zip_file, 'w', zipfile.ZIP_DEFLATED, False) as zf:
+
+			for file_name, io_data in self.gen_images(fimages, start_from):
+				zf.writestr(file_name, io_data.getvalue())
+				self.counter += 1 #need enumerate
+				self.progress_callback(self.counter, len(self.input_doc_list))
+
+		self.counter = 0
+
---
+++ packages/PDF4Cat/PDF4Cat-0.1.4-py3-none-any.whl/PDF4Cat/crypt.py
@@ -0,0 +1,41 @@
+import os
+
+from .cat import PDF4Cat
+
+class Crypter(PDF4Cat):
+	def __init__(self, *args, **kwargs):
+		super(Crypter, self).__init__(*args, **kwargs)
+
+	# @PDF4Cat.run_in_subprocess
+	def crypt_to(self, user_passwd: str = None, 
+		owner_passwd: str = None, 
+		perm: dict = None, 
+		R: int = 4, 
+		output_pdf: str = None) -> None:
+
+		if not output_pdf:
+			output_pdf = os.path.join(self.pdf_path, self.pdf_name+"_out.pdf")
+
+		if not perm:
+			perm = {'extract': False,}
+
+		if not user_passwd:
+			user_passwd = owner_passwd
+		elif not user_passwd:
+			owner_passwd = user_passwd
+		elif not user_passwd and not owner_passwd:
+			raise TypeError("Missing user or owner password!")
+
+
+		self.pdf.save(output_pdf, encryption=self.pdfEncryption(
+			user=user_passwd, owner=owner_passwd, R=R,
+			allow=self.pdfPermissions(**perm)
+			))
+
+	@PDF4Cat.run_in_subprocess
+	def decrypt_to(self, output_pdf = None) -> None: # like save
+		if not output_pdf:
+			output_pdf = os.path.join(self.pdf_path, self.pdf_name+"_out.pdf")
+
+		self.pdf.save(output_pdf)
+
