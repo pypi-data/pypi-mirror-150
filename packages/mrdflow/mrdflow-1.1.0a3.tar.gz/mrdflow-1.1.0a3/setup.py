5c3f08382699de0cb6731e2ff4016a3e270f58a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:05 +0000

    Add package mrdflow/1.1.0a3/mrdflow-1.1.0a3.tar.gz

---
+++ packages/mrdflow/mrdflow-1.1.0a3.tar.gz/mrdflow-1.1.0a3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mrdflow',
+    version='1.1.0a3',
+    description=(
+        'This is a network.'
+    ),
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    author='Zhou Chengyu',
+    author_email='earuil@outlook.com',
+    maintainer='Zhou Chengyu',
+    maintainer_email='earuil@outlook.com',
+    license='MIT License',
+    packages = find_packages(),
+    platforms=["all"],
+    url='https://github.com/Zhou-chengy/mrdflow/',
+    install_requires=[
+        "numpy",
+    ]
+)
