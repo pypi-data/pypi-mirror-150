a54f6a66ed78ba993a4d68b8c66d68a3cefba231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:06 +0000

    Add package Sergei-JWT/0.4/Sergei_JWT-0.4-py3-none-any.whl

---
+++ packages/Sergei-JWT/Sergei_JWT-0.4-py3-none-any.whl/authentication_app/utils/userConfig.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+
+
+try:
+    Outstanding_REQUIRED = False
+    Outstanding_REQUIRED = settings.SERGEI['DATA_STORE']['POSTGRESQL']['OutstandingToken']
+except KeyError:
+    pass
+
+try:
+    Blacklist_REQUIRED = False
+    Blacklist_REQUIRED = settings.SERGEI['DATA_STORE']['POSTGRESQL']['BlacklistToken']
+except KeyError:
+    pass
+
+try:
+    USE_REDIS = False
+    USE_REDIS = settings.SERGEI['DATA_STORE']['REDIS']['REQUIRED']
+except KeyError:
+    pass
+
+
+def getPref():
+    """
+     Helper function to resolve userpreference for outstanding and blacklist schema generation from settings files
+     returns -> Outstanding_REQUIRED, Blacklist_REQUIRED
+     """
+    pref = dict()
+    pref['Outstanding_REQUIRED'] = Outstanding_REQUIRED
+    pref['Blacklist_REQUIRED'] = Blacklist_REQUIRED
+    pref['USE_REDIS'] = USE_REDIS
+    return pref
