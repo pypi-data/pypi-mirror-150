b13497a4fd37df5c7f45c22604f997b277ab748f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:28 +0000

    Add package sphinxcontrib-needs/0.7.9/sphinxcontrib_needs-0.7.9-py3-none-any.whl

---
+++ packages/sphinxcontrib-needs/sphinxcontrib_needs-0.7.9-py3-none-any.whl/sphinxcontrib/needs/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+from .configuration import (
+    add_dynamic_function,
+    add_extra_option,
+    add_need_type,
+    get_need_types,
+)
+from .need import add_external_need, add_need, del_need, make_hashed_id
