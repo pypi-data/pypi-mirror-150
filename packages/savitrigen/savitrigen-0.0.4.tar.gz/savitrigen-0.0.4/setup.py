92bccd3c9a1aa654cf9baa25c666e2c122aa37c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:24 +0000

    Add package savitrigen/0.0.4/savitrigen-0.0.4.tar.gz

---
+++ packages/savitrigen/savitrigen-0.0.4.tar.gz/savitrigen-0.0.4/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import runpy
+
+from setuptools import setup
+
+def get_version():
+    filename = os.path.join(os.path.dirname(__file__), 'savitrigen', '__init__.py')
+    var = runpy.run_path(filename)
+    return var['__version__']
+
+_VERSION = get_version()
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='savitrigen',
+    version=_VERSION,
+    author='João Santos',
+    author_email='joaosan177@gmail.com',
+    description='Savitri code generator',
+    loing_description=long_description,
+    long_description_content_type='text/x-rst',
+    packages=['savitrigen'],
+    license='ISC',
+    install_requires=[
+        'multipledispatch==0.6.0',
+        'pyyaml==6.0'
+    ]
+)
