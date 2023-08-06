17c34d1d4f6c2ccb65e58a040e8c4fbf36379619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:27 +0000

    Add package django-webpack-pages/0.0.1/django_webpack_pages-0.0.1-py3-none-any.whl

---
+++ packages/django-webpack-pages/django_webpack_pages-0.0.1-py3-none-any.whl/webpack_pages/jinja2ext.py
@@ -0,0 +1,24 @@
+from jinja2.ext import Extension
+from jinja2.utils import pass_context
+
+from .templatetags import (
+    asset_url,
+    inline_entrypoint,
+    register_entrypoint,
+    render_css,
+    render_js,
+)
+
+
+class WebpackExtension(Extension):
+    def __init__(self, environment):
+        super(WebpackExtension, self).__init__(environment)
+        environment.globals.update(
+            {
+                "register_entrypoint": pass_context(register_entrypoint),
+                "render_css": pass_context(render_css),
+                "render_js": pass_context(render_js),
+                "inline_entrypoint": inline_entrypoint,
+                "asset": pass_context(asset_url),
+            }
+        )
