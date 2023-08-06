bf06d9282afa8b8c50016b3cf6f3eaa8d4a70399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:30 +0000

    Add package natlog/1.0.3/natlog-1.0.3.tar.gz

---
+++ packages/natlog/natlog-1.0.3.tar.gz/natlog-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import setuptools
+
+import natlog
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+version = natlog.__version__
+setup(name='natlog',
+      version=version,
+      description='Prolog-like interpreter and tuple store',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ptarau/minlog.git',
+      author='Paul Tarau',
+      author_USER_EMAIL='<paul.tarau@gmail.com>',
+      license='Apache',
+      packages=setuptools.find_packages(),
+      #scripts=['bin/*'],
+      package_data={'natlog': [
+                               'doc/*',
+                               'natlog/natprogs/*',
+                               ]
+                    },
+      include_package_data=True,
+      install_requires=required,
+      zip_safe=False
+      )
