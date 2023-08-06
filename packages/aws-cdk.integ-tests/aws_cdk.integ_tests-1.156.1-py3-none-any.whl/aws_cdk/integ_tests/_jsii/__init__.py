29c8021e716a7dec79478a1bb3b9dfee21ef9b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:01 +0000

    Add package aws-cdk.integ-tests/1.156.1/aws_cdk.integ_tests-1.156.1-py3-none-any.whl

---
+++ packages/aws-cdk.integ-tests/aws_cdk.integ_tests-1.156.1-py3-none-any.whl/aws_cdk/integ_tests/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.assertions._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.cloud_assembly_schema._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import aws_cdk.cx_api._jsii
+import aws_cdk.triggers._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/integ-tests", "1.156.1", __name__[0:-6], "integ-tests@1.156.1.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
