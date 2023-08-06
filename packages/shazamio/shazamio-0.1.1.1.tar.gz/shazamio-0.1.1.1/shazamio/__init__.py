160e46d8baae441361128b01bb3a74948097b397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:24 +0000

    Add package shazamio/0.1.1.1/shazamio-0.1.1.1.tar.gz

---
+++ packages/shazamio/shazamio-0.1.1.1.tar.gz/shazamio-0.1.1.1/shazamio/__init__.py
@@ -0,0 +1,11 @@
+from .serializers import Serialize
+from .api import Shazam
+from .converter import Geo
+from .enums import GenreMusic
+
+__all__ = (
+    'Serialize',
+    'Shazam',
+    'Geo',
+    'GenreMusic'
+)
