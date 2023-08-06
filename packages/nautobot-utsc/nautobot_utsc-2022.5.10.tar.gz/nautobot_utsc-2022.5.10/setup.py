12644b85d8958ebf2add4c5c6e9396c21062b186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:19 +0000

    Add package nautobot-utsc/2022.5.10/nautobot_utsc-2022.5.10.tar.gz

---
+++ packages/nautobot-utsc/nautobot_utsc-2022.5.10.tar.gz/nautobot_utsc-2022.5.10/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['nautobot_utsc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['debugpy',
+ 'django-auth-ldap',
+ 'django-debug-toolbar',
+ 'django-environ',
+ 'html-table-parser-python3>=0.2.0',
+ 'nautobot>=1.3',
+ 'nautobot_ssot>=1.1.0',
+ 'utsc.core']
+
+setup_kwargs = {
+    'name': 'nautobot-utsc',
+    'version': '2022.5.10',
+    'description': 'A collection of UTSC-specific modifications and extensions to Nautobot.',
+    'long_description': None,
+    'author': 'Alex Tremblay',
+    'author_email': 'alex.tremblay@utoronto.ca',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
