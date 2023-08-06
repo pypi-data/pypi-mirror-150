b60d0380852cc828c2ee966d2d57430d0437e6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:09 +0000

    Add package ts-tariffs/3.2.2/ts_tariffs-3.2.2.tar.gz

---
+++ packages/ts-tariffs/ts_tariffs-3.2.2.tar.gz/ts_tariffs-3.2.2/ts_tariffs/meters.py
@@ -0,0 +1,239 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import timedelta, datetime, time
+from typing import Union, Optional, List
+from copy import deepcopy, copy
+
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+
+from ts_tariffs.ts_utils import period_cascades_map, TimeWindow, FrequencyOption, SampleRate, DateWindow, DatetimeWindow
+from ts_tariffs.utils import EnforcedDict
+
+
+class Validator:
+    @staticmethod
+    def index_as_dt(consumption: Union[pd.Series, pd.DataFrame]):
+        if not isinstance(consumption.index.dtype, datetime):
+            raise ValueError('DataFrames and Series index must be dtype datetime')
+
+
+@dataclass
+class MeterData:
+    name: str
+    tseries: pd.Series
+    sample_rate: Union[timedelta, SampleRate]
+    units: str
+
+    """ Representation of data from an interval metering device, i.e. a
+    meter that records data at regular time intervals
+
+    Common examples:
+        - electricity smart meter with consumption at 30 minute intervals
+        - gas meter with daily consumption data
+    """
+
+    def first_datetime(self) -> datetime:
+        return self.tseries.first_valid_index()
+
+    def last_datetime(self) -> datetime:
+        return self.tseries.last_valid_index()
+
+    def timedelta_covered(self) -> timedelta:
+        return self.last_datetime() - self.first_datetime()
+
+    def first_day_slice(self) -> slice:
+        start_dt = self.first_datetime()
+        end_dt = start_dt + timedelta(days=1)
+        return slice(start_dt, end_dt)
+
+    def last_day_slice(self) -> slice:
+        end_dt = self.last_datetime()
+        start_dt = end_dt - timedelta(days=1)
+        return slice(start_dt, end_dt)
+
+    def first_week_slice(self):
+        start_dt = self.first_datetime()
+        end_dt = start_dt + timedelta(weeks=1)
+        return slice(start_dt, end_dt)
+
+    def last_week_slice(self) -> slice:
+        end_dt = self.last_datetime()
+        start_dt = end_dt - timedelta(weeks=1)
+        return slice(start_dt, end_dt)
+
+    def first_day(self) -> pd.Series:
+        return self.tseries[self.first_day_slice()]
+
+    def last_day(self) -> pd.Series:
+        return self.tseries[self.last_day_slice()]
+
+    def first_week(self) -> pd.Series:
+        return self.tseries[self.first_week_slice()]
+
+    def last_week(self) -> pd.Series:
+        return self.tseries[self.last_week_slice()]
+
+    def between(self, start: datetime, end: datetime) -> pd.Series:
+        return self.tseries[slice(start, end)]
+
+    def max(self) -> float:
+        return self.tseries.max()
+
+    def min(self) -> float:
+        return self.tseries.min()
+
+    def max_between(self, start: datetime, end: datetime) -> float:
+        return self.tseries[slice(start, end)].max()
+
+    def min_between(self, start: datetime, end: datetime) -> float:
+        return self.tseries[slice(start, end)].min()
+
+    def window_covered(
+            self,
+            window: Union[DateWindow, DatetimeWindow]
+    ) -> bool:
+        window_covered = True
+        if isinstance(window, DateWindow):
+            # Start and end of window at the beginning and end of the
+            # start and end dates, respectively
+            start_of_first_day = datetime.combine(window.start, time(0))
+            end_of_last_day = \
+                datetime.combine(window.end, time(0)) + \
+                timedelta(days=1) - \
+                self.sample_rate
+            if start_of_first_day < self.first_datetime():
+                window_covered = False
+            if end_of_last_day > self.last_datetime():
+                window_covered = False
+        elif isinstance(window, DatetimeWindow):
+            if window.start < self.first_datetime():
+                window_covered = False
+            if window.end > self.last_datetime():
+                window_covered = False
+        else:
+            raise TypeError(f'Wrong type recieved: {window.__class__.__name__}. '
+                            f'The window param must be a DateWindow or DatetimeWindow')
+        return window_covered
+
+    def window_slice(
+            self,
+            window: Union[DateWindow, DatetimeWindow]
+    ) -> pd.Series:
+        return self.between(window.start, window.end)
+
+    def period_slice(
+            self,
+            from_dt: datetime,
+            period_freq: FrequencyOption,
+            number_periods: float,
+    ) -> pd.Series:
+        # Pluralise period so that relativedelta adds to datetime, rather than replaces freq attr
+        period_freq += 's'
+        to_dt = from_dt + relativedelta(**{period_freq: number_periods * period_freq})
+        return self.between(from_dt, to_dt)
+
+    def kwh_to_kw(self, inplace=False) -> Optional[MeterData]:
+        if self.units != 'kWh':
+            raise ValueError(f'Can only convert from kWh. Units are set as {self.units} not kWh. ')
+        if inplace:
+            self.tseries /= self.sample_rate / timedelta(hours=1)
+            self.units = 'kW'
+        else:
+            new_meter = self.copy()
+            new_meter.tseries = new_meter.tseries / (new_meter.sample_rate / timedelta(hours=1))
+            new_meter.units = 'kW'
+            return new_meter
+
+    def groupby_freq_stats(
+            self,
+            frequency: FrequencyOption,
+            within_window: Union[DateWindow, DatetimeWindow] = None,
+            within_times: TimeWindow = None,
+            stats: Union[str, List[str]] = 'max'
+    ):
+        """ Perform groupby at specific frequency and for any number
+        of specific aggregation statistics
+        e.g. max(), min(), mean() at week, month, year
+
+        To constrain time period pass DateWindow or DatetimeWindow
+        """
+        if isinstance(stats, str):
+            stats = [stats]
+        if within_window:
+            ts = self.window_slice(within_window)
+        else:
+            ts = self.tseries
+        if within_times:
+            ts = ts.between_time(within_times.start, within_times.end, inclusive='left')
+        period_cascade = period_cascades_map[frequency]
+        period_bins = list([getattr(ts.index, period) for period in period_cascade])
+        return ts.groupby(period_bins).agg(stats).rename_axis(period_cascade)
+
+    def year_peaks(self) -> pd.Series:
+        return self.groupby_freq_stats(frequency='year', stats='max')
+
+    def month_peaks(self):
+        return self.groupby_freq_stats(frequency='month', stats='max')
+
+    def year_sum(self) -> pd.Series:
+        return self.groupby_freq_stats(frequency='year', stats='sum')
+
+    def month_sum(self):
+        return self.groupby_freq_stats(frequency='month', stats='sum')
+
+    def period_peaks(
+            self,
+            frequency: FrequencyOption,
+            within_window: Union[DateWindow, DatetimeWindow] = None,
+            within_times: TimeWindow = None
+    ) -> pd.Series:
+        return self.groupby_freq_stats(
+            frequency,
+            within_window,
+            within_times,
+            'max'
+        )
+
+    def period_sum(
+            self,
+            frequency: FrequencyOption,
+            within_window: Union[DateWindow, DatetimeWindow] = None,
+            within_times: TimeWindow = None
+    ) -> pd.Series:
+        return self.groupby_freq_stats(
+            frequency,
+            within_window,
+            within_times,
+            'sum'
+        )
+
+    def to_numpy(self):
+        return self.tseries.to_numpy(dtype=float)
+
+    def copy(self, deep=True):
+        if deep:
+            return deepcopy(self)
+        else:
+            return copy(self)
+
+
+@dataclass
+class Meters(EnforcedDict):
+    def __init__(
+            self,
+            data: dict = None,
+    ):
+        super().__init__(
+            data,
+            key_type=str,
+            value_type=MeterData
+        )
+
+    @property
+    def meters_by_unit(self):
+        return {meter.units: meter for meter in self.values()}
+
+    def append(self, meter: MeterData):
+        self[meter.name] = meter
---
+++ packages/ts-tariffs/ts_tariffs-3.2.2.tar.gz/ts_tariffs-3.2.2/ts_tariffs/tariffs.py
@@ -0,0 +1,296 @@
+import warnings
+from abc import ABC, abstractmethod
+from types import MappingProxyType
+
+import pandas as pd
+import numpy as np
+from datetime import timedelta
+from typing import (
+    List,
+    Union, Optional
+)
+from dataclasses import dataclass, field
+
+from ts_tariffs.meters import MeterData
+from ts_tariffs.ts_utils import FrequencyOption, resample_schema, TouBins, TimeWindow, SampleRate, DatetimeWindow, \
+    DateWindow
+from ts_tariffs.units import ConsumptionUnitOption
+from ts_tariffs.utils import Block
+
+
+@dataclass(frozen=True)
+class AppliedCharge:
+    """ Charges related to a tariff and given consumption meter data
+    """
+    name: str
+    charge_ts: Union[pd.DataFrame, pd.Series]
+    rate_unit: str
+    consumption_units: str
+    total: float
+
+
+@dataclass
+class Tariff(ABC):
+    name: str
+    charge_type: str
+    consumption_unit: ConsumptionUnitOption
+    rate_unit: str
+    sample_rate: Union[SampleRate, timedelta]
+    adjustment_factor: Optional[float]
+
+    def __post_init__(self):
+        if not self.adjustment_factor:
+            self.adjustment_factor = 1.0
+
+    @abstractmethod
+    def apply(
+            self,
+            consumption: MeterData,
+    ) -> AppliedCharge:
+        pass
+
+    @classmethod
+    def from_dict(cls, tariff_dict: dict):
+        return cls(**tariff_dict)
+
+
+@dataclass
+class SingleRateTariff(Tariff):
+    """ Single charge per unit of consumption
+    """
+    rate: float
+
+    def apply(
+            self,
+            consumption: MeterData,
+    ) -> AppliedCharge:
+
+        charge_vector = consumption.tseries * self.adjustment_factor * self.rate
+
+        return AppliedCharge(
+            self.name,
+            charge_vector,
+            self.rate_unit,
+            consumption.units,
+            sum(charge_vector)
+        )
+
+
+@dataclass
+class ConnectionTariff(Tariff):
+    """ Charge applied for having service - applied periodically
+    """
+    rate: float
+    frequency_applied: FrequencyOption
+
+    def apply(
+            self,
+            consumption: MeterData,
+    ) -> AppliedCharge:
+        cost_ts = consumption.tseries.resample(
+            resample_schema[self.frequency_applied]
+        ).sum()
+        cost_ts = pd.DataFrame(cost_ts)
+        cost_ts['periods'] = 1.0
+        cost_ts['charge'] = self.rate * cost_ts['periods']
+        cost_ts[f'rate ({self.rate_unit})'] = self.rate
+        return AppliedCharge(
+            self.name,
+            cost_ts,
+            self.rate_unit,
+            consumption.units,
+            sum(cost_ts['charge'])
+        )
+
+
+@dataclass
+class TouTariff(Tariff):
+    """ Variable charge rate depending on time of day
+    """
+    tou: TouBins
+
+    def __post_init__(self):
+        if isinstance(self.tou, dict):
+            self.tou = TouBins(**self.tou)
+
+    def apply(
+            self,
+            consumption: MeterData,
+    ) -> AppliedCharge:
+        prices = np.array(self.tou.bin_rates)
+        bins = np.digitize(
+            consumption.tseries.index.hour.values,
+            bins=self.tou.time_bins,
+        )
+        charge_vector = prices[bins] * consumption.to_numpy()
+        cost_ts = pd.DataFrame(consumption.tseries)
+        cost_ts['charge'] = charge_vector
+        cost_ts[f'rate ({self.rate_unit})'] = prices[bins]
+
+        return AppliedCharge(
+            self.name,
+            cost_ts,
+            self.rate_unit,
+            consumption.units,
+            sum(charge_vector)
+        )
+
+
+@dataclass
+class DemandTariff(Tariff):
+    """ Charge applied to the peak consumption value for a given period
+    May additionally be specific to times of day (e.g. peak in a month between 3pm and 6pm)
+    """
+    rate: float
+    frequency_applied: str
+    time_window: TimeWindow = None
+
+    def apply(
+            self,
+            consumption: MeterData,
+    ) -> AppliedCharge:
+        peaks = consumption.period_peaks(
+            self.frequency_applied,
+            within_times=self.time_window
+        )
+        charge_vector = peaks * self.rate
+        return AppliedCharge(
+            self.name,
+            charge_vector,
+            self.rate_unit,
+            consumption.units,
+            charge_vector.sum()
+        )
+
+
+@dataclass
+class BlockTariff(Tariff):
+    """ Variable charge applied to sum of consumption for a given period,
+    between an upper and lower threshold
+    (e.g. $0.10/kWh for the first 1000kWh per month, $0.20/kWh for the second 1000kWh in same month)
+
+    """
+    frequency_applied: str
+    blocks: List[Block]
+    bin_rates: List[float]
+    bin_labels: List[str]
+
+    def apply(
+            self,
+            consumption: MeterData,
+    ) -> AppliedCharge:
+        period_sums = consumption.period_sum(self.frequency_applied)
+        charge_ts = pd.DataFrame(period_sums)
+        charge_ts['charge_total'] = 0.0
+        for j, block in enumerate(self.blocks):
+            rate = self.bin_rates[j]
+            consumption_in_block = np.clip(
+                period_sums,
+                block.min,
+                block.max
+            ) - block.min
+            charge_col_name = f'block_{j + 1}_charge'
+            charge_ts[charge_col_name] = rate * consumption_in_block * self.adjustment_factor
+            charge_ts[f'block_{j + 1}_rate ({self.rate_unit})'] = rate
+            charge_ts['charge_total'] += charge_ts[charge_col_name]
+
+        return AppliedCharge(
+            self.name,
+            charge_ts,
+            self.rate_unit,
+            consumption.units,
+            sum(charge_ts['charge_total'])
+        )
+
+
+@dataclass
+class CapacityTariff(Tariff):
+    """ Essentially a connection tariff that is multiplied by a specific capacity
+    """
+    capacity: float
+    rate: float
+    frequency_applied: FrequencyOption
+
+    def apply(
+            self,
+            consumption: MeterData,
+    ) -> AppliedCharge:
+        cost_ts = consumption.tseries.resample(
+            resample_schema[self.frequency_applied]
+        ).sum()
+        cost_ts = pd.DataFrame(cost_ts)
+        cost_ts['periods'] = self.capacity
+        cost_ts['charge'] = self.rate * cost_ts['periods']
+        cost_ts[f'rate ({self.rate_unit})'] = self.rate
+        return AppliedCharge(
+            self.name,
+            cost_ts,
+            self.rate_unit,
+            consumption.units,
+            sum(cost_ts['charge'])
+        )
+
+
+@dataclass
+class CriticalPeakDemandTariff(Tariff):
+    """ Charge calculated by multiplying the tariff rate by the
+    average of peak demands during the critical time window on each
+    nominated critical demand days
+    """
+    rate: float
+    frequency_applied: FrequencyOption
+    period_active: DateWindow
+    critical_period: DateWindow
+    critical_peak_windows: List[DatetimeWindow]
+
+    def apply(
+            self,
+            consumption: MeterData,
+    ) -> AppliedCharge:
+
+        if not consumption.window_covered(self.critical_period):
+            warnings.warn(
+                f'The critical period tariff, {self.name}, was not applied '
+                f'because the consumption MeterData did not cover the full'
+                f' critical period (must include full days within period)\n'
+                f'Period covered by consumption: '
+                f'{consumption.first_datetime().strftime("%Y-%m-%d %H:%M")} to {consumption.last_datetime().strftime("%Y-%m-%d %H:%M")} \n'
+                f'Critical time period coverage: '
+                f'{self.critical_period.start.strftime("%Y-%m-%d")} to {self.critical_period.end.strftime("%Y-%m-%d")}',
+            )
+
+        if not consumption.window_covered(self.period_active):
+            warnings.warn(
+                f'The critical period tariff, {self.name}, was not applied to the full period_active window'
+                f' because the consumption MeterData did not cover the full window'
+            )
+
+        # Check critical peak windows are in critical period
+        mean_of_peaks = np.mean([
+            consumption.max_between(*window.as_tuple)
+            for window in self.critical_peak_windows
+        ])
+        charge = mean_of_peaks * self.rate
+
+        # Get index grouped by frequency_applied period
+        charge_df = pd.DataFrame(index=consumption.groupby_freq_stats(
+            frequency=self.frequency_applied,
+            within_window=self.period_active
+            ).index
+        )
+        charge_df['charge'] = charge
+
+        return AppliedCharge(
+            self.name,
+            charge_df['charge'],
+            self.rate_unit,
+            consumption.units,
+            sum(charge_df['charge'])
+        )
+
+
+# All tariffs should be added here - map facilitates
+# multi tarif instantiation via dicts etc
+tariffs_map = MappingProxyType(
+    {tariff.__name__: tariff for tariff in Tariff.__subclasses__()}
+)
