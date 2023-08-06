2c032a3777e2e67c9aa547f4073d73187a2d6bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:14 +0000

    Add package django-inet/1.1.1/django_inet-1.1.1-py3-none-any.whl

---
+++ packages/django-inet/django_inet-1.1.1-py3-none-any.whl/django_inet/rest.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from .models import IPAddressValidator, IPNetworkValidator
+
+
+class IPAddressField(serializers.CharField):
+    version = 0
+
+    def __init__(self, **kwargs):
+        self.version = kwargs.pop("version", None)
+        super().__init__(**kwargs)
+        self.validators.append(IPAddressValidator(self))
+
+
+class IPNetworkField(serializers.CharField):
+    version = 0
+
+    def __init__(self, **kwargs):
+        self.version = kwargs.pop("version", None)
+        super().__init__(**kwargs)
+        self.validators.append(IPNetworkValidator(self))
+
+
+# Alias to original name
+IPPrefixField = IPNetworkField
