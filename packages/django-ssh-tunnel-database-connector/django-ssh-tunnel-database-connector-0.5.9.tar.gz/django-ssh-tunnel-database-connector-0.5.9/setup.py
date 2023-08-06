9022e688203bc6fb966237bf5d89372c64da4545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:33 +0000

    Add package django-ssh-tunnel-database-connector/0.5.9/django-ssh-tunnel-database-connector-0.5.9.tar.gz

---
+++ packages/django-ssh-tunnel-database-connector/django-ssh-tunnel-database-connector-0.5.9.tar.gz/django-ssh-tunnel-database-connector-0.5.9/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='django-ssh-tunnel-database-connector',
+    packages=['DjangoSSHTunnelDatabaseConnector'],
+    version='0.5.9',
+    license='MIT',
+    description='This is a library that enables one to use django models to do CRUD operations on a database ' +
+                'hosted remotely and accessible through ssh in a pythonic way',
+    long_description="Detailed documentation can be found at: " +
+                     "https://github.com/Arnie09/DjangoSSHTunnelDatabaseConnector",
+    author='Arnab Chanda',
+    author_email='arnabchanda@gmail.com',
+    url='https://github.com/Arnie09/DjangoSSHTunnelDatabaseConnector',
+    download_url='https://github.com/Arnie09/DjangoSSHTunnelDatabaseConnector/archive/refs/tags/0.1.tar.gz',
+    keywords=['Django', 'sshtunnel', 'database'],
+    install_requires=[
+        'pymysql',
+        'sshtunnel'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
