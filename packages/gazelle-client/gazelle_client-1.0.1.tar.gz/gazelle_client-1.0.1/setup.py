d5f0d7f8f9bd964aa08aa11910cd98a94c7a01f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:50 +0000

    Add package gazelle-client/1.0.1/gazelle_client-1.0.1.tar.gz

---
+++ packages/gazelle-client/gazelle_client-1.0.1.tar.gz/gazelle_client-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+# Requirements
+requirements = [
+    "requests",
+    "appdirs"
+]
+
+
+setup(
+    name="gazelle_client",
+    version="1.0.1",
+    description="Python API for Gazelle",
+    url="https://github.com/hibikidesu/gazelle_api_py",
+    author="Hibiki",
+    license_files=("LICENSE",),
+    py_modules=["gazelle_client"],
+    python_requires=">=3.7",
+    install_requires=requirements,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+    ]
+)
