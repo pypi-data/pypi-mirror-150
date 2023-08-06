4feca57b506b97018fb95a13134c024a246cb4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:40 +0000

    Add package mamimo/0.1.2/mamimo-0.1.2.tar.gz

---
+++ packages/mamimo/mamimo-0.1.2.tar.gz/mamimo-0.1.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mamimo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['isort>=5.10.1,<6.0.0',
+ 'numpy>=1.22.3,<2.0.0',
+ 'pandas>=1.4.2,<2.0.0',
+ 'pre-commit>=2.18.1,<3.0.0',
+ 'pytest>=7.1.2,<8.0.0',
+ 'scikit-learn>=1.0.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'mamimo',
+    'version': '0.1.2',
+    'description': 'A package to compute a marketing mix model.',
+    'long_description': None,
+    'author': 'Garve',
+    'author_email': 'xgarve@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
