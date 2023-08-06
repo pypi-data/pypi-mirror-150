a529c32d92bf5eae6281b1eddf22028e73c20e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:17 +0000

    Add package fivesimapi/0.1.8/fivesimapi-0.1.8.tar.gz

---
+++ packages/fivesimapi/fivesimapi-0.1.8.tar.gz/fivesimapi-0.1.8/FiveSimApi/exceptions.py
@@ -0,0 +1,10 @@
+class NumberNotAvailable(Exception):
+    """Raised when a phone number is not provided."""
+
+
+class InvalidApiKey(Exception):
+    """Raised when an invalid API key is provided."""
+
+
+class InvalidInput(Exception):
+    """Raised when multiple errors are raised."""
