fc5982a1ee1f8df605b518cd563e2abf276fe410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:31 +0000

    Add package PyTaskbarProgress/0.0.2/PyTaskbarProgress-0.0.2.tar.gz

---
+++ packages/PyTaskbarProgress/PyTaskbarProgress-0.0.2.tar.gz/PyTaskbarProgress-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import sys
+import shutil
+
+# Copy the PyTaskbarProgress/TaskbarLib.tlb file to the dist folder
+
+def copy_tlb(dst):
+    shutil.copy("PyTaskbarProgress\\TaskbarLib.tlb", dst)
+
+sitepackages_path = sys.prefix + '\\Lib\\site-packages'
+libpath = sitepackages_path + '\\PyTaskbarProgress\\TaskbarLib.tlb'.replace('\\', '/')
+
+setuptools.setup(
+    name='PyTaskbarProgress',
+    version='0.0.2',
+    author='somePythonProgrammer',
+    description='The ultimate taskbar progress python package!',
+    packages = setuptools.find_packages(include=['PyTaskbarProgress']),
+    package_dir={'PyTaskbarProgress': 'PyTaskbarProgress'},
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'comtypes',
+        'setuptools',
+    ],
+    include_package_data=True,
+    url = 'https://github.com/somePythonProgrammer/PyTaskbar',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+)
