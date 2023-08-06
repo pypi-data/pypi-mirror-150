9b12bf7a958b98560c9e3784b5cd182ed7cc135c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:07 +0000

    Add package asking-more-questions/0.1.3/asking_more_questions-0.1.3-py3-none-any.whl

---
+++ packages/asking-more-questions/asking_more_questions-0.1.3-py3-none-any.whl/asking_more_questions/ask.py
@@ -0,0 +1,18 @@
+import pyodbc as odbc
+import pandas as pd
+
+from typing import Union
+from typing import Optional
+
+from . connections import Names
+from . connections import connection_get
+
+
+
+def query(sql: str, name: Union[Names, str]) -> pd.DataFrame:
+    name = name.value if isinstance(name, Names) else name
+    
+    connection = connection_get(name)
+
+    with odbc.connect(DSN=connection.dsn, UID=connection.usr, PWD=connection.pwd) as conn:
+        return pd.read_sql(sql, conn)
