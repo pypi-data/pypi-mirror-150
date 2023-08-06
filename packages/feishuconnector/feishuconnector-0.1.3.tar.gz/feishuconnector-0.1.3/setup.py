69d0c6ffde05cf655bc0a01a8645b84afba74cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:53 +0000

    Add package feishuconnector/0.1.3/feishuconnector-0.1.3.tar.gz

---
+++ packages/feishuconnector/feishuconnector-0.1.3.tar.gz/feishuconnector-0.1.3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+from feishuconnector import __version__
+
+
+setup(
+    name='feishuconnector',
+    version=__version__,
+    description=(
+        'connect feishu content franchise'
+    ),
+    long_description=open('README.rst').read(),
+    author='Changhao Jiang',
+    author_email='jch@puyuan.tech',
+    url='http://www.puyuan.tech',
+    license='MIT License',
+    packages=find_packages(),
+    platforms=["all"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'requests >= 2.26.0',
+        'numpy >= 1.21.2',
+        'pandas >= 1.3.3',
+        'requests-toolbelt >= 0.9.1',
+        'dataframe-image-cn >= 0.1.1',
+    ],
+)
