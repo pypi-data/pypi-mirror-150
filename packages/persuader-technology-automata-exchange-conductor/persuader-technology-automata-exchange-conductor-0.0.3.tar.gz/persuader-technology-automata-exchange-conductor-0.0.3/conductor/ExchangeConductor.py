2383164313b20668db10f480b28b58735f3bb0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:04 +0000

    Add package persuader-technology-automata-exchange-conductor/0.0.3/persuader-technology-automata-exchange-conductor-0.0.3.tar.gz

---
+++ packages/persuader-technology-automata-exchange-conductor/persuader-technology-automata-exchange-conductor-0.0.3.tar.gz/persuader-technology-automata-exchange-conductor-0.0.3/conductor/ExchangeConductor.py
@@ -0,0 +1,23 @@
+import logging
+
+from conductor.instrument.InstrumentExchangeHandler import InstrumentExchangeHandler
+from conductor.provider.ExchangeDataProvider import ExchangeDataProvider
+from conductor.transform.ExchangeTransformer import ExchangeTransformer
+
+
+class ExchangeConductor:
+
+    def __init__(self, options, transformer: ExchangeTransformer, data_provider: ExchangeDataProvider, handler: InstrumentExchangeHandler):
+        self.options = options
+        self.transformer = transformer
+        self.data_provider = data_provider
+        self.handler = handler
+
+    def get_instrument_exchanges(self):
+        instrument_exchanges_holder = self.handler.obtain_holder()
+        exchange_instruments_payload = self.data_provider.fetch_exchange_instruments()
+        logging.info(f'Fetched raw exchange instruments[{len(exchange_instruments_payload)}]')
+        for exchange_instrument_data in exchange_instruments_payload:
+            instrument_exchange = self.transformer.transform(exchange_instrument_data)
+            instrument_exchanges_holder.add(instrument_exchange)
+        self.handler.update_holder(instrument_exchanges_holder)
