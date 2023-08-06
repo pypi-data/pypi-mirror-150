b3ad31dd7707716fbe4eda98fa49d74292f410f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:23 +0000

    Add package space-classy/0.2.4/space-classy-0.2.4.tar.gz

---
+++ packages/space-classy/space-classy-0.2.4.tar.gz/space-classy-0.2.4/classy/logging.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+from rich.logging import RichHandler
+from rich.traceback import install
+
+# pretty-print tracebacks with rich
+install()
+
+FORMAT = "%(message)s"
+
+
+def init_logging(level):
+    """
+    Set the global logging level.
+
+    Parameters
+    ----------
+    level : int
+        The logging level, logging everything [0] to nothing [5]. Default is 2.",
+    """
+    logging.basicConfig(
+        level=level * 10,
+        format=FORMAT,
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True, show_path=False, show_time=False)],
+    )
+
+    # Set the same logging level for tensorflow
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = str(level)
