4628665e9e9a3e8768d3a729eb0cd7745653c749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:25:44 +0000

    Add package insight-gateway-python-v4.0.5/4.0.5/insight_gateway_python_v4.0.5-4.0.5.tar.gz

---
+++ packages/insight-gateway-python-v4.0.5/insight_gateway_python_v4.0.5-4.0.5.tar.gz/insight_gateway_python_v4.0.5-4.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages
+from setuptools import setup
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="insight_gateway_python_v4.0.5",
+    author="htsc",
+    version="4.0.5",
+    author_email="insight@htsc.com",
+    description="insight_gateway_python_4.0.5",
+    long_description="insight_gateway_python_v4.0.5",
+    license='insightpythonsdk',
+    project_urls={
+        'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+        'Funding': 'https://donate.pypi.org',
+        'Source': 'https://github.com/pypa/sampleproject/',
+        'Tracker': 'https://github.com/pypa/sampleproject/issues',
+    },
+
+    package_dir={'': 'com'},
+    packages= find_packages('com'),
+    install_requires=['numpy>=1.14'],
+
+    python_requires='>=3.7',
+)
