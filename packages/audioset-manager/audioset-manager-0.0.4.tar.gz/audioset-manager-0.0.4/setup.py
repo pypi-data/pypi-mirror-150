28f3be4d3ff9c2c444f8f8faafa380ab37d32a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:24 +0000

    Add package audioset-manager/0.0.4/audioset-manager-0.0.4.tar.gz

---
+++ packages/audioset-manager/audioset-manager-0.0.4.tar.gz/audioset-manager-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="audioset-manager", # Replace with your own username
+    version="0.0.4",
+    author="Muhammad Umar Ali",
+    author_email="umaruali@student.ubc.ca",
+    description="A simple python package for managing the audio data from Google Research's ontology of 632 audio event classes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gitUmaru/AudioSet-Data-Manager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+       "pydub",
+       "youtubedl",
+       "pandas"
+   ]
+)
