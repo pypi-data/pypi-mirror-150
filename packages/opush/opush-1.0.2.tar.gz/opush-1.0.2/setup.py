63602de3520bac4bfaceed8aafa9b14af881db69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:34 +0000

    Add package opush/1.0.2/opush-1.0.2.tar.gz

---
+++ packages/opush/opush-1.0.2.tar.gz/opush-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opush',
+    version='1.0.2',
+    description='Push message to your devices and social accounts easily.',
+    py_modules=['opush']
+)
