868553a6160e78a1bf494ef59d86db36cda2e75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:07 +0000

    Add package vardautomation/1.1.0/vardautomation-1.1.0-py3-none-any.whl

---
+++ packages/vardautomation/vardautomation-1.1.0-py3-none-any.whl/vardautomation/_metadata.py
@@ -0,0 +1,10 @@
+__author__ = 'Ichunjo'
+
+__version__ = '1.1.0'
+__maintainer__ = 'Ichunjo'
+__email__ = 'ichunjo.le.terrible@gmail.com'
+__status__ = 'Development'
+
+
+def version() -> str:
+    return __version__
