c867a1794e42e26fc1b0a93a8adf188fbec10ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:45 +0000

    Add package giant-tour-class/0.4/giant_tour_class-0.4.tar.gz

---
+++ packages/giant-tour-class/giant_tour_class-0.4.tar.gz/giant_tour_class-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+# with open("README.md", "r") as fh:
+#     long_description=fh.read()
+setup(
+    name="giant_tour_class",
+    version="0.4",
+    description="giant tour class",
+    py_modules=["giant_tour_class"],
+    package_dir={"": "src"},
+    classifiers=[
+            "Programming Language :: Python :: 2.7",
+            "License :: OSI Approved :: MIT License",
+    ],
+    # long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["ortools","kmedoids", "math", "numpy"],
+    license="LICENSE.txt",
+    author="Mr. Hanh",
+)
