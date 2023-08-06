3b2e9e8aab3c5dcc48ca8689f4a6ec26aaf3ea3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:09 +0000

    Add package readily/0.0.3/readily-0.0.3.tar.gz

---
+++ packages/readily/readily-0.0.3.tar.gz/readily-0.0.3/README.rst
@@ -0,0 +1,1 @@
+readily is a package for generating chronologically ordered reading lists using a Google Scholar API.
---
+++ packages/readily/readily-0.0.3.tar.gz/readily-0.0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='readily',
+    version='0.0.3',
+    packages=find_packages()  # will return a list ['spam', 'spam.fizz']
+)
