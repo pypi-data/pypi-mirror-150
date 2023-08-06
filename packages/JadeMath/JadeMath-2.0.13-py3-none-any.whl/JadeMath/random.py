7ebb630b94031e74a5d1d9fad660d2cc5d65e585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:38 +0000

    Add package JadeMath/2.0.13/JadeMath-2.0.13-py3-none-any.whl

---
+++ packages/JadeMath/JadeMath-2.0.13-py3-none-any.whl/JadeMath/random.py
@@ -0,0 +1,14 @@
+pi = 3.141592653589
+e = 2.718281
+
+
+
+
+def c_to_f(celcius: float):
+    f = celcius * (9/5) + 32
+    return f
+
+
+def f_to_c(fehrenheit: float):
+    c = (fehrenheit - 32) * 5/9
+    return c
