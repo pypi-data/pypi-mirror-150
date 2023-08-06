bf76071d477b55e80a81054f846a88dec4c4634d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:48 +0000

    Add package altvmasterlist/2.0.0/altvmasterlist-2.0.0.tar.gz

---
+++ packages/altvmasterlist/altvmasterlist-2.0.0.tar.gz/altvmasterlist-2.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="altvmasterlist",
+    version="2.0.0",
+    author="Nickwasused",
+    author_email="contact.nickwasused.fa6c8@simplelogin.co",
+    description="A package to use the alt:V Masterlist api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nickwasused/altv-python-masterlist",
+    project_urls={
+        "Bug Tracker": "https://github.com/Nickwasused/altv-python-masterlist/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
