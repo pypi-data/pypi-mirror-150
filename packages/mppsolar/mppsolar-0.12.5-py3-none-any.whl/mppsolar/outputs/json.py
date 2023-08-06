ab2e178528cacd0ddc0c4d0b8512cbb95e1e9cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:30 +0000

    Add package mppsolar/0.12.5/mppsolar-0.12.5-py3-none-any.whl

---
+++ packages/mppsolar/mppsolar-0.12.5-py3-none-any.whl/mppsolar/outputs/json.py
@@ -0,0 +1,35 @@
+import json as js
+import logging
+import re
+
+from . import to_json
+from .baseoutput import baseoutput
+from ..helpers import get_kwargs
+from ..helpers import key_wanted
+
+log = logging.getLogger("json")
+
+
+class json(baseoutput):
+    def __str__(self):
+        return "outputs the results to standard out in json format"
+
+    def __init__(self, *args, **kwargs) -> None:
+        log.debug(f"__init__: kwargs {kwargs}")
+
+    def output(self, *args, **kwargs):
+        log.info("Using output processor: json")
+        log.debug(f"kwargs {kwargs}")
+        data = get_kwargs(kwargs, "data")
+        keep_case = get_kwargs(kwargs, "keep_case")
+        data.pop("raw_response", None)
+
+        filter = get_kwargs(kwargs, "filter")
+        if filter is not None:
+            filter = re.compile(filter)
+        excl_filter = get_kwargs(kwargs, "excl_filter")
+        if excl_filter is not None:
+            excl_filter = re.compile(excl_filter)
+
+        output = to_json(data, keep_case, excl_filter, filter)
+        print(js.dumps(output))
---
+++ packages/mppsolar/mppsolar-0.12.5-py3-none-any.whl/mppsolar/version.py
@@ -0,0 +1,2 @@
+__version__ = "0.12.5"
+__version_comment__ = "tweak jkbleio for getInfo and to return first 300 bytes"
