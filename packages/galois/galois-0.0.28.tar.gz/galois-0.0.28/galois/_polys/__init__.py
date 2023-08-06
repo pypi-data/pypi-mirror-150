37291e90f4df13f42d2706d300d4cf18ba6af199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:40 +0000

    Add package galois/0.0.28/galois-0.0.28.tar.gz

---
+++ packages/galois/galois-0.0.28.tar.gz/galois-0.0.28/galois/_polys/__init__.py
@@ -0,0 +1,8 @@
+"""
+A subpackage containing arrays over Galois fields.
+"""
+from ._functions import *
+from ._irreducible import *
+from ._poly import *
+from ._primitive import *
+from ._primitive_element import *
