5a45c327f5051df87710af445bc0fcf8b10a528b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:16 +0000

    Add package hspylib-kafman/0.9.28/hspylib-kafman-0.9.28.tar.gz

---
+++ packages/hspylib-kafman/hspylib-kafman-0.9.28.tar.gz/hspylib-kafman-0.9.28/kafman/core/schema/avro_schema.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HSPyLib
+      @file: avro_schema.py
+   @created: Sat, 17 Jul 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2021, HSPyLib team
+"""
+from typing import List, Tuple
+
+from confluent_kafka.schema_registry.avro import AvroDeserializer, AvroSerializer
+from confluent_kafka.serialization import StringDeserializer, StringSerializer
+from hspylib.core.enums.charset import Charset
+from hspylib.core.exception.exceptions import InvalidStateError
+from PyQt5.QtWidgets import QFrame, QGridLayout, QLabel, QStackedWidget, QVBoxLayout, QWidget
+
+from kafman.core.consumer_config import ConsumerConfig
+from kafman.core.producer_config import ProducerConfig
+from kafman.core.schema.field.field_factory import FieldFactory
+from kafman.core.schema.field.record_field import RecordField
+from kafman.core.schema.field.schema_field import SchemaField
+from kafman.core.schema.kafka_schema import KafkaSchema
+from kafman.core.schema.schema_type import SchemaType
+from kafman.core.schema.schema_utils import SchemaUtils
+
+
+class AvroSchema(KafkaSchema):
+    """Apache AVRO schema serializer/deserializer
+       Documentation:
+        - https://avro.apache.org/docs/current/spec.html
+        - https://avro.apache.org/docs/current/gettingstartedpython.html
+       Additional Ref: https://docs.confluent.io/5.3.0/schema-registry/serializer-formatter.html
+
+        E.g:.
+        {
+          "type": "record",
+          "name": "myRecord",
+          "fields": [
+              {"name": "name",  "type": "string" }
+            , {"name": "calories", "type": "float" }
+            , {"name": "colour", "type": "string" }
+          ]
+        }
+    """
+
+    @classmethod
+    def extensions(cls) -> List[str]:
+        return ['*.avsc']
+
+    def __init__(
+        self,
+        filepath: str,
+        registry_url: str,
+        charset: Charset = Charset.UTF_8):
+        super().__init__(SchemaType.AVRO, filepath, registry_url, charset)
+
+    def settings(self) -> dict:
+        """TODO"""
+        return {
+            ProducerConfig.KEY_SERIALIZER: StringSerializer(self._charset.value),
+            ProducerConfig.VALUE_SERIALIZER: AvroSerializer(self._schema_client, self._content_text, self.to_dict),
+            ConsumerConfig.KEY_DESERIALIZER: StringDeserializer(self._charset.value),
+            ConsumerConfig.VALUE_DESERIALIZER: AvroDeserializer(self._schema_client, self._content_text, self.from_dict)
+        }
+
+    def create_schema_form_widget(self, parent: QWidget) -> QStackedWidget:
+        """TODO"""
+        # TODO Create according to descendant fields using QStackedWidget
+        fields = self._attributes.fields
+        self._form_widget = QStackedWidget(parent)
+        if fields and len(fields) > 0:
+            form_pane = QFrame(self._form_widget)
+            layout = QGridLayout(form_pane)
+            form_pane.setLayout(layout)
+            for row, field in enumerate(fields):
+                req_label, label, widget = self \
+                    .create_form_row_widgets(field, self._form_widget)
+                layout.addWidget(label, row, 0)
+                layout.addWidget(req_label, row, 1)
+                layout.addWidget(widget, row, 2)
+        else:
+            form_pane = QFrame(self._form_widget)
+            layout = QVBoxLayout(form_pane)
+            label = QLabel(f'Unable to detect valid fields')
+            label.setStyleSheet('QLabel {color: #FF554D;}')
+            layout.addWidget(label)
+        self._form_widget.addWidget(form_pane)
+
+        return self._form_widget
+
+    def create_form_row_widgets(
+        self,
+        field: SchemaField,
+        stack_widget: QStackedWidget) -> Tuple[QLabel, QLabel, QWidget]:
+        """TODO"""
+
+        self._form_widget = stack_widget
+        label = QLabel(f"{field.name[0].upper() + field.name[1:]}: ")
+        if field.required:
+            req_label = QLabel('*')
+            req_label.setStyleSheet('QLabel {color: #FF554D;}')
+        else:
+            req_label = QLabel(' ')
+        req_label.setToolTip(f"This field is {'required' if field.required else 'optional'}")
+        widget = field.create_input_widget()
+
+        return req_label, label, widget
+
+    def _parse(self) -> None:
+        """TODO"""
+
+        if self._content_text.startswith('{') and self._content_text.endswith('}'):
+            self._schema_name = SchemaUtils.check_and_get('name', self._content_dict)
+            field_type = SchemaUtils.check_and_get('type', self._content_dict)
+            self._attributes.name = self._schema_name
+            self._attributes.namespace = SchemaUtils.check_and_get(
+                'namespace', self._content_dict, required=False)
+            self._attributes.doc = SchemaUtils.check_and_get(
+                'doc', self._content_dict, required=False, default=f'the {self._schema_name}')
+            self._attributes.aliases = SchemaUtils.check_and_get(
+                'aliases', self._content_dict, required=False)
+            if 'record' == field_type:
+                fields = SchemaUtils.check_and_get('fields', self._content_dict)
+                self._attributes.fields = FieldFactory.create_fields(fields)
+        else:
+            raise InvalidStateError(f"UnsupportedSchema: {self._filepath}")
---
+++ packages/hspylib-kafman/hspylib-kafman-0.9.28.tar.gz/hspylib-kafman-0.9.28/kafman/core/schema/widget_utils.py
@@ -0,0 +1,128 @@
+from abc import ABC
+from typing import List, Type, Union
+
+from hspylib.modules.cli.icons.font_awesome.form_icons import FormIcons
+from hspylib.modules.qt.promotions.hcombobox import HComboBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QCheckBox, QDoubleSpinBox, QLineEdit, QSizePolicy, QSpinBox, QToolButton, QWidget
+
+from kafman.core.schema.schema_field_type import SchemaFieldType
+
+
+class WidgetUtils(ABC):
+
+    QWIDGET_TYPE_MAP = {
+        'boolean': QCheckBox,
+        'integer': QSpinBox,
+        'int': QSpinBox,
+        'long': QSpinBox,
+        'float': QDoubleSpinBox,
+        'double': QDoubleSpinBox,
+        'number': QDoubleSpinBox,
+        'bytes': QLineEdit,
+        'string': QLineEdit,
+        'fixed': QLineEdit,
+        'enum': HComboBox,
+        'array': HComboBox,
+        'record': QToolButton,
+        'object': QToolButton,
+        'complex': QToolButton,
+    }
+
+    @staticmethod
+    def get_widget(field_type: Union[str, SchemaFieldType]) -> Type[QWidget]:
+        return WidgetUtils.QWIDGET_TYPE_MAP[field_type.value] \
+            if isinstance(field_type, SchemaFieldType) \
+            else WidgetUtils.QWIDGET_TYPE_MAP[field_type]
+
+    @staticmethod
+    def setup_widget(
+        widget: QWidget,
+        doc: str = None,
+        symbols: list = None,
+        default: Union[str, int, float, bool] = None) -> QWidget:
+        """Return the QWidget that the field is displayed at the schema form"""
+
+        widget_type = widget.__class__
+        tooltip = doc
+        if widget_type == HComboBox:
+            WidgetUtils.setup_combo_box(widget, symbols, tooltip, default)
+        elif widget_type == QCheckBox:
+            WidgetUtils.setup_checkbox(widget, tooltip, default)
+        elif widget_type in [QSpinBox, QDoubleSpinBox]:
+            WidgetUtils.setup_spin_box(widget, tooltip, default)
+        elif widget_type == QLineEdit:
+            WidgetUtils.setup_line_edit(widget, tooltip)
+        elif widget_type == QToolButton:
+            WidgetUtils.setup_tool_button(widget)
+
+        return widget
+
+    @staticmethod
+    def setup_widget_commons(widget: QWidget, tooltip: str) -> QWidget:
+        widget.setToolTip(tooltip)
+        widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        widget.setStyleSheet('QWidget {padding: 5px;}')
+        widget.setFont(QFont("DroidSansMono Nerd Font", 14))
+        widget.setMinimumHeight(35)
+
+        return widget
+
+    @staticmethod
+    def setup_combo_box(
+        widget: QWidget,
+        symbols: List[str],
+        tooltip: str = None,
+        default: str = None) -> QWidget:
+
+        widget.addItems(symbols or [])
+        widget.setEditable(True)
+        widget.lineEdit().setPlaceholderText(tooltip)
+        widget.setCurrentText(default or widget.itemText(0))
+
+        return WidgetUtils.setup_widget_commons(widget, tooltip)
+
+    @staticmethod
+    def setup_checkbox(
+        widget: QWidget,
+        tooltip: str = None,
+        default: Union[int, float] = False) -> QWidget:
+
+        widget.setChecked(default or False)
+
+        return WidgetUtils.setup_widget_commons(widget, tooltip)
+
+    @staticmethod
+    def setup_spin_box(
+        widget: QWidget,
+        tooltip: str = None,
+        default: Union[int, float] = 0) -> QWidget:
+
+        min_val, max_val = 0.0, 9999.999
+        widget.setMinimum(min_val)
+        widget.setMaximum(max_val)
+        widget.setValue(default or 0)
+        widget.setLayoutDirection(Qt.RightToLeft)
+
+        return WidgetUtils.setup_widget_commons(widget, tooltip)
+
+    @staticmethod
+    def setup_line_edit(
+        widget: QWidget,
+        tooltip: str = None,
+        default: str = '') -> QWidget:
+
+        widget.setPlaceholderText(tooltip)
+        widget.setText(default or '')
+
+        return WidgetUtils.setup_widget_commons(widget, tooltip)
+
+    @staticmethod
+    def setup_tool_button(widget: QWidget) -> QWidget:
+        WidgetUtils.setup_widget_commons(widget, "Click to fill")
+        widget.setText(FormIcons.SELECTOR.value)
+        widget.setMaximumWidth(30)
+        widget.setMinimumHeight(30)
+
+        return widget
