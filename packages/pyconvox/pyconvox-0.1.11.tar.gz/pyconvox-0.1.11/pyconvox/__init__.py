b3721ae5182d7a72ba550d0d17ce7967d547b214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:45 +0000

    Add package pyconvox/0.1.11/pyconvox-0.1.11.tar.gz

---
+++ packages/pyconvox/pyconvox-0.1.11.tar.gz/pyconvox-0.1.11/pyconvox/__init__.py
@@ -0,0 +1,22 @@
+__all__ = (
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+)
+
+
+import importlib_metadata
+
+metadata = importlib_metadata.metadata("pyconvox")
+
+
+__title__ = metadata["name"]
+__summary__ = metadata["summary"]
+__version__ = metadata["version"]
+__author__ = metadata["author"]
+__email__ = metadata["author-email"]
+__license__ = metadata["license"]
