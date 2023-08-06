73db91d476f39237f42e953a3d5fedd2a69175e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:40 +0000

    Add package pysustrans/0.0.104/pysustrans-0.0.104.tar.gz

---
+++ packages/pysustrans/pysustrans-0.0.104.tar.gz/pysustrans-0.0.104/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+PACKAGE_NAME = 'pysustrans'
+AUTHOR = 'Rahul Raoniar'
+EMAIL = "rahul.raoniar@outlook.com"
+URL = ''
+DOWNLOAD_URL = ''
+VERSION = '0.0.104'
+DESCRIPTION = 'A transportation data analytics package'
+LONG_DESCRIPTION = 'A package that allows performing common statitical analysis on transportation related data.'
+LICENSE = 'MIT'
+CLASSIFIERS = [
+        'Programming Language :: Python :: 3'
+]
+LONG_DESC_TYPE = "text/markdown"
+
+
+# Setting up
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    license=LICENSE,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESC_TYPE,
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib'],
+    classifiers= CLASSIFIERS,
+)
+
