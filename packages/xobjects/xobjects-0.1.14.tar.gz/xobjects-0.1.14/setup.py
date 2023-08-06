80d7a0df03bb56290ecee5f16e417c5ef051c515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:12 +0000

    Add package xobjects/0.1.14/xobjects-0.1.14.tar.gz

---
+++ packages/xobjects/xobjects-0.1.14.tar.gz/xobjects-0.1.14/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="xobjects",
+    version="0.1.14",
+    description="In-memory serialization and code generator for CPU and GPU",
+    author="Riccardo De Maria",
+    author_email="riccardo.de.maria@cern.ch",
+    url="https://github.com/rdemaria/cobjects",
+    python_requires=">=3.7",
+    setup_requires=[],
+    install_requires=["numpy", "cffi"],
+    packages=["xobjects"],
+)
