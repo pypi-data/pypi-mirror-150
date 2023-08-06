e76627b26a4e7037b8edab360e4ab322f89cdb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:47 +0000

    Add package pyNetatmoHue/0.0.3/pyNetatmoHue-0.0.3.tar.gz

---
+++ packages/pyNetatmoHue/pyNetatmoHue-0.0.3.tar.gz/pyNetatmoHue-0.0.3/setup.py
@@ -0,0 +1,24 @@
+# python setup.py --dry-run --verbose install
+
+from distutils.core import setup
+
+setup(
+    name='pyNetatmoHue',
+    version='0.0.3',
+    classifiers=[        
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+        ],
+    author='Steve de Peijper',
+    author_email='steve.depeijper@gmail.com',
+    py_modules=['pynetatmohue'],
+    scripts=[],
+    install_requires=['python-dateutil',
+      ],
+    data_files=[],
+    url='https://github.com/philippelt/netatmo-api-python',
+    download_url='https://github.com/philippelt/netatmo-api-python/archive/v2.1.0.tar.gz',
+    license='GPL V3',
+    description='Simple API to access Netatmo weather station data from any python script.'
+)
