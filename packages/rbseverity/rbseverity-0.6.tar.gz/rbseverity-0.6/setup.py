79fca02aa244d8bd83e726c5adfc60f73353977b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:14 +0000

    Add package rbseverity/0.6/rbseverity-0.6.tar.gz

---
+++ packages/rbseverity/rbseverity-0.6.tar.gz/rbseverity-0.6/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from reviewboard.extensions.packaging import setup
+
+from rbseverity import get_package_version
+
+
+PACKAGE = 'rbseverity'
+
+setup(
+    name=PACKAGE,
+    version=get_package_version(),
+    description='Comment severity fields for Review Board.',
+    url='http://www.reviewboard.org/',
+    author='Beanbag, Inc.',
+    author_email='support@beanbaginc.com',
+    maintainer='Beanbag, Inc.',
+    maintainer_email='support@beanbaginc.com',
+    packages=['rbseverity'],
+    entry_points={
+        'reviewboard.extensions': [
+            'rbseverity = rbseverity.extension:SeverityExtension',
+        ],
+    },
+    package_data={
+        'rbseverity': [
+            'templates/rbseverity/*.html',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Review Board',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
