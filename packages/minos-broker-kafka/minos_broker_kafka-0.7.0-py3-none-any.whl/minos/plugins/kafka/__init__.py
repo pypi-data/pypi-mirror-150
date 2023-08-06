fd9d565de39b3ebbe70bccc12a47d421a06b3ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:39 +0000

    Add package minos-broker-kafka/0.7.0/minos_broker_kafka-0.7.0-py3-none-any.whl

---
+++ packages/minos-broker-kafka/minos_broker_kafka-0.7.0-py3-none-any.whl/minos/plugins/kafka/__init__.py
@@ -0,0 +1,18 @@
+"""The kafka plugin of the Minos Framework."""
+
+__author__ = "Minos Framework Devs"
+__email__ = "hey@minos.run"
+__version__ = "0.7.0"
+
+from .common import (
+    KafkaBrokerBuilderMixin,
+    KafkaCircuitBreakerMixin,
+)
+from .publisher import (
+    KafkaBrokerPublisher,
+    KafkaBrokerPublisherBuilder,
+)
+from .subscriber import (
+    KafkaBrokerSubscriber,
+    KafkaBrokerSubscriberBuilder,
+)
