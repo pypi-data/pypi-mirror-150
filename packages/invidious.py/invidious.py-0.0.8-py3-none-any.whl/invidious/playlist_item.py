d9b76ff0ea968b5be8743a343dbf314b2bfc6083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:47:40 +0000

    Add package invidious.py/0.0.8/invidious.py-0.0.8-py3-none-any.whl

---
+++ packages/invidious.py/invidious.py-0.0.8-py3-none-any.whl/invidious/playlist_item.py
@@ -0,0 +1,27 @@
+
+
+class PlaylistItem():
+
+    def __init__(self, title="", playlistId="", author="",
+                       authorId="", authorUrl="", videoCount=0, videos=[]):
+        """PlaylistItem class. Used in search."""
+        self.title = title
+        self.playlistId = playlistId
+        self.author = author
+        self.authorId = authorId
+        self.authorUrl = authorUrl
+        self.videoCount = videoCount
+        self.videos = videos
+
+    def loadFromDict(self, dct):
+        self.title = dct['title']
+        self.playlistId = dct['playlistId']
+        self.author = dct['author']
+        self.authorId = dct['authorId']
+        self.authorUrl = dct['authorUrl']
+        self.videoCount = dct['videoCount']
+        self.videos = dct['videos']
+
+
+
+
---
+++ packages/invidious.py/invidious.py-0.0.8-py3-none-any.whl/invidious/recommended_video.py
@@ -0,0 +1,27 @@
+
+
+class RecommendedVideo():
+
+    def __init__(self, title="", videoId=0, videoThumbnails=[], author="",
+                 auhtorId="", auhtorUrl="", lengthSeconds=0, 
+                 viewCount=0, viewCountText=""):
+        self.title = title
+        self.videoId = videoId
+        self.videoThumbnails = videoThumbnails
+        self.author = author
+        self.authorId = auhtorId
+        self.authorUrl = auhtorUrl
+        self.lengthSeconds = lengthSeconds
+        self.viewCount = viewCount
+        self.viewCountText = viewCountText
+
+    def loadFromDict(self, dct):        
+        self.title = dct['title']
+        self.videoId = dct['videoId']
+        self.videoThumbnails = dct['videoThumbnails']
+        self.author = dct['author']
+        self.authorId = dct['auhtorId']
+        self.authorUrl = dct['auhtorUrl']
+        self.lengthSeconds = dct['lengthSeconds']
+        self.viewCount = dct['viewCount']
+        self.viewCountText = dct['viewCountText']
