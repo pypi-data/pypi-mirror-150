30a625f281e1ddd2a4d9b1034a2b2c0f3d73e152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:50 +0000

    Add package Finance-Jindowin/0.2.5/Finance-Jindowin-0.2.5.tar.gz

---
+++ packages/Finance-Jindowin/Finance-Jindowin-0.2.5.tar.gz/Finance-Jindowin-0.2.5/jdw/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import requests,pdb
+
+
+import jdw.data.DataAPI.mg as MGAPI
+import jdw.mfc.experimental.DataAPI.mg as ExperimentalAPI
+
+from jdw.mfc.neutron.factory import Factory
+
+from .version import __version__
+
+try:
+    unicode
+except:
+    unicode = str
+    
+session = requests.Session()
+
+
+NeutronAPI = Factory()
+
