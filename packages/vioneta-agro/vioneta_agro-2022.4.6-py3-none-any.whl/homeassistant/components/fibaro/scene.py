f4b684a48840ecf3fe2f423a5773b21ef13c9a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:52 +0000

    Add package vioneta-agro/2022.4.6/vioneta_agro-2022.4.6-py3-none-any.whl

---
+++ packages/vioneta-agro/vioneta_agro-2022.4.6-py3-none-any.whl/homeassistant/components/fibaro/scene.py
@@ -0,0 +1,35 @@
+"""Support for Fibaro scenes."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.scene import Scene
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from . import FIBARO_DEVICES, FibaroDevice
+from .const import DOMAIN
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Perform the setup for Fibaro scenes."""
+    async_add_entities(
+        [
+            FibaroScene(scene)
+            for scene in hass.data[DOMAIN][entry.entry_id][FIBARO_DEVICES]["scene"]
+        ],
+        True,
+    )
+
+
+class FibaroScene(FibaroDevice, Scene):
+    """Representation of a Fibaro scene entity."""
+
+    def activate(self, **kwargs: Any) -> None:
+        """Activate the scene."""
+        self.fibaro_device.start()
