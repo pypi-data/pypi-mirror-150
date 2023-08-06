8585414feb1493c0ed3b8fb9424e720f0ed61bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:40 +0000

    Add package hardser/0.0.0.8/hardser-0.0.0.8.tar.gz

---
+++ packages/hardser/hardser-0.0.0.8.tar.gz/hardser-0.0.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="hardser",
+    version="0.0.0.8",
+    author="Mikhail Hancharou",
+    author_email="engeneer2003@mail.ru",
+    packages=['modules', 'Converter'],
+    entry_points={
+        'console_scripts': ['hardser=Converter.Converter:setup'],
+    },
+    install_requires=[
+        'coverage==6.3.2',
+        'entrypoints==0.3',
+        'pytest==7.1.2',
+        'pytest-cov==3.0.0',
+        'PyYAML==5.3.1',
+        'simplejson==3.16.0',
+        'toml==0.10.2',
+        'virtualenv==20.0.17',
+    ]
+)
