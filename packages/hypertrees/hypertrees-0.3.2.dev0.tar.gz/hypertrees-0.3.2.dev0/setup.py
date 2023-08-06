c3d8e5d4bdb2653804eef4aa0dba50dd93b33596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:57 +0000

    Add package hypertrees/0.3.2.dev0/hypertrees-0.3.2.dev0.tar.gz

---
+++ packages/hypertrees/hypertrees-0.3.2.dev0.tar.gz/hypertrees-0.3.2.dev0/setup.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+# Copyright (C) 2022 Pedro Ilídio <ilidio@alumni.usp.br>
+# License: 3-clause BSD
+
+from setuptools import setup, Extension, find_packages
+from pathlib import Path
+from Cython.Build import cythonize
+import numpy
+
+PATH_ROOT = Path(__file__).parent
+README = (PATH_ROOT/"README.md").read_text()
+# Get __version__ from _version.py
+exec((PATH_ROOT/"hypertree/_version.py").read_text())
+VERSION = __version__
+
+extensions = [
+    Extension(
+        "hypertree.tree._nd_tree", ["hypertree/tree/_nd_tree.pyx"]),
+    Extension(
+        "hypertree.tree._nd_splitter", ["hypertree/tree/_nd_splitter.pyx"]),
+    Extension(
+        "hypertree.tree._nd_criterion", ["hypertree/tree/_nd_criterion.pyx"]),
+    Extension(
+        "hypertree.tree._semisupervised_criterion", ["hypertree/tree/_semisupervised_criterion.pyx"]),
+]
+
+setup(
+    name='hypertrees',
+    version=VERSION,
+    description='HyperTrees in Python.',
+    include_dirs=[numpy.get_include()],
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='http://github.com/pedroilidio/hypertree',
+    author='Pedro Ilídio',
+    author_email='pedrilidio@gmail.com',
+    license='new BSD',
+    packages=find_packages(),
+    # scripts=['bin/hypertree'],
+    zip_safe=False,
+
+    install_requires=[
+        'cython>=0.29.27',
+        'scikit-learn>=1.0.2',
+        'numpy>=1.22.2',
+        'imbalanced-learn>=0.7.0',
+    ],
+    ext_modules=cythonize(
+        extensions,
+        language_level="3",
+    ),
+)
