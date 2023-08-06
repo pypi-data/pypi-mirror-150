91271fd9298766afdf6aefe5cd7e594160f2af88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:02 +0000

    Add package SCALECAST/0.9.7/SCALECAST-0.9.7.tar.gz

---
+++ packages/SCALECAST/SCALECAST-0.9.7.tar.gz/SCALECAST-0.9.7/setup.py
@@ -0,0 +1,45 @@
+
+from setuptools import setup, find_packages
+import shutil
+from src.scalecast.__init__ import __version__ as version
+
+logo = """<p align="center">
+  <img src="https://github.com/mikekeith52/scalecast/blob/main/assets/logo2.png" />
+</p>"""
+
+long_description = open('README.md', 'r', encoding="UTF-8").read().replace(logo,"")
+
+setup(
+  name = 'SCALECAST',
+  version = version,
+  license='MIT',
+  description="The practitioner's time series forecasting library",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Michael Keith',
+  author_email = 'mikekeith52@gmail.com',
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+  project_urls = {
+    'GitHub': 'https://github.com/mikekeith52/scalecast',
+    'Read the Docs': 'https://scalecast.readthedocs.io/en/latest/',
+    'Examples': 'https://scalecast-examples.readthedocs.io/en/latest/',
+  },
+  keywords = ['FORECAST', 'SCALE', 'DYNAMIC'],
+  install_requires = [
+    'scikit-learn',
+    'tensorflow',
+    'statsmodels',
+    'scipy',
+    'eli5',
+    'numpy',
+    'pandas',
+    'scipy',
+    'matplotlib',
+    'seaborn',
+    'xgboost',
+    'lightgbm',
+    'openpyxl',
+    'pandas-datareader',
+  ],
+)
