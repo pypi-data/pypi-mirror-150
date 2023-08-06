1efaa5231c605f8fc51cde28ce2ac49c93a9726d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:14 +0000

    Add package enex2notion/0.2.11/enex2notion-0.2.11.tar.gz

---
+++ packages/enex2notion/enex2notion-0.2.11.tar.gz/enex2notion-0.2.11/enex2notion/cli.py
@@ -0,0 +1,298 @@
+import argparse
+import logging
+import sys
+from pathlib import Path
+from typing import Optional
+
+from enex2notion.cli_wkhtmltopdf import ensure_wkhtmltopdf
+from enex2notion.enex_parser import iter_notes
+from enex2notion.enex_uploader import (
+    BadTokenException,
+    NoteUploadFailException,
+    get_import_root,
+    get_notion_client,
+    upload_note,
+)
+from enex2notion.enex_uploader_modes import get_notebook_database, get_notebook_page
+from enex2notion.note_parser import parse_note
+from enex2notion.version import __version__
+
+logger = logging.getLogger(__name__)
+
+
+class DoneFile(object):
+    def __init__(self, path: Path):
+        self.path = path
+
+        try:
+            with open(path, "r") as f:
+                self.done_hashes = {line.strip() for line in f}
+        except FileNotFoundError:
+            self.done_hashes = set()
+
+    def __contains__(self, note_hash):
+        return note_hash in self.done_hashes
+
+    def add(self, note_hash):
+        self.done_hashes.add(note_hash)
+
+        with open(self.path, "a") as f:
+            f.write(f"{note_hash}\n")
+
+
+def _upload_note(notebook_root, note, note_blocks):
+    for attempt in range(5):
+        try:
+            upload_note(notebook_root, note, note_blocks)
+        except NoteUploadFailException:
+            if attempt == 4:
+                raise
+            logger.warning(
+                f"Failed to upload note '{note.title}' to Notion! Retrying..."
+            )
+            continue
+        break
+
+
+class EnexUploader(object):
+    def __init__(
+        self,
+        import_root,
+        mode: str,
+        mode_webclips: str,
+        done_file: Path,
+        add_meta: bool,
+        add_pdf_preview: bool,
+        condense_lines: bool,
+        condense_lines_sparse: bool,
+        custom_tag: str,
+    ):
+        self.import_root = import_root
+        self.mode = mode
+        self.mode_webclips = mode_webclips
+        self.done_hashes = DoneFile(done_file) if done_file else set()
+        self.add_meta = add_meta
+        self.add_pdf_preview = add_pdf_preview
+        self.condense_lines = condense_lines
+        self.condense_lines_sparse = condense_lines_sparse
+        self.custom_tag = custom_tag
+
+    def upload(self, enex_file: Path):
+        logger.info(f"Processing notebook '{enex_file.stem}'...")
+
+        notebook_root = self._get_notebook_root(enex_file.stem)
+
+        for note in iter_notes(enex_file):
+            if note.note_hash in self.done_hashes:
+                logger.debug(f"Skipping note '{note.title}' (already uploaded)")
+                continue
+
+            if self.custom_tag and self.custom_tag not in note.tags:
+                note.tags.append(self.custom_tag)
+
+            note_blocks = self._parse_note(note)
+            if not note_blocks:
+                continue
+
+            if notebook_root is not None:
+                _upload_note(notebook_root, note, note_blocks)
+                self.done_hashes.add(note.note_hash)
+
+    def _parse_note(self, note):
+        try:
+            return parse_note(
+                note,
+                mode_webclips=self.mode_webclips,
+                is_add_meta=self.add_meta,
+                is_add_pdf_preview=self.add_pdf_preview,
+                is_condense_lines=self.condense_lines,
+                is_condense_lines_sparse=self.condense_lines_sparse,
+            )
+        except Exception:
+            logger.error(f"Unhandled exception while parsing note '{note.title}'!")
+            raise
+
+    def _get_notebook_root(self, notebook_title):
+        if self.import_root is None:
+            return None
+
+        if self.mode == "DB":
+            return get_notebook_database(self.import_root, notebook_title)
+
+        return get_notebook_page(self.import_root, notebook_title)
+
+
+def cli(argv):
+    args = parse_args(argv)
+
+    _setup_logging(args.verbose, args.log)
+
+    if args.mode_webclips == "PDF":
+        ensure_wkhtmltopdf()
+
+    root = get_root(args.token, args.root_page)
+
+    enex_uploader = EnexUploader(
+        import_root=root,
+        mode=args.mode,
+        mode_webclips=args.mode_webclips,
+        done_file=args.done_file,
+        add_meta=args.add_meta,
+        add_pdf_preview=args.add_pdf_preview,
+        condense_lines=args.condense_lines,
+        condense_lines_sparse=args.condense_lines_sparse,
+        custom_tag=args.tag,
+    )
+
+    for enex_input in args.enex_input:
+        if enex_input.is_dir():
+            logger.info(f"Processing directory '{enex_input.name}'...")
+            for enex_file in sorted(enex_input.glob("**/*.enex")):
+                enex_uploader.upload(enex_file)
+        else:
+            enex_uploader.upload(enex_input)
+
+
+def get_root(token, name):
+    if not token:
+        logger.warning(
+            "No token provided, dry run mode. Nothing will be uploaded to Notion!"
+        )
+        return None
+
+    try:
+        client = get_notion_client(token)
+    except BadTokenException:
+        logger.error("Invalid token provided!")
+        sys.exit(1)
+
+    return get_import_root(client, name)
+
+
+def main():  # pragma: no cover
+    try:
+        cli(sys.argv[1:])
+    except KeyboardInterrupt:
+        sys.exit(1)
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(
+        prog="enex2notion", description="Uploads ENEX files to Notion"
+    )
+
+    schema = {
+        "enex_input": {
+            "type": Path,
+            "nargs": "+",
+            "help": "ENEX files or directories to upload",
+            "metavar": "FILE/DIR",
+        },
+        "--token": {
+            "type": str,
+            "help": (
+                "Notion token, stored in token_v2 cookie for notion.so"
+                " [NEEDED FOR UPLOAD]"
+            ),
+        },
+        "--root-page": {
+            "type": str,
+            "default": "Evernote ENEX Import",
+            "help": (
+                "root page name for the imported notebooks,"
+                " it will be created if it does not exist"
+                ' (default: "Evernote ENEX Import")'
+            ),
+            "metavar": "NAME",
+        },
+        "--mode": {
+            "choices": ["DB", "PAGE"],
+            "default": "DB",
+            "help": (
+                "upload each ENEX as database (DB) or page with children (PAGE)"
+                " (default: DB)"
+            ),
+        },
+        "--mode-webclips": {
+            "choices": ["TXT", "PDF"],
+            "default": "TXT",
+            "help": (
+                "convert web clips to text (TXT) or pdf (PDF) before upload"
+                " (default: TXT)"
+            ),
+        },
+        "--add-pdf-preview": {
+            "action": "store_true",
+            "default": False,
+            "help": (
+                "include preview image with PDF webclips for gallery view thumbnail"
+                " (works only with --mode-webclips=PDF)"
+            ),
+        },
+        "--add-meta": {
+            "action": "store_true",
+            "default": False,
+            "help": (
+                "include metadata (created, tags, etc) in notes,"
+                " makes sense only with PAGE mode"
+            ),
+        },
+        "--tag": {
+            "type": str,
+            "help": "add custom tag to uploaded notes",
+        },
+        "--condense-lines": {
+            "action": "store_true",
+            "default": False,
+            "help": (
+                "condense text lines together into paragraphs"
+                " to avoid making block per line"
+            ),
+        },
+        "--condense-lines-sparse": {
+            "action": "store_true",
+            "default": False,
+            "help": "like --condense-lines but leaves gaps between paragraphs",
+        },
+        "--done-file": {
+            "type": Path,
+            "metavar": "FILE",
+            "help": "file for uploaded notes hashes to resume interrupted upload",
+        },
+        "--log": {
+            "type": Path,
+            "metavar": "FILE",
+            "help": "file to store program log",
+        },
+        "--verbose": {
+            "action": "store_true",
+            "default": False,
+            "help": "output debug information",
+        },
+        "--version": {
+            "action": "version",
+            "version": f"%(prog)s {__version__}",  # noqa: WPS323
+        },
+    }
+
+    for arg, arg_params in schema.items():
+        parser.add_argument(arg, **arg_params)
+
+    return parser.parse_args(argv)
+
+
+def _setup_logging(is_verbose: bool, log_file: Optional[Path]):
+    logging.basicConfig(format="%(levelname)s: %(message)s")
+
+    logging.getLogger("enex2notion").setLevel(
+        logging.DEBUG if is_verbose else logging.INFO
+    )
+
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(
+            logging.Formatter("%(asctime)s [%(levelname)-8.8s] %(message)s")
+        )
+        logging.getLogger("enex2notion").addHandler(file_handler)
+
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
---
+++ packages/enex2notion/enex2notion-0.2.11.tar.gz/enex2notion-0.2.11/enex2notion/string_extractor.py
@@ -0,0 +1,139 @@
+import copy
+from typing import List
+
+from bs4 import NavigableString, Tag
+
+from enex2notion.notion_blocks_text import TextProp
+from enex2notion.string_extractor_properties import resolve_string_properties
+
+STANDALONES = ("h1", "h2", "h3", "div")
+
+
+def extract_string(tag: Tag) -> TextProp:
+    """Convert a block content into a string with properties
+
+     IN: <div>some text <b>bold <i>bold and italic</i></b></div>
+    OUT: some text bold bold and italic
+         [["some text "], ["bold ", ["b"]], ["bold and italic", ["b", "i"]]]
+    """
+
+    # Element is either a single div itself or a collection of div or h1-3 "lines"
+    # it can also contain random inline strings, so we group them in separate lines
+    div_lines = _split_line(copy.copy(tag)) if tag.find_all(STANDALONES) else [tag]
+
+    string_blocks = _extract_blocks(div_lines)
+
+    result_properties, result_string = _format_blocks(string_blocks)
+
+    return TextProp(result_string, result_properties)
+
+
+def _split_line(element: Tag):
+    blocks = []
+    group = []
+
+    for sub in element.children:
+        is_inline = not isinstance(sub, Tag) or sub.name not in STANDALONES
+
+        if is_inline:
+            # skip mid-tag whitespaces
+            if isinstance(sub, NavigableString) and not sub.text.strip():
+                continue
+
+            group.append(sub)
+        else:
+            if group:
+                blocks.append(_make_block(group))
+                group = []
+
+            blocks.append(sub)
+
+    if group:
+        blocks.append(_make_block(group))
+
+    return blocks
+
+
+def _make_block(elements: List[Tag]):
+    """Make a single block from a list of elements"""
+
+    block = Tag(name="div")
+
+    for element in elements:
+        block.append(copy.copy(element))
+
+    return block
+
+
+def _extract_blocks(div_lines):
+    """Get parent stack for each string in the line and convert them to properties
+
+    IN: <div>some text <b>bold</b></div>
+    OUT: [
+            {"string": "some text", "properties": set()},
+            {"string": "bold", "properties": {("b",)}},
+         ]
+    """
+
+    string_blocks = []
+    for div in div_lines:
+        _convert_newlines(div)
+        for string in div.strings:
+            parent_stack = _parents_upto(string, div)
+            string_properties = resolve_string_properties(parent_stack)
+
+            _add_string_block(string_blocks, string, string_properties)
+
+        # Add linebreak after each "line"
+        # to render embedded lists in tables
+        # skip last to avoid trailing linebreak
+        if div != div_lines[-1]:
+            _add_string_block(string_blocks, "\n", set())
+    return string_blocks
+
+
+def _convert_newlines(element: Tag):
+    for br in element.find_all("br"):
+        br.replace_with("\n")
+
+
+def _parents_upto(tag: Tag, upto: Tag):
+    parents = []
+    for p in tag.parents:
+        parents.append(p)
+        if p == upto:
+            break
+    return parents
+
+
+def _add_string_block(string_blocks, string, string_properties):
+    if string_blocks and string_blocks[-1]["properties"] == string_properties:
+        string_blocks[-1]["string"] += str(string)
+    else:
+        string_blocks.append({"string": str(string), "properties": string_properties})
+
+
+def _format_blocks(string_blocks):
+    """Notion properties format:
+
+    plain text: ["some text"]
+    formatted text: ["some text", ["property"]]
+    multiple properties: ["some text", ["property1", "property2"]]
+    property with args: ["some text", [["property", "arg"]]]
+    """
+
+    result_properties = []
+    for block in string_blocks:
+        if block["properties"]:
+            properties = [list(p) for p in block["properties"]]
+            properties.sort(key=lambda p: "".join(p))
+
+            result_properties.append([block["string"], properties])
+        else:
+            result_properties.append([block["string"]])
+    result_string = "".join(b["string"] for b in string_blocks)
+
+    if not result_string.strip():
+        return [], ""
+
+    return result_properties, result_string
