ddf39cb4fb6061506eeb0e9f8d36672362298c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:13 +0000

    Add package python-garminconnect-enhanced/0.0.1/python-garminconnect-enhanced-0.0.1.tar.gz

---
+++ packages/python-garminconnect-enhanced/python-garminconnect-enhanced-0.0.1.tar.gz/python-garminconnect-enhanced-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['garminconnect']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cloudscraper>=1.2.60,<2.0.0', 'requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'python-garminconnect-enhanced',
+    'version': '0.0.1',
+    'description': 'A fork of python-garminconnect with additional functionality to upload activities',
+    'long_description': None,
+    'author': 'Marvin Straathof',
+    'author_email': 'marvinstraathof@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
