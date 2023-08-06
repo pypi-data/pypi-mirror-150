8e56d49213abc1b0e758940b7377ea63ec3671b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:06 +0000

    Add package cinemasci/1.7.9/cinemasci-1.7.9.tar.gz

---
+++ packages/cinemasci/cinemasci-1.7.9.tar.gz/cinemasci-1.7.9/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+# read the description file 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'doc/description.md'), encoding='utf-8') as f:
+    long_description_text = f.read()
+
+setuptools.setup(
+    name="cinemasci",
+    version="1.7.9",
+    author="David H. Rogers",
+    author_email="dhr@lanl.gov",
+    description="Cinema scientific toolset.",
+    long_description=long_description_text,
+    long_description_content_type='text/markdown',
+    url="https://github.com/cinemascience",
+    include_package_data=True,
+    packages=["cinemasci", 
+                "cinemasci.cdb", 
+                "cinemasci.cis",
+                "cinemasci.viewers",
+                "cinemasci.pynb",
+                "cinemasci.install",
+                "cinemasci.server"
+            ],
+    install_requires=[
+        "pandas",
+        "jupyterlab",
+        "ipywidgets",
+        "pyyaml",
+        "scikit-image",
+        "h5py",
+        "matplotlib"
+    ],
+    scripts=['scripts/cisconvert', 'scripts/cinema', 'doc/description.md'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
