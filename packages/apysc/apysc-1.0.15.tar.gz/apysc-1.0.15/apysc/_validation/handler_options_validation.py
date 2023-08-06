246c0fde3e1a03fc5bfc441480f2423116ac601e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:23 +0000

    Add package apysc/1.0.15/apysc-1.0.15.tar.gz

---
+++ packages/apysc/apysc-1.0.15.tar.gz/apysc-1.0.15/apysc/_validation/handler_options_validation.py
@@ -0,0 +1,28 @@
+"""This module is for the handler's option value
+validation implementations.
+"""
+
+from typing import Any
+
+
+def validate_options_type(options: Any) -> None:
+    """
+    Validate a specified options type.
+
+    Parameters
+    ----------
+    options : Any
+        Target options value.
+
+    Raises
+    ------
+    TypeError
+        If a specified options type is not the dictionary or None.
+    """
+    if options is None:
+        return
+    if isinstance(options, dict):
+        return
+    raise TypeError(
+        f"Handler's options argument must be a dictionary: {type(options)}"
+        f'\n{options}')
