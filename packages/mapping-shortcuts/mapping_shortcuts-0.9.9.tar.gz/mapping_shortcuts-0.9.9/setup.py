76fbd721fa45135a7427a1656ce7ea87c926471f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:22 +0000

    Add package mapping-shortcuts/0.9.9/mapping_shortcuts-0.9.9.tar.gz

---
+++ packages/mapping-shortcuts/mapping_shortcuts-0.9.9.tar.gz/mapping_shortcuts-0.9.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='mapping_shortcuts',
+    version='0.9.9',
+    author='Komissarov Andrey',
+    author_email='Komissar.off.andrey@gmail.com',
+    description='Useful shortcuts for create mappings',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/moff4/shortcuts',
+    install_requires=[
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
+)
