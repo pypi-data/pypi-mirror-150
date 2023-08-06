4f530fa4d4d54e4b072eb1957d7a8fe8f228638b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:12 +0000

    Add package cuefig/0.0.9/cuefig-0.0.9.tar.gz

---
+++ packages/cuefig/cuefig-0.0.9.tar.gz/cuefig-0.0.9/cuefig/__init__.py
@@ -0,0 +1,17 @@
+from cuefig.utils import eprint
+
+try:
+    from conf import *
+except ImportError:
+    eprint("You dont have create conf package")
+
+try:
+    from conf.config import *
+except ImportError:
+    eprint("You dont have create conf/config.py file")
+
+try:
+    from conf.config_deploy import *
+except ImportError:
+    eprint("You dont have create conf/config_deploy.py file")
+
