9e6ef20351228d7bbbaa101b9808383c6f34f3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:49 +0000

    Add package deps/0.1.2/deps-0.1.2-py3-none-any.whl

---
+++ packages/deps/deps-0.1.2-py3-none-any.whl/deps/config.py
@@ -0,0 +1,15 @@
+from decouple import AutoConfig, Csv
+
+config = AutoConfig()
+
+
+# Github Config
+GITHUB_DEFAULT_PAGE_SIZE = config("GITHUB_DEFAULT_PAGE_SIZE", cast=int, default=100)
+GITHUB_ORG = config("GITHUB_ORG", cast=str)
+GITHUB_REPOSITORIES = config("GITHUB_REPOSITORIES", cast=Csv(post_process=list[str], strip=" "))
+GITHUB_TOKEN = config("GITHUB_TOKEN", cast=str)
+GITHUB_URL = config("GITHUB_URL", cast=str, default="https://api.github.com")
+GITHUB_USER = config("GITHUB_USER", cast=str)
+
+# Deps Config
+DEPS_EXPORT_TO_SVG = config("DEPS_EXPORT_TO_SVG", cast=bool, default=False)
