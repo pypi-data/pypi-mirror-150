a76adcab48da1d3c909009757ef2b5f7e6f9a5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:02 +0000

    Add package aliyun-python-sdk-mse/3.0.19/aliyun-python-sdk-mse-3.0.19.tar.gz

---
+++ packages/aliyun-python-sdk-mse/aliyun-python-sdk-mse-3.0.19.tar.gz/aliyun-python-sdk-mse-3.0.19/aliyunsdkmse/request/v20190531/AddSeataServerRequest.py
@@ -0,0 +1,73 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkmse.endpoint import endpoint_data
+
+class AddSeataServerRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'mse', '2019-05-31', 'AddSeataServer','mse')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_Replica(self): # Integer
+		return self.get_query_params().get('Replica')
+
+	def set_Replica(self, Replica):  # Integer
+		self.add_query_param('Replica', Replica)
+	def get_PrimaryUser(self): # String
+		return self.get_query_params().get('PrimaryUser')
+
+	def set_PrimaryUser(self, PrimaryUser):  # String
+		self.add_query_param('PrimaryUser', PrimaryUser)
+	def get_Vpc(self): # String
+		return self.get_query_params().get('Vpc')
+
+	def set_Vpc(self, Vpc):  # String
+		self.add_query_param('Vpc', Vpc)
+	def get_Vswitch(self): # String
+		return self.get_query_params().get('Vswitch')
+
+	def set_Vswitch(self, Vswitch):  # String
+		self.add_query_param('Vswitch', Vswitch)
+	def get_Name(self): # String
+		return self.get_query_params().get('Name')
+
+	def set_Name(self, Name):  # String
+		self.add_query_param('Name', Name)
+	def get_AcceptLanguage(self): # String
+		return self.get_query_params().get('AcceptLanguage')
+
+	def set_AcceptLanguage(self, AcceptLanguage):  # String
+		self.add_query_param('AcceptLanguage', AcceptLanguage)
+	def get_ZoneId(self): # String
+		return self.get_query_params().get('ZoneId')
+
+	def set_ZoneId(self, ZoneId):  # String
+		self.add_query_param('ZoneId', ZoneId)
+	def get_Region(self): # String
+		return self.get_query_params().get('Region')
+
+	def set_Region(self, Region):  # String
+		self.add_query_param('Region', Region)
