73979aae1b926e73d0c66ea9ddf50400d323d882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:24 +0000

    Add package ampel/0.8.2a0/ampel-0.8.2a0.tar.gz

---
+++ packages/ampel/ampel-0.8.2a0.tar.gz/ampel-0.8.2a0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ampel']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ampel-core>=0.8.2-alpha.13,<0.8.3',
+ 'ampel-interface>=0.8.2-alpha.9,<0.8.3',
+ 'ampel-ipython==0.7']
+
+setup_kwargs = {
+    'name': 'ampel',
+    'version': '0.8.2a0',
+    'description': 'Asynchronous and Modular Platform with Execution Layers',
+    'long_description': None,
+    'author': 'Valery Brinnel',
+    'author_email': None,
+    'maintainer': 'Jakob van Santen',
+    'maintainer_email': 'jakob.van.santen@desy.de',
+    'url': 'https://ampelproject.github.io',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
