1b8b3950465bc541ec9d0f2c328a7ff5d0eaa2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:58 +0000

    Add package Geode-ImplicitModeling/1.2.1/Geode_ImplicitModeling-1.2.1-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/Geode-ImplicitModeling/Geode_ImplicitModeling-1.2.1-cp310-cp310-manylinux2014_x86_64.whl/geode_implicitmodeling/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+
+import opengeode
+from geode_common import core
+
+from .explicitation import *
+from .implicitation import *
---
+++ packages/Geode-ImplicitModeling/Geode_ImplicitModeling-1.2.1-cp310-cp310-manylinux2014_x86_64.whl/geode_implicitmodeling/model.py
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+from geode_geosciences import geosciences
+
+from .geode_implicitmodeling_py_model import *
