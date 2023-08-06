1f0df7d0a3484912b00bfd1fc72fbf7b44f05b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:04 +0000

    Add package se-math/1.0.0/se_math-1.0.0.tar.gz

---
+++ packages/se-math/se_math-1.0.0.tar.gz/se_math-1.0.0/README.md
@@ -0,0 +1,1 @@
+Soyol erdem package
---
+++ packages/se-math/se_math-1.0.0.tar.gz/se_math-1.0.0/setup.py
@@ -0,0 +1,4 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(name='se_math', version="1.0.0", long_description=Path("README.md").read_text(), packages=setuptools.find_packages(exclude=['.vscode']))
