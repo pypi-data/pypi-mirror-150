7011e96a833f91c476edf6b9802a186adde40bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:31 +0000

    Add package telegrambots-custom/0.0.6rc0/telegrambots_custom-0.0.6rc0-py3-none-any.whl

---
+++ packages/telegrambots-custom/telegrambots_custom-0.0.6rc0-py3-none-any.whl/telegrambots/custom/contexts/_contexts/callback_query_context.py
@@ -0,0 +1,160 @@
+from typing import Any, Optional, final, TYPE_CHECKING
+
+from telegrambots.wrapper.types.objects import (
+    CallbackQuery,
+    Update,
+    MessageEntity,
+    InlineKeyboardMarkup,
+)
+
+from .context_template import GenericContext
+
+if TYPE_CHECKING:
+    from ...dispatcher import Dispatcher
+
+
+class CallbackQueryContext(GenericContext[CallbackQuery]):
+    def __init__(
+        self, dp: "Dispatcher", update: Update, handler_tag: str, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            dp,
+            update=update,
+            update_type=CallbackQuery,
+            handler_tag=handler_tag,
+            **kwargs
+        )
+
+    @final
+    def __extractor__(self, update: Update) -> CallbackQuery:
+        c = update.callback_query
+        if c is not None:
+            return c
+        raise ValueError("Update has no callback query")
+
+    async def answer(
+        self,
+        text: str,
+        *,
+        show_alert: bool = False,
+        url: Optional[str] = None,
+        cache_time: int = 0
+    ) -> None:
+        """Answers a callback query.
+
+        Args:
+            text (`str`): The text to show in the answer.
+            show_alert (`bool`, optional): If true, an alert will be shown by the client. Defaults to False.
+            url (`str`, optional): If specified, clients will open this url instead of the default url.
+            cache_time (`int`, optional): The time in seconds that the result of the callback query will be available. Defaults to 0.
+        """
+
+        await self.bot.answer_callback_query(
+            callback_query_id=self.update.id,
+            text=text,
+            show_alert=show_alert,
+            url=url,
+            cache_time=cache_time,
+        )
+
+    async def edit_text(
+        self,
+        text: str,
+        parse_mode: Optional[str] = None,
+        entities: Optional[list[MessageEntity]] = None,
+        disable_web_page_preview: bool = False,
+        reply_markup: Optional[InlineKeyboardMarkup] = None,
+    ):
+        """Edits the text of a message.
+
+        Args:
+            text (`str`): The new text of the message.
+            parse_mode (`str`, optional): The parse mode to use for the text. Defaults to None.
+            entities (`list[MessageEntity]`, optional): A list of additional entities to send with the message. Defaults to None.
+            disable_web_page_preview (`bool`, optional): Disables link previews for links in this message. Defaults to False.
+            reply_markup (`InlineKeyboardMarkup`, optional): Additional interface options. Defaults to None.
+        """
+
+        if self.update.message is not None:
+            return await self.bot.edit_message_text(
+                chat_id=self.update.message.chat.id,
+                message_id=self.update.message.message_id,
+                text=text,
+                parse_mode=parse_mode,
+                entities=entities,
+                disable_web_page_preview=disable_web_page_preview,
+                reply_markup=reply_markup,
+            )
+        else:
+            raise ValueError(
+                "CallbackQuery contains no message. you may use edit_inline_text instead"
+            )
+
+    async def edit_inline_text(
+        self,
+        text: str,
+        parse_mode: Optional[str] = None,
+        entities: Optional[list[MessageEntity]] = None,
+        disable_web_page_preview: bool = False,
+        reply_markup: Optional[InlineKeyboardMarkup] = None,
+    ):
+        """Edits the text of an inline message.
+
+        Args:
+            text (`str`): The new text of the message.
+            parse_mode (`str`, optional): The parse mode to use for the text. Defaults to None.
+            entities (`list[MessageEntity]`, optional): A list of additional entities to send with the message. Defaults to None.
+            disable_web_page_preview (`bool`, optional): Disables link previews for links in this message. Defaults to False.
+            reply_markup (`InlineKeyboardMarkup`, optional): Additional
+        """
+
+        if self.update.inline_message_id is not None:
+            return await self.bot.edit_inline_message_text(
+                inline_message_id=self.update.inline_message_id,
+                text=text,
+                parse_mode=parse_mode,
+                entities=entities,
+                disable_web_page_preview=disable_web_page_preview,
+                reply_markup=reply_markup,
+            )
+        else:
+            raise ValueError(
+                "CallbackQuery contains no inline message id. you may use edit_text instead"
+            )
+
+    async def edit_reply_markup(
+        self, reply_markup: Optional[InlineKeyboardMarkup] = None
+    ):
+        """Edits the reply markup of a message.
+
+        Args:
+            reply_markup (`InlineKeyboardMarkup`, optional): The new reply markup of the message. Defaults to None.
+        """
+        if self.update.message is not None:
+            return await self.bot.edit_message_reply_markup(
+                chat_id=self.update.message.chat.id,
+                message_id=self.update.message.message_id,
+                reply_markup=reply_markup,
+            )
+        else:
+            raise ValueError(
+                "CallbackQuery contains no message. you may use edit_inline_reply_markup instead"
+            )
+
+    async def edit_inline_reply_markup(
+        self, reply_markup: Optional[InlineKeyboardMarkup] = None
+    ):
+        """Edits the reply markup of an inline message.
+
+        Args:
+            reply_markup (`InlineKeyboardMarkup`, optional): The new reply markup of the message. Defaults to None.
+        """
+        if self.update.inline_message_id is not None:
+            return await self.bot.edit_inline_message_reply_markup(
+                inline_message_id=self.update.inline_message_id,
+                reply_markup=reply_markup,
+            )
+        else:
+            raise ValueError(
+                "CallbackQuery contains no inline message id. you may use edit_reply_markup instead"
+            )
---
+++ packages/telegrambots-custom/telegrambots_custom-0.0.6rc0-py3-none-any.whl/telegrambots/custom/extensions/context.py
@@ -0,0 +1,346 @@
+from abc import ABC
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    NoReturn,
+    Optional,
+    Sequence,
+    Coroutine,
+    final,
+)
+
+from telegrambots.wrapper.types.objects import CallbackQuery, Message
+
+from ..contexts._contexts._continuously_handler import (
+    ContinueWithInfo,
+    ContinuouslyHandler,
+)
+from ..exceptions.propagations import BreakPropagation, ContinuePropagation
+from ..general import TUpdate
+from ..key_resolvers.key_resolver import AbstractKeyResolver
+from ..key_resolvers import (
+    MessageSenderId,
+    CallbackQueryMessageId,
+    CallbackQuerySenderId,
+)
+from ..filters import Filter, messages as mf
+
+if TYPE_CHECKING:
+    from .. import ContextTemplate
+    from ..contexts import CallbackQueryContext, MessageContext
+
+
+class ContextExtensions(ABC):
+    def __init__(self, context: "ContextTemplate") -> None:
+        self.__context = context
+
+    @final
+    @property
+    def _context(self) -> "ContextTemplate":
+        return self.__context
+
+
+class PropagationExtension(ContextExtensions):
+    def __init__(self, context: "ContextTemplate") -> None:
+        super().__init__(context)
+
+    def stop(self) -> NoReturn:
+        """Stops the propagation of the current context."""
+        raise BreakPropagation()
+
+    def resume(self) -> NoReturn:
+        """Continues the propagation of the current context."""
+        raise ContinuePropagation()
+
+
+class ContinueWithThisExtensions(ContextExtensions):
+    def __init__(self, context: "ContextTemplate") -> None:
+        super().__init__(context)
+
+    def callback_query(
+        self,
+        keys: Sequence[AbstractKeyResolver[CallbackQuery, Any]],
+        filter: Filter[CallbackQuery],
+        tag: Optional[str] = None,
+        other_continue_with: Optional[list[str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        def decorator(
+            _function: Callable[["CallbackQueryContext"], Coroutine[Any, Any, None]]
+        ):
+            _tag = tag or _function.__name__
+            if not self._context.dp.handler_tag_exists(_tag, CallbackQuery):
+                self._context.dp.add.handlers.callback_query(
+                    _tag,
+                    _function,
+                    filter,
+                    [self._context.handler_tag] + (other_continue_with or []),
+                )
+            self._context.continue_with.callback_query(_tag, keys, *args, **kwargs)
+
+        return decorator
+
+    def callback_query_form(
+        self,
+        user_id: int,
+        filter: Filter[CallbackQuery],
+        tag: Optional[str] = None,
+        other_continue_with: Optional[list[str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        def decorator(
+            _function: Callable[["CallbackQueryContext"], Coroutine[Any, Any, None]]
+        ):
+            _tag = tag or _function.__name__
+            if not self._context.dp.handler_tag_exists(_tag, CallbackQuery):
+                self._context.dp.add.handlers.callback_query(
+                    _tag,
+                    _function,
+                    filter,
+                    [self._context.handler_tag] + (other_continue_with or []),
+                )
+            self._context.continue_with.callback_query(
+                _tag, [CallbackQuerySenderId(user_id)], *args, **kwargs
+            )
+
+        return decorator
+
+    def callback_query_same_message_form(
+        self,
+        message_id: int,
+        user_id: int,
+        filter: Filter[CallbackQuery],
+        tag: Optional[str] = None,
+        other_continue_with: Optional[list[str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        def decorator(
+            _function: Callable[["CallbackQueryContext"], Coroutine[Any, Any, None]]
+        ):
+            _tag = tag or _function.__name__
+            if not self._context.dp.handler_tag_exists(_tag, CallbackQuery):
+                self._context.dp.add.handlers.callback_query(
+                    _tag,
+                    _function,
+                    filter,
+                    [self._context.handler_tag] + (other_continue_with or []),
+                )
+            self._context.continue_with.callback_query(
+                _tag,
+                [CallbackQuerySenderId(user_id), CallbackQueryMessageId(message_id)],
+                *args,
+                **kwargs,
+            )
+
+        return decorator
+
+    def callback_query_same_message(
+        self,
+        message_id: int,
+        filter: Filter[CallbackQuery],
+        tag: Optional[str] = None,
+        other_continue_with: Optional[list[str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        def decorator(
+            _function: Callable[["CallbackQueryContext"], Coroutine[Any, Any, None]]
+        ):
+            _tag = tag or _function.__name__
+            if not self._context.dp.handler_tag_exists(_tag, CallbackQuery):
+                self._context.dp.add.handlers.callback_query(
+                    _tag,
+                    _function,
+                    filter,
+                    [self._context.handler_tag] + (other_continue_with or []),
+                )
+            self._context.continue_with.callback_query(
+                _tag,
+                [CallbackQueryMessageId(message_id)],
+                *args,
+                **kwargs,
+            )
+
+        return decorator
+
+    def message(
+        self,
+        keys: list[AbstractKeyResolver[Message, Any]],
+        filter: "Filter[Message]",
+        tag: Optional[str] = None,
+        other_continue_with: Optional[list[str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        def decorator(
+            _function: Callable[["MessageContext"], Coroutine[Any, Any, None]]
+        ):
+            _tag = tag or _function.__name__
+            if not self._context.dp.handler_tag_exists(_tag, Message):
+                self._context.dp.add.handlers.message(
+                    _tag,
+                    _function,
+                    filter,
+                    [self._context.handler_tag] + (other_continue_with or []),
+                )
+            self._context.continue_with.message(_tag, keys, *args, **kwargs)
+
+        return decorator
+
+    def message_from(
+        self,
+        user_id: int,
+        filter: "Filter[Message]",
+        tag: Optional[str] = None,
+        other_continue_with: Optional[list[str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        def decorator(
+            _function: Callable[["MessageContext"], Coroutine[Any, Any, None]]
+        ):
+            _tag = tag or _function.__name__
+            if not self._context.dp.handler_tag_exists(_tag, Message):
+                self._context.dp.add.handlers.message(
+                    _tag,
+                    _function,
+                    filter,
+                    [self._context.handler_tag] + (other_continue_with or []),
+                )
+            self._context.continue_with.message(
+                _tag, [MessageSenderId(user_id)], *args, **kwargs
+            )
+
+        return decorator
+
+    def text_input_from(
+        self,
+        user_id: int,
+        tag: Optional[str] = None,
+        other_continue_with: Optional[list[str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        def decorator(
+            _function: Callable[["MessageContext"], Coroutine[Any, Any, None]]
+        ):
+            _tag = tag or _function.__name__
+            if not self._context.dp.handler_tag_exists(_tag, Message):
+                self._context.dp.add.handlers.message(
+                    _tag,
+                    _function,  # type: ignore
+                    mf.text_message,
+                    [self._context.handler_tag] + (other_continue_with or []),
+                )
+            self._context.continue_with.message(
+                _tag, [MessageSenderId(user_id)], *args, **kwargs
+            )
+
+        return decorator
+
+
+class ContinueWithExtensions(ContextExtensions):
+    def __init__(self, context: "ContextTemplate") -> None:
+        super().__init__(context)
+
+        # extensions
+        self.__this: Optional[ContinueWithThisExtensions] = None
+
+    @final
+    @property
+    def this(self) -> ContinueWithThisExtensions:
+        """Extension methods that allow you to add, register and continue with a handler, directly inside another."""
+        if self.__this is None:
+            self.__this = ContinueWithThisExtensions(self._context)
+        return self.__this
+
+    def any(
+        self,
+        target_tag: str,
+        update_type: type[TUpdate],
+        keys: Sequence[AbstractKeyResolver[TUpdate, Any]],
+        priority: int = 0,
+        *args: Any,
+        **kwargs: Any,
+    ) -> NoReturn:  # type: ignore
+        """
+        Continues the propagation of the current context, with another handler.
+
+        Args:
+            target_tag (`str`): Tag of the target handler.
+            update_type (`type`): Type of the update.
+            keys (`list[AbstractKeyResolver[TUpdate, Any]]`): Keys to resolve.
+            priority (`int`): Priority of the handler.
+            args (`Any`): Arguments to pass to the handler.
+            kwargs (`Any`): Keyword arguments to pass to the handler.
+        """
+        self._context.dp.add_continuously_handler(
+            ContinuouslyHandler(
+                target_tag,
+                self._context.handler_tag,
+                update_type,
+                keys,
+                priority,
+                *args,
+                **kwargs,
+            )
+        )
+        self._context.propagation.stop()
+
+    def many(
+        self, *continue_with_info: ContinueWithInfo[Any]
+    ) -> NoReturn:  # type: ignore
+        """Continues the propagation of the current context, with another handler."""
+        self._context.dp.add_continuously_handler(
+            tuple(  # type: ignore
+                ContinuouslyHandler(
+                    info.target_tag,
+                    self._context.handler_tag,
+                    info.update_type,
+                    info.keys,
+                    info.priority,
+                    *info.args,
+                    **info.kwargs,
+                )
+                for info in continue_with_info
+            )
+        )
+        self._context.propagation.stop()
+
+    def message(
+        self,
+        target_tag: str,
+        keys: Sequence[AbstractKeyResolver[Message, Any]],
+        priority: int = 0,
+        *args: Any,
+        **kwargs: Any,
+    ) -> NoReturn:
+        self.any(
+            target_tag,
+            Message,
+            keys,
+            priority,
+            *args,
+            **kwargs,
+        )
+
+    def callback_query(
+        self,
+        target_tag: str,
+        keys: Sequence[AbstractKeyResolver[CallbackQuery, Any]],
+        priority: int = 0,
+        *args: Any,
+        **kwargs: Any,
+    ) -> NoReturn:
+        self.any(
+            target_tag,
+            CallbackQuery,
+            keys,
+            priority,
+            *args,
+            **kwargs,
+        )
