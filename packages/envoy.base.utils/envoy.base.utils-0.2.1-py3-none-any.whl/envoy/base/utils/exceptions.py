db71f2d71a28405bee43e2701dfdffd594f12b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:21 +0000

    Add package envoy.base.utils/0.2.1/envoy.base.utils-0.2.1-py3-none-any.whl

---
+++ packages/envoy.base.utils/envoy.base.utils-0.2.1-py3-none-any.whl/envoy/base/utils/exceptions.py
@@ -0,0 +1,19 @@
+
+class TypeCastingError(TypeError):
+    pass
+
+
+class ChangelogError(Exception):
+    pass
+
+
+class ReleaseError(Exception):
+    pass
+
+
+class DevError(Exception):
+    pass
+
+
+class CommitError(Exception):
+    pass
