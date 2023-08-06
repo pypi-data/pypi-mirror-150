e20380aed4de006af32c3a776cae44808d672808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:37 +0000

    Add package pydiscordwrapper/0.1.2/PyDiscordWrapper-0.1.2.tar.gz

---
+++ packages/pydiscordwrapper/PyDiscordWrapper-0.1.2.tar.gz/PyDiscordWrapper-0.1.2/src/pydiscordwrapper/_endpoints.py
@@ -0,0 +1,12 @@
+from ._urls import API_BASE_URL
+
+CURRENT_USER = API_BASE_URL + "/users/@me"
+CURRENT_USER_GUILDS = API_BASE_URL + "/users/@me/guilds"
+
+GUILD = API_BASE_URL + "/guilds/{guild_id}"
+GUILD_CHANNELS = GUILD + "/channels"
+GUILD_MEMBERS = GUILD + "/members"
+GUILD_ROLES = GUILD + "/roles"
+
+CDN_URL = "https://cdn.discordapp.com"
+GUILD_ICON_CDN = CDN_URL + "/icons/{guild_id}/{icon_hash}.jpg"
