fa6601f5313217206cbdf45e3c788f1de77153fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:24:34 +0000

    Add package zzd/0.1.2/zzd-0.1.2.tar.gz

---
+++ packages/zzd/zzd-0.1.2.tar.gz/zzd-0.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# 读取项目的readme介绍
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zzd",# 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
+    version="0.1.2",
+    author="zzd lab", # 项目作者
+    author_email="1965770446@qq.com",
+    description="encode and scores", # 项目的一句话描述
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zzd/",# 项目地址
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
