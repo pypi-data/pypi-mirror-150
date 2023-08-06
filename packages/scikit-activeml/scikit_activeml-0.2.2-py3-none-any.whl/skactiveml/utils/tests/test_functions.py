e8e9ddf5e8d4ffa477cb031a5d02a5c32cff9412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:33 +0000

    Add package scikit-activeml/0.2.2/scikit_activeml-0.2.2-py3-none-any.whl

---
+++ packages/scikit-activeml/scikit_activeml-0.2.2-py3-none-any.whl/skactiveml/utils/tests/test_functions.py
@@ -0,0 +1,14 @@
+import unittest
+
+from skactiveml.utils import call_func
+
+
+class TestFunctions(unittest.TestCase):
+    def test_call_func(self):
+        def dummy_function(a, b=2, c=3):
+            return a * b * c
+
+        result = call_func(dummy_function, a=2, b=5, c=5)
+        self.assertEqual(result, 50)
+        result = call_func(dummy_function, only_mandatory=True, a=2, b=5, c=5)
+        self.assertEqual(result, 12)
