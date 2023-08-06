1b8a5f090977511ba5bb188dea28a376f17f8b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:28 +0000

    Add package ixapipes/0.0.8/ixapipes-0.0.8.tar.gz

---
+++ packages/ixapipes/ixapipes-0.0.8.tar.gz/ixapipes-0.0.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ixapipes",
+    version="0.0.8",
+    license="Apache License 2.0",
+    author="Iker García Ferrero",
+    author_email="iker.garciaf@ehu.eus",
+    packages=find_packages("ixapipes"),
+    url="https://github.com/ikergarcia1996/Python-IXA-Pipes",
+    keywords="ixa pipes ixapipes",
+    install_requires=[],
+)
