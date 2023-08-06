e33ef23a6ea4474cd71b3e8b01effbb2a8a1a9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:58 +0000

    Add package tastyworks-api/4.3.0/tastyworks-api-4.3.0.tar.gz

---
+++ packages/tastyworks-api/tastyworks-api-4.3.0.tar.gz/tastyworks-api-4.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+from tastyworks.utils import VERSION
+
+
+f = open('README.md', 'r')
+LONG_DESCRIPTION = f.read()
+f.close()
+
+setup(
+    name='tastyworks-api',
+    version=VERSION,
+    description='An unofficial API for Tastyworks!',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Graeme Holliday',
+    author_email='graeme.holliday@pm.me',
+    url='https://github.com/tastyware/tastyworks-api',
+    license='Apache',
+    install_requires=[
+        'aiohttp<4',
+        'requests<3',
+        'tastyworks-aiocometd>=1.0',
+        'dataclasses'
+    ],
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    include_package_data=True,
+)
