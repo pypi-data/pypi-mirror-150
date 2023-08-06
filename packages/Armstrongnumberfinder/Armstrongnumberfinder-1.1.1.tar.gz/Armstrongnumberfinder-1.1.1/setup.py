363029e69cc49d0ec50e9e22c6810d93c3c44f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:41 +0000

    Add package Armstrongnumberfinder/1.1.1/Armstrongnumberfinder-1.1.1.tar.gz

---
+++ packages/Armstrongnumberfinder/Armstrongnumberfinder-1.1.1.tar.gz/Armstrongnumberfinder-1.1.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name="Armstrongnumberfinder",
+version="1.1.1",description="This package allows you to find armstrong number",
+long_description="This is a package which helps us find armstrong number it is very usefull",
+author="Shourjjo Majumder",
+packages=['armstrongnbrfinder'])
