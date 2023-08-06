6f23c9df26626746b5cb0505c13b6c82b205f82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:41 +0000

    Add package aistore/0.2.0/aistore-0.2.0-py3-none-any.whl

---
+++ packages/aistore/aistore-0.2.0-py3-none-any.whl/aistore/client/const.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+
+#pylint: disable=unused-variable
+
+TAR2TF = "tar2tf"
+OBJECTS = "objects"
+START = "start"
+
+# URL Params
+QParamArchpath = "archpath"
+QParamProvider = "provider"
+QParamWhat = "what"
+QParamKeepBckMD = "keep_md"
+
+# Bucket providers
+ProviderAIS = "ais"
+ProviderAmazon = "aws"
+ProviderGoogle = "gcp"
+ProviderHTTP = "ht"
+ProviderAzure = "azure"
+ProviderHDFS = "hdfs"
+
+HTTP_METHOD_GET = "get"
+HTTP_METHOD_POST = "post"
+HTTP_METHOD_DELETE = "delete"
+HTTP_METHOD_PUT = "put"
+HTTP_METHOD_HEAD = "head"
