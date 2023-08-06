45df626043885f6840ff745273c2867094199d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:28 +0000

    Add package Annotations2Sub/0.1.33/Annotations2Sub-0.1.33-py3-none-any.whl

---
+++ packages/Annotations2Sub/Annotations2Sub-0.1.33-py3-none-any.whl/Annotations2Sub/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+if __name__ == "__main__":
+    path = os.path.split(os.path.realpath(__file__))[0]
+    sys.path.append(path)
+    path = os.path.abspath(os.path.join("path", os.pardir))
+    sys.path.append(path)
+    from Annotations2Sub.cli import main
+
+    main()
