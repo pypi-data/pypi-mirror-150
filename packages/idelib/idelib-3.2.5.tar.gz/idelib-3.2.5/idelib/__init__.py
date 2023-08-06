71cdb09fee89ea1827653c1643172df3ecd98842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:16 +0000

    Add package idelib/3.2.5/idelib-3.2.5.tar.gz

---
+++ packages/idelib/idelib-3.2.5.tar.gz/idelib-3.2.5/idelib/__init__.py
@@ -0,0 +1,16 @@
+#!
+"""
+Package for reading and analyzing Mide Instrumentation Data Exchange (MIDE)
+files.
+"""
+
+__author__ = "David Randall Stokes"
+__copyright__ = "Copyright (c) 2021 Midé Technology"
+
+__maintainer__ = "Mide Technology"
+__email__ = "help@mide.com"
+
+__version__ = (3, 2, 4)
+__status__ = "Production/Stable"
+
+from .importer import importFile
