8c7a6fc6fe248018bb7884fae9b70d5589196cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:47 +0000

    Add package sqllineage/1.3.5/sqllineage-1.3.5.tar.gz

---
+++ packages/sqllineage/sqllineage-1.3.5.tar.gz/sqllineage-1.3.5/sqllineage/__init__.py
@@ -0,0 +1,64 @@
+import os
+
+
+def _patch_adding_window_function_token() -> None:
+    from sqlparse.engine import grouping
+    from sqllineage.utils.sqlparse import group_function_with_window
+
+    grouping.group_functions = group_function_with_window
+
+
+def _patch_adding_builtin_type() -> None:
+    from sqlparse import tokens
+    from sqlparse.keywords import KEYWORDS
+
+    KEYWORDS["STRING"] = tokens.Name.Builtin
+    KEYWORDS["DATETIME"] = tokens.Name.Builtin
+
+
+def _monkey_patch() -> None:
+    try:
+        _patch_adding_window_function_token()
+        _patch_adding_builtin_type()
+    except ImportError:
+        # when imported by setup.py for constant variables, dependency is not ready yet
+        pass
+
+
+_monkey_patch()
+
+NAME = "sqllineage"
+VERSION = "1.3.5"
+DEFAULT_LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {"default": {"format": "%(levelname)s: %(message)s"}},
+    "handlers": {
+        "console": {
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        }
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "level": "WARNING",
+            "propagate": False,
+            "filters": [],
+        },
+        "werkzeug": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+            "filters": [],
+        },
+    },
+}
+
+STATIC_FOLDER = "build"
+DATA_FOLDER = os.environ.get(
+    "SQLLINEAGE_DIRECTORY", os.path.join(os.path.dirname(__file__), "data")
+)
+DEFAULT_HOST = "localhost"
+DEFAULT_PORT = 5000
