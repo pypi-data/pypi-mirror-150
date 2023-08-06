70493ee7eb4673f1efc946e9edc5c0aac2545c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:16 +0000

    Add package puppy-test/1.2.0/puppy_test-1.2.0-py3-none-any.whl

---
+++ packages/puppy-test/puppy_test-1.2.0-py3-none-any.whl/puppy/core/function/beautify/beautify.py
@@ -0,0 +1,29 @@
+# -*- encoding=utf-8 *-*
+"""
+    author: Li Junxian
+    function: beautify json
+"""
+from .beautify_json import BeautifyJson
+from .beautify_xml import BeautifyXml
+
+
+class Beautify(object):
+    def __init__(self, data, _type):
+        """
+        美化输出
+        :param data: 数据
+        :param _type: 类型
+        """
+        self.__data = data
+        self.__type = _type
+
+    def beautify(self):
+        """
+        返回美化后的结果
+        :return:
+        """
+        if self.__type in ["json","key_value","form_gb18030"] and self.__data is not None:
+            return "\n{}".format(BeautifyJson(self.__data).beautify())
+        if self.__type in ["xml"] and self.__data is not None:
+            return "{}".format(BeautifyXml(self.__data).beautify())
+        return self.__data
