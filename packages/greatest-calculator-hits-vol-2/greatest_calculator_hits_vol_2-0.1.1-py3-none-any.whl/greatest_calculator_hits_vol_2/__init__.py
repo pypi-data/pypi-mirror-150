89b102c0e637a7b999e4d5fbbb68ebc64c2e25b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:12 +0000

    Add package greatest-calculator-hits-vol-2/0.1.1/greatest_calculator_hits_vol_2-0.1.1-py3-none-any.whl

---
+++ packages/greatest-calculator-hits-vol-2/greatest_calculator_hits_vol_2-0.1.1-py3-none-any.whl/greatest_calculator_hits_vol_2/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.1"
+
+from greatest_calculator_hits_vol_2.add import add
+from greatest_calculator_hits_vol_2.subtract import subtract
+from greatest_calculator_hits_vol_2.multiply import multiply
+from greatest_calculator_hits_vol_2.divide import divide
