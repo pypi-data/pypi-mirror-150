4656de85faf42688a73a3f60652d894648118aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:25 +0000

    Add package kie-server-py/0.1.0/kie_server_py-0.1.0-py3.6.egg

---
+++ packages/kie-server-py/kie_server_py-0.1.0-py3.6.egg/kie_server_py/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for KIE Server Python SDK."""
+
+__author__ = """Aman Agrawal"""
+__email__ = 'amanagrawal04081999@gmail.com'
+__version__ = '0.1.0'
---
+++ packages/kie-server-py/kie_server_py-0.1.0-py3.6.egg/kie_server_py/kie_server_py.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+from requests import Response
+from requests.auth import HTTPBasicAuth
+
+
+class KIEServerBaseAdapter:
+    def __init__(self, host: str, port: str, auth_username: str, auth_password: str):
+        self.host = host
+        self.port = port
+        self.auth_username = auth_username
+        self.auth_password = auth_password
+        self.base_url = self.host + ":" + self.port
+        self.headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+
+    def execute_dmn(
+        self,
+        container_name: str,
+        model_namespace: str,
+        model_name: str,
+        dmn_context: dict,
+        timeout: float = 1,
+    ) -> Response:
+        url = (
+            self.base_url
+            + "/kie-server/services/rest/server/containers/"
+            + container_name
+            + "/dmn"
+        )
+        payload = json.dumps(
+            {
+                "model-namespace": model_namespace,
+                "model-name": model_name,
+                "dmn-context": dmn_context,
+            }
+        )
+        response: Response = requests.post(
+            url=url,
+            headers=self.headers,
+            data=payload,
+            auth=HTTPBasicAuth(self.auth_username, self.auth_password),
+            timeout=timeout,
+        )
+        return response
