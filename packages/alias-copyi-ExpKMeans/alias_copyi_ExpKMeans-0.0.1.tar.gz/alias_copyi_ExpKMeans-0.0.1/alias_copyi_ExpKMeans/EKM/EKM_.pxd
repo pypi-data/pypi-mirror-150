718f207e68a564dc3a753d412d2159d799dc83a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:06 +0000

    Add package alias-copyi-ExpKMeans/0.0.1/alias_copyi_ExpKMeans-0.0.1.tar.gz

---
+++ packages/alias-copyi-ExpKMeans/alias_copyi_ExpKMeans-0.0.1.tar.gz/alias_copyi_ExpKMeans-0.0.1/alias_copyi_ExpKMeans/EKM/EKM_.pxd
@@ -0,0 +1,20 @@
+from libcpp.vector cimport vector
+from libcpp cimport bool
+
+cdef extern from "exp_kmeanspp.cpp":
+    pass
+
+cdef extern from "EKM.cpp":
+    pass
+
+cdef extern from "EKM.h":
+    cdef cppclass EKM:
+
+        vector[vector[int]] Y
+        vector[int] n_iter_
+        vector[double] time_arr
+        vector[int] cal_num_dist
+
+        EKM() except +
+        EKM(vector[vector[double]] &X, int c_true, bool debug) except +
+        void opt(vector[vector[vector[double]]] &Cen, int ITER)
---
+++ packages/alias-copyi-ExpKMeans/alias_copyi_ExpKMeans-0.0.1.tar.gz/alias_copyi_ExpKMeans-0.0.1/alias_copyi_ExpKMeans/EKM/EKM_.pyx
@@ -0,0 +1,31 @@
+cimport numpy as np
+import numpy as np
+np.import_array()
+
+from .EKM_ cimport EKM
+
+cdef class PyEKM:
+    cdef EKM c_EKM
+
+    def __cinit__(self, np.ndarray[double, ndim=2] X, int c_true, bool debug=False):
+        self.c_EKM = EKM(X, c_true, debug)
+
+    def opt(self, Cen, int ITER=300):
+
+        self.c_EKM.opt(Cen, ITER)
+
+    @property
+    def y_pre(self):
+        return np.array(self.c_EKM.Y)
+    
+    @property
+    def n_iter_(self):
+        return np.array(self.c_EKM.n_iter_)
+
+    @property
+    def cal_num_dist(self):
+        return np.array(self.c_EKM.cal_num_dist)
+
+    @property
+    def time_arr(self):
+        return np.array(self.c_EKM.time_arr)
