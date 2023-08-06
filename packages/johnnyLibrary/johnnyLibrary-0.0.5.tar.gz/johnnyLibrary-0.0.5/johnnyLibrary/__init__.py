8e0cdc10e6e5287ce42340eae2b15dac60541177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:37 +0000

    Add package johnnyLibrary/0.0.5/johnnyLibrary-0.0.5.tar.gz

---
+++ packages/johnnyLibrary/johnnyLibrary-0.0.5.tar.gz/johnnyLibrary-0.0.5/johnnyLibrary/__init__.py
@@ -0,0 +1,14 @@
+"""
+from PCpranks import shutdownPC
+from CalIPv4 import kal_IP_GUI
+from CalIPv4 import VLSM_GUI
+
+def shutdown():
+    shutdownPC.main()
+
+def calIPv4():
+    kal_IP_GUI.root.mainloop()
+
+def vlsm():
+    VLSM_GUI.root.mainloop()
+"""
