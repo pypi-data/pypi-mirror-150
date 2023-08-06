60138b48fd7f29cbe8bb1cb4a8112426465c96ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:42 +0000

    Add package arithmos-cipher/3.5/arithmos-cipher-3.5.tar.gz

---
+++ packages/arithmos-cipher/arithmos-cipher-3.5.tar.gz/arithmos-cipher-3.5/arithmos/__init__.py
@@ -0,0 +1,9 @@
+from .encrypt import encrypt
+from .decrypt import decrypt
+from .chain import chain
+
+__title__ = "Arithmos Cipher"
+__version__ = "3.5"
+__authors__ = "LyQuid"
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2022-present LyQuid"
