0ef2378ad5b5473bbc84a9fb2e5f80fc873d86fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:37 +0000

    Add package PPDAS/0.0.0/PPDAS-0.0.0-py3-none-any.whl

---
+++ packages/PPDAS/PPDAS-0.0.0-py3-none-any.whl/PPDAS/convert.py
@@ -0,0 +1,15 @@
+import os, json
+
+class PDAS_Conversion:
+  def convert(arg):
+      if type(arg) == str:
+       if arg in ["true", "True", "1", "Y"]:
+         return True
+       if arg in ["false", "False", "0", "N"]:
+         return False
+      if type(arg) == int:
+        if arg in [0o1, 1]:
+          return True
+        if arg in [0o0, 0]:
+          return False
+        
