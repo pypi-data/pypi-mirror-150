95878d69532a06a0204ee17f9053def7f2dafbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:47 +0000

    Add package hagrid/0.2.41/hagrid-0.2.41.tar.gz

---
+++ packages/hagrid/hagrid-0.2.41.tar.gz/hagrid-0.2.41/setup.py
@@ -0,0 +1,39 @@
+# stdlib
+import platform
+
+# third party
+from hagrid import __version__
+from setuptools import find_packages
+from setuptools import setup
+
+DATA_FILES = {
+    "img": ["hagrid/img/*.png"],
+}
+
+packages = [
+    "ascii_magic",
+    "click",
+    "gitpython",
+    "names",
+    "requests",
+    "rich",
+    "setuptools",
+    "cryptography==36.0.2",
+]
+
+if platform.system().lower() != "windows":
+    packages.extend(["ansible", "ansible-core"])
+
+# Pillow binary wheels for Apple Silicon on Python 3.8 don't seem to work well
+# try using Python 3.9+ for HAGrid on Apple Silicon
+setup(
+    name="hagrid",
+    description="Happy Automation for Grid",
+    version=__version__,
+    author="Andrew Trask <andrew@openmined.org>",
+    packages=find_packages(),
+    package_data=DATA_FILES,
+    install_requires=packages,
+    include_package_data=True,
+    entry_points={"console_scripts": ["hagrid = hagrid.cli:cli"]},
+)
