74e33a0c4d64170018ce5803265873dbf2cac8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:37 +0000

    Add package respect-validation/1.3.0/respect_validation-1.3.0-py3-none-any.whl

---
+++ packages/respect-validation/respect_validation-1.3.0-py3-none-any.whl/respect_validation/__init__.py
@@ -0,0 +1,7 @@
+from respect_validation.Validator import Validator
+from respect_validation.FormValidator import FormValidator
+from respect_validation.Factory import Factory
+
+__all__ = ['Validator', 'Factory', 'FormValidator']
+
+__version__ = '1.3.0'
