4012775da7db8aa21503679b501328ac012740cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:11 +0000

    Add package seqits/0.0.2/seqits-0.0.2.tar.gz

---
+++ packages/seqits/seqits-0.0.2.tar.gz/seqits-0.0.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+from setuptools import setup, find_packages
+import os,re
+from seqits import seqits_version
+
+try:
+    with open(os.path.join(os.path.abspath(os.path.dirname("__file__")),"README.md")) as f:
+        long_description=f.read()
+except:
+    long_description="Seqits"
+
+setup(
+    name="seqits",
+    version=seqits_version,
+    description="Tools for sequences process",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/sfjdx1144/seqits',
+    author="Fujun Sun",
+    author_email="sfjdx1144@live.com",
+    packages=find_packages(),
+    install_requires=['argparse','colorama'],
+    platforms="any",
+    license="GPL-3.0 license",
+    python_requires=">=3.4.0",
+    include_package_data=True
+)
