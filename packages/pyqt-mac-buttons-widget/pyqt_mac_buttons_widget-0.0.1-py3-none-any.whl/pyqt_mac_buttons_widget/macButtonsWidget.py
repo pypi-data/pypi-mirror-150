737dbe11f54de8debce525eb1627b3b6cfe31658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:50 +0000

    Add package pyqt-mac-buttons-widget/0.0.1/pyqt_mac_buttons_widget-0.0.1-py3-none-any.whl

---
+++ packages/pyqt-mac-buttons-widget/pyqt_mac_buttons_widget-0.0.1-py3-none-any.whl/pyqt_mac_buttons_widget/macButtonsWidget.py
@@ -0,0 +1,42 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor
+from pyqt_titlebar_buttons_widget import TitlebarButtonsWidget
+
+
+class MacButtonsWidget(TitlebarButtonsWidget):
+    def __init__(self, hint=Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint):
+        super().__init__(hint)
+        self.__initVal()
+        self.__initUi(hint)
+
+    def __initVal(self):
+        self.__size = 20
+        self.__border_width = self.__size // 20
+        self.__border_radius = self.__size // 2
+        self.__macBtnStyle = ''
+
+    def __initUi(self, hint):
+        self.layout().setSpacing(2)
+
+        btns = [self._closeBtn, self._minimizeBtn, self._maximizeBtn]
+        colors = ['#DD0000', '#AA8800', '#008800']
+        for i in range(len(btns)):
+            btn = btns[i]
+            btn.setFixedSize(self.__size, self.__size)
+            self.setStyleForEachButton(btn, colors[i])
+
+    def setStyleForEachButton(self, btn, color):
+        border_color = QColor(color)
+        border_color_name = border_color.name()
+        background_color_name = border_color.lighter().name()
+
+        self.__macBtnStyle = f'''
+                             QPushButton
+                             {{ 
+                             background-color: {background_color_name};
+                             border: {self.__border_width} solid {border_color_name};
+                             border-radius: {self.__border_radius};
+                             }}
+                             '''
+
+        btn.setStyleSheet(self.__macBtnStyle)
