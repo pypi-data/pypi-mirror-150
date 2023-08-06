490262e2bde8f570f6a25e839d07fe690d66adcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:45 +0000

    Add package metamorph/0.0.6/metamorph-0.0.6.tar.gz

---
+++ packages/metamorph/metamorph-0.0.6.tar.gz/metamorph-0.0.6/README.md
@@ -0,0 +1,39 @@
+# Metamorph
+
+![IMG](/img/img.png)
+
+First line is the input followed by colorized suggestions.
+
+Doc: `metamorph --help`
+
+(Work-in-progress: Doc: https://apn-pucky.github.io/metamorph/index.html )
+
+[![PyPI version][pypi image]][pypi link]  ![downloads](https://img.shields.io/pypi/dm/metamorph.svg) 
+
+| [Stable][doc release]        | [Dev][doc test]           |
+| ------------- |:-------------:|
+| [![workflow][a s image]][a s link]      | [![test][a t image]][a t link]     |
+
+## Versions
+
+### Stable
+
+```sh
+pip install metamorph [--user] [--upgrade]
+```
+
+### Dev
+
+```sh
+pip install --index-url https://test.pypi.org/simple/ metamorph [--user] [--upgrade]
+```
+[doc release]: https://apn-pucky.github.io/metamorph/index.html
+[doc test]: https://apn-pucky.github.io/metamorph/test/index.html
+
+[pypi image]: https://badge.fury.io/py/metamorph.svg
+[pypi link]: https://pypi.org/project/metamorph/
+
+[a s image]: https://github.com/APN-Pucky/metamorph/actions/workflows/release.yml/badge.svg
+[a s link]: https://github.com/APN-Pucky/metamorph/actions/workflows/release.yml
+[a t link]: https://github.com/APN-Pucky/metamorph/actions/workflows/test.yml
+[a t image]: https://github.com/APN-Pucky/metamorph/actions/workflows/test.yml/badge.svg
---
+++ packages/metamorph/metamorph-0.0.6.tar.gz/metamorph-0.0.6/metamorph/main.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+import deep_translator
+from deep_translator import * 
+from termcolor import colored
+import difflib
+import argparse
+import sys
+import pkg_resources as pkg
+
+package = "metamorph"
+
+try:
+    version = pkg.require(package)[0].version
+except pkg.DistributionNotFound:
+    version = "dirty"
+
+
+def get_edits_string(old, new):
+    result = ""
+    codes = difflib.SequenceMatcher(a=old, b=new).get_opcodes()
+    for code in codes:
+        if code[0] == "equal": 
+            result += old[code[1]:code[2]]
+            #result += white(old[code[1]:code[2]])
+        #elif code[0] == "delete":
+            #result += red(old[code[1]:code[2]])
+        elif code[0] == "insert":
+            result += colored(new[code[3]:code[4]],'green')
+        elif code[0] == "replace":
+            #result += red(old[code[1]:code[2]]) 
+            result += colored(new[code[3]:code[4]],'green')
+    return result
+
+def __main__():
+    parser = argparse.ArgumentParser(description='Metamorph-' + str(version) + ' text by passing through several translators.')
+    parser.add_argument("-gs","--goal-start",type=str, help="initial and final language", default="en")
+    parser.add_argument("-g","--goal",type=str, help="final language", default=None)
+    parser.add_argument("-s","--start",type=str, help="initial language", default=None)
+    parser.add_argument("-l","--languages",type=str,nargs='+', help="list of intermediate languages (for more variation choose inherently different languages).", default=["de","fr","es"])
+    parser.add_argument("-t","--translators", type=str,nargs='+',help="list of translators (from deep_translator: GoogleTranslator, PonsTranslator, LingueeTranslator, MyMemoryTranslator, DeeplTranslator, ... )",default=["GoogleTranslator"])
+    parser.add_argument("-i","--iterations", type=int,help="number of translation iterations (start and goal language must be the same for more than one iteration)",default=1)
+    parser.add_argument("-v","--verbose", type=bool,help="print error messages instead of skipping them",default=False)
+    parser.add_argument("-q","--quiet", type=bool,help="suppress error messages",default=False)
+    #parser.add_argument("-c","--cross", type=bool,help="mix results between iterations",default=False)
+    args = parser.parse_args()
+    goal = args.goal_start
+    start = args.goal_start 
+    if args.goal is not None:
+        goal = args.goal
+    if args.start is not None:
+        start= args.start
+    assert (not (args.iterations > 1 and args.goal != args.start))
+    while True:
+        print("Text:")
+        to_translate = input()
+        for lang in args.languages:
+            for st in args.translators:
+                # string translator to class
+                t = getattr(sys.modules[__name__], st)
+                try:
+                    tmp_text = to_translate
+                    for i in range(args.iterations):
+                        tmp_text = t(source=lang, target=goal).translate(t(source=start, target=lang).translate(tmp_text))
+                    print(get_edits_string(to_translate,tmp_text))
+                except deep_translator.exceptions.LanguageNotSupportedException as e:
+                    if not args.quiet:
+                        print(e)
+                except Exception as e:
+                    if args.verbose:
+                        print(e)
+                    pass
+        print()
+
+
+__main__()
+
+
