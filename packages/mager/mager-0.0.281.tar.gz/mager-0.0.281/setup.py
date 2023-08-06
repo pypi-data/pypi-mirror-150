008bad0a5ec25c976e0cbcc1e5db5f42772bd951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:50 +0000

    Add package mager/0.0.281/mager-0.0.281.tar.gz

---
+++ packages/mager/mager-0.0.281.tar.gz/mager-0.0.281/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Developers',
+  'Intended Audience :: Education',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+
+with open('README.txt') as readme, open('requirements.txt') as requirements:
+    README = readme.read()
+    REQUIREMENTS = requirements.read().splitlines()
+
+setup(
+  name='mager',
+  version='0.0.281',
+  description='Gmail sender + Copy type checker + More to come',
+  long_description=README,
+  url='https://github.com/Frank17/Mager',
+  author='Frank Zhang',
+  author_email='frankzhang314159@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords=['assistant', 'helper'],
+  packages=find_packages(),
+  install_requires=REQUIREMENTS
+)
