2acd720fca2c60df382a1a743d34c6166b0b31a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:51 +0000

    Add package faas-cache-dict/0.2.7/faas_cache_dict-0.2.7-py3-none-any.whl

---
+++ packages/faas-cache-dict/faas_cache_dict-0.2.7-py3-none-any.whl/faas_cache_dict/__init__.py
@@ -0,0 +1,10 @@
+import sys
+
+__version__ = '0.2.7'
+
+
+if sys.version_info < (3, 8):
+    raise SystemError('Python 3.8 or newer required.')
+
+from faas_cache_dict.faas_cache_dict import FaaSCacheDict  # noqa
+from faas_cache_dict.file_faas_cache_dict import FileBackedFaaSCache  # noqa
