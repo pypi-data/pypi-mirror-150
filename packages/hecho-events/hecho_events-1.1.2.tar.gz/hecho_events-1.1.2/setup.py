d8aa37da83fd73bf473f042323ae4334c5198d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:42 +0000

    Add package hecho-events/1.1.2/hecho_events-1.1.2.tar.gz

---
+++ packages/hecho-events/hecho_events-1.1.2.tar.gz/hecho_events-1.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name='hecho_events',
+    version='1.1.2',
+    license='MIT License',
+    author='Compasso UOL',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='hecho.teste@outlook.com',
+    keywords='Pacote hecho',
+    description=u'Event Broker Rasa',
+    packages=['hecho'],
+    package_data={'': ['.env']},
+    dependency_links=['https://pypi.org/project/requests'],
+    install_requires=['python-decouple', 'requests>=2.20']
+)
