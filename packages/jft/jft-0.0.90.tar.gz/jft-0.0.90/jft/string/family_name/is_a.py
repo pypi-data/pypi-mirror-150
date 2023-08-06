ef64675db1392f6e488f387510a6edbd43f5426d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:38 +0000

    Add package jft/0.0.90/jft-0.0.90.tar.gz

---
+++ packages/jft/jft-0.0.90.tar.gz/jft-0.0.90/jft/string/family_name/is_a.py
@@ -0,0 +1,16 @@
+from string import ascii_uppercase
+def f(x):
+  if x[:4] == 'den ': return True
+  return (
+    all([
+      isinstance(x, str),
+      not (' ' in x and x[:3] != 'El '),
+      x[0] in ascii_uppercase]) if len(x)
+    else False
+  )
+t = lambda: all([
+  f('Forbes'),
+  f('El Sawah'),
+  f('den Hartog'),
+  not any([f(_) for _ in ['New York', 'apple', '']])
+])
