b53afaf25cc0a9df4e24a757477c25b6400c061d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:57 +0000

    Add package mhexport/0.3/mhexport-0.3.tar.gz

---
+++ packages/mhexport/mhexport-0.3.tar.gz/mhexport-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'mhexport',
+      version = '0.3',
+      description = 'Mass Hunter Raw Data Export to Dataframe',
+      packages = ['mhexport'],
+      zip_safe = False)
