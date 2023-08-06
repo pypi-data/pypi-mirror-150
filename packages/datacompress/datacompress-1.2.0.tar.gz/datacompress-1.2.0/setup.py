abcba855ea9d592dac35d814ac43eb5c8ebfce34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:00 +0000

    Add package datacompress/1.2.0/datacompress-1.2.0.tar.gz

---
+++ packages/datacompress/datacompress-1.2.0.tar.gz/datacompress-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='datacompress',
+    version='1.2.0',    
+    description='Comprimir archivos y descomprimir',
+    url='https://gitlab.com/datacommpress',
+    author='datacompress',
+    author_email='hola@datacompression.click',
+    license='BSD 2-claus',
+    packages=['datacompress'],
+
+    )   
