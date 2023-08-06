44df0b2821d842682546b32144d11d3191c8dadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:12 +0000

    Add package delta-task/0.5.1/delta-task-0.5.1.tar.gz

---
+++ packages/delta-task/delta-task-0.5.1.tar.gz/delta-task-0.5.1/delta/delta_node.py
@@ -0,0 +1,22 @@
+from tempfile import TemporaryFile
+
+import httpx
+
+from . import serialize
+from .core.task import Task
+
+
+class DeltaNode(object):
+    def __init__(self, url: str) -> None:
+        self._url = url
+
+    def create_task(self, task: Task) -> int:
+        url = f"{self._url}/v1/task"
+        with TemporaryFile(mode="w+b") as file:
+            serialize.dump_task(file, task)
+            file.seek(0)
+            resp = httpx.post(url, files={"file": file})
+            resp.raise_for_status()
+            data = resp.json()
+            task_id = data["task_id"]
+            return task_id
