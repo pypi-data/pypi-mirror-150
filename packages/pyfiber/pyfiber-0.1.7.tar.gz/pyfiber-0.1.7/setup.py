ca32bedfadd4653e58c58cec313272ac743f5404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:54 +0000

    Add package pyfiber/0.1.7/pyfiber-0.1.7.tar.gz

---
+++ packages/pyfiber/pyfiber-0.1.7.tar.gz/pyfiber-0.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyfiber',
+    packages=find_packages(),
+    version='0.1.7',
+    description='Fiber photometry and behavioral data analysis tool',
+    author='Matias Ceau',
+    author_email="matias@ceau.net",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    license='GPLv3',
+    url="https://github.com/matias-ceau/pyfiber",
+    install_requires=['numpy','scipy','pandas','pyyaml','datetime','seaborn','matplotlib'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+#   package_dir={'': 'pyfiber'}
+)
