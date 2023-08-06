e4ccedc9df292886ed8251f4f22e069a2dc6b50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:38 +0000

    Add package JadeMath/2.0.13/JadeMath-2.0.13.tar.gz

---
+++ packages/JadeMath/JadeMath-2.0.13.tar.gz/JadeMath-2.0.13/src/JadeMath/math.py
@@ -0,0 +1,28 @@
+def velocity(velocity1, velocity2):
+    vel = velocity2 - velocity1
+    return vel
+
+
+def acceleration(velocity: int, time: int):
+    ac = velocity / time
+    return ac
+
+
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+
+def subtract(num1, num2):
+    sub = num1 - num2
+    return sub
+
+
+def product(num1, num2):
+    product = num1 * num2
+    return product
+
+
+def divide(num1, num2):
+    div = num1 / num2
+    return div
