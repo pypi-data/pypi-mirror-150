8c80022e1cb2cc9faa78bde545bff6ff898ba6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:45 +0000

    Add package leotune/0.0.3/leotune-0.0.3-py3-none-any.whl

---
+++ packages/leotune/leotune-0.0.3-py3-none-any.whl/leotune/data/__init__.py
@@ -0,0 +1,10 @@
+from . import leoneed
+from . import others
+from . import acgnjrock
+#from . import voca_pinosophy
+LRC_SOURCE_MAP = {
+    "leoneed": leoneed,
+    "others": others,
+    "acgnjrock": acgnjrock,
+    #"voca": { "pinosophy": voca_pinosophy },
+    }
