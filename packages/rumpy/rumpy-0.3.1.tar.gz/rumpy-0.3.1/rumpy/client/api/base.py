d74063299191183c35afb8aa2b1c3ecc97dcf69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:45 +0000

    Add package rumpy/0.3.1/rumpy-0.3.1.tar.gz

---
+++ packages/rumpy/rumpy-0.3.1.tar.gz/rumpy-0.3.1/rumpy/client/api/base.py
@@ -0,0 +1,37 @@
+class BaseAPI:
+    def __init__(self, client=None):
+        self._client = client
+
+    def _get(self, url: str, relay={}):
+        return self._client.get(url, relay)
+
+    def _post(self, url: str, relay={}):
+        return self._client.post(url, relay)
+
+    @property
+    def baseurl(self) -> str:
+        return self._client.baseurl
+
+    @property
+    def baseurl_app(self) -> str:
+        return self._client.baseurl_app
+
+    @property
+    def group_id(self):
+        return self._client.group_id
+
+    @property
+    def node(self):
+        return self._client.node
+
+    @property
+    def group(self):
+        return self._client.group
+
+    def _check_group_id(self):
+        if self.group_id == None:
+            raise ValueError("group_id is not set yet.")
+
+    def _check_owner(self):
+        if self.group.pubkey != self.group.owner:
+            raise ValueError("you are not owner.")
---
+++ packages/rumpy/rumpy-0.3.1.tar.gz/rumpy-0.3.1/rumpy/client/api/paidgroup.py
@@ -0,0 +1,55 @@
+from rumpy.client.api.base import BaseAPI
+
+
+class PaidGroup(BaseAPI):
+    def dapp(self):
+        """Get Info of Paidgroup DApp"""
+        resp = self._get("https://prs-bp2.press.one/api/dapps/PaidGroupMvm")
+        if resp.get("success"):
+            return resp.get("data")
+        return resp
+
+    def paidgroup(self):
+        """Get Detail of a Paidgroup"""
+        self._check_group_id()
+        resp = self._get(f"https://prs-bp2.press.one/api/mvm/paidgroup/{self.group_id}")
+        if resp.get("success"):
+            return resp.get("data").get("group")
+        return resp
+
+    def payment(self):
+        """Check Payment"""
+        self._check_group_id()
+        resp = self._get(f"https://prs-bp2.press.one/api/mvm/paidgroup/{self.group_id}/{self.group.eth_addr}")
+        if resp.get("success"):
+            return resp.get("data").get("payment")
+        return resp
+
+    def announce(self, amount, duration):
+        """Announce a Paidgroup"""
+        self._check_group_id()
+        self._check_owner()
+
+        relay = {
+            "group": self.group_id,
+            "owner": self.group.eth_addr,
+            "amount": str(amount),
+            "duration": duration,
+        }
+        resp = self._post("https://prs-bp2.press.one/api/mvm/paidgroup/announce", relay)
+        if resp.get("success"):
+            return resp.get("data")
+        return resp
+
+    def pay(self):
+        """Pay for a Paidgroup"""
+        self._check_group_id()
+
+        relay = {
+            "user": self.group.eth_addr,
+            "group": self.group_id,
+        }
+        resp = self._post("https://prs-bp2.press.one/api/mvm/paidgroup/pay", relay)
+        if resp.get("success"):
+            return resp.get("data")
+        return resp
