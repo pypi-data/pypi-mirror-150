4fd7682c7cd3d7f12db1d3e8f77c834623176afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:31 +0000

    Add package jsw-scrapy/1.0.5/jsw_scrapy-1.0.5-py3-none-any.whl

---
+++ packages/jsw-scrapy/jsw_scrapy-1.0.5-py3-none-any.whl/jsw_scrapy/__init__.py
@@ -0,0 +1,8 @@
+import pkg_resources
+
+version = pkg_resources.get_distribution('jsw-scrapy').version
+__version__ = version
+
+# next pipelines/spiders
+from jsw_scrapy.pipelines.base_pipeline import BasePipeline
+from jsw_scrapy.spiders.base_spider import BaseSpider
