3f7feef897f3a62c5a8fb088410f5fc088a6e534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:35 +0000

    Add package coin-gecko/1.0.0/coin_gecko-1.0.0-py3-none-any.whl

---
+++ packages/coin-gecko/coin_gecko-1.0.0-py3-none-any.whl/coin_gecko/coin.py
@@ -0,0 +1,18 @@
+from bot_studio import *
+dk=bot_studio.new(apiKey="ldc7Ea6A4AbpiUu4Nfbs3zt7ZPH3")
+response1=dk.coingecko(url="https://www.coingecko.com/")
+
+data_list = []
+
+def scraper():
+    for i in response1["body"]:
+        data_list.append(i)
+        response2=dk.coingeckoisidedetail(url=i["coin_link"])
+        for j in response2["body"]:
+            res = response2.get('body', {}).get(j)
+            i[j]=res
+    print(data_list)
+    return data_list
+
+scraper()
+
