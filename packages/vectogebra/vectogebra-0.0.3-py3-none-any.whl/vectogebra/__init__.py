d50ad2f3b2796c8e16ad173e78ec2cbe0f12ab7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:48 +0000

    Add package vectogebra/0.0.3/vectogebra-0.0.3-py3-none-any.whl

---
+++ packages/vectogebra/vectogebra-0.0.3-py3-none-any.whl/vectogebra/__init__.py
@@ -0,0 +1,40 @@
+"""
+    file under vectogebra - python module for vector algebra
+    
+    vectogrbra/__init__.py
+    -------------------------
+    This module contains the 'vector' class.
+    -------------------------------------------------
+    copyright: (C) 2022 Mohammad Maasir
+    license: MIT License (see LICENSE in project's main directory)
+    -------------------------------------------------
+    date created: 9th of May, 2022 (3:09 AM)
+    last modified: 9th of May, 2022
+    -------------------------------------------------
+    contributor(s): Mohammad Maasir
+    -------------------------------------------------
+    github: github.com/maasir554
+
+"""
+
+# importing vector class from vector.py
+# the dot is necessary to avoid name conflict. 
+# from .vector import vector
+
+# from .vector import vector
+
+
+"""
+    to use vector object class, in your file, write: import vectogerbra.vector as v
+    then you can use the vector class in your file
+    example: v1 = v(1,2,3)
+
+    to import the vector class in your file, you can use the following:
+    ## import vectogerbra.vector as v [or any other alias name you want]
+    ## from vectogerbra import vector as v [or any other alias name you want]
+"""
+
+# importing utility functions from utilities.py
+
+# from .utilities import *
+
