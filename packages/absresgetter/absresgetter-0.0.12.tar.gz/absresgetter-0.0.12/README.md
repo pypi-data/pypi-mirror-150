352112bc742ca4379cc788384aaab2baee7f4567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:14 +0000

    Add package absresgetter/0.0.12/absresgetter-0.0.12.tar.gz

---
+++ packages/absresgetter/absresgetter-0.0.12.tar.gz/absresgetter-0.0.12/README.md
@@ -0,0 +1,17 @@
+# absresgetter
+Get absolute resource path of exterior package
+
+## Setup
+`python -m pip install absresgetter`
+
+## Description
+There is only one method. `getabsres(res: str) -> str`
+
+This package find the module in the stack which includes relative resource path like 'ico/sample.png'. 
+
+You can get the absolute path like 'C:/.../sample_exists_directory/ico/sample.png'.
+
+### Example
+```python
+print(getabsres('ico/dark-notepad.svg')) # C:/.../ico_dark_notepad_exists_directory/ico/dark-notepad.svg
+```
---
+++ packages/absresgetter/absresgetter-0.0.12.tar.gz/absresgetter-0.0.12/setup.py
@@ -0,0 +1,22 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='absresgetter',
+    version='0.0.12',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='Get absolute resource path of exterior package',
+    url='https://github.com/yjg30737/absresgetter.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description
+)
