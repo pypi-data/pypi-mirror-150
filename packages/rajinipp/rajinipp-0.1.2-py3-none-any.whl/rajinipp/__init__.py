7786b1da2130ed576cfecc0a87fb7c1a060a75ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:40 +0000

    Add package rajinipp/0.1.2/rajinipp-0.1.2-py3-none-any.whl

---
+++ packages/rajinipp/rajinipp-0.1.2-py3-none-any.whl/rajinipp/__init__.py
@@ -0,0 +1,8 @@
+"""rajini++"""
+
+__version__ = "0.1.2"
+__version_str__ = (
+    f"rajini++ v{__version__}."
+    + "\nrajini++(rajinipp) is a hobby programming language based on the iconic dialogues of super star Rajinikanth."
+    + "\nCreated by Aadhithya Sankar(a.sankar@tum.de)."
+)
---
+++ packages/rajinipp/rajinipp-0.1.2-py3-none-any.whl/rajinipp/lexer.py
@@ -0,0 +1,23 @@
+from munch import Munch
+from rply import LexerGenerator
+
+from .utils import read_yml
+
+
+class Lexer:
+    def __init__(self, tokens: Munch = None) -> None:
+        self._lexer = LexerGenerator()
+        self._tokens = tokens
+
+    def __add_tokens(self):
+        for token in self._tokens:
+            self._lexer.add(token, r"{}".format(self._tokens[token]))
+
+        # * ignore spaces
+        self._lexer.ignore(r"\s+")
+        self._lexer.ignore(r"!!.*\n")
+
+    def get_lexer(self):
+        """builds and returns lexer."""
+        self.__add_tokens()
+        return self._lexer.build()
