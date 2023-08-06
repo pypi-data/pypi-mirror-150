afca57d8ccda1d1f39f255a7d07036c59462b842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:39 +0000

    Add package spatialgeometry/1.0.0/spatialgeometry-1.0.0-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/spatialgeometry/spatialgeometry-1.0.0-cp310-cp310-macosx_10_15_x86_64.whl/spatialgeometry/SceneGroup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+@author: Jesse Haviland
+"""
+
+from numpy import ndarray, eye, zeros, copy as npcopy
+from spatialmath.base import r2q
+from abc import ABC
+from collections import UserList
+
+# from roboticstoolbox.robot.ETS import ETS
+from typing import Type
+
+from spatialgeometry.SceneNode import SceneNode
+
+
+class SceneGroup(SceneNode, UserList):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __getitem__(self, i: int):
+        return self._scene_children[i]
+
+    @property
+    def data(self):
+        return self._scene_children
+
+    def hello(self):
+        print("HEllo")
