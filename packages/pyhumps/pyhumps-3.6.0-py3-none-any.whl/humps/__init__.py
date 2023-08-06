0b6eaa6681b07b5d34bb755c59f3832e9782e3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:23 +0000

    Add package pyhumps/3.6.0/pyhumps-3.6.0-py3-none-any.whl

---
+++ packages/pyhumps/pyhumps-3.6.0-py3-none-any.whl/humps/__init__.py
@@ -0,0 +1,20 @@
+# flake8: noqa
+# noreorder
+"""
+Underscore-to-camelCase converter (and vice versa) for strings and dict keys in Python.
+"""
+__title__ = "pyhumps"
+__version__ = "3.6.0"
+__author__ = "Nick Ficano"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2019 Nick Ficano"
+
+from humps.main import (
+    camelize,
+    decamelize,
+    depascalize,
+    is_camelcase,
+    is_pascalcase,
+    is_snakecase,
+    pascalize,
+)
