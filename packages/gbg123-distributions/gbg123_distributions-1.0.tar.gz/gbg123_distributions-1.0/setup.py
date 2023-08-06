54e373267c41774900f149e97b1eb9e59ad6629d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:30 +0000

    Add package gbg123-distributions/1.0/gbg123_distributions-1.0.tar.gz

---
+++ packages/gbg123-distributions/gbg123_distributions-1.0.tar.gz/gbg123_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gbg123_distributions',
+      version='1.0',
+      description='General, binomial and gaussian distributions',
+      packages=['gbg123_distributions'],
+      author = 'Pierre Liebenberg',
+      author_email = 'pierrelieb@gmail.com',
+      zip_safe=False)
