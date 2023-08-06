bbaf051b4e52bc081917aba71cc5347b3f34ca8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:23 +0000

    Add package arthcalculator/0.1/arthcalculator-0.1-py3-none-any.whl

---
+++ packages/arthcalculator/arthcalculator-0.1-py3-none-any.whl/arthcalculator/__init__.py
@@ -0,0 +1,19 @@
+def add(num1, num2):
+    result = num1 + num2
+    print("The addition results are : {}".format(result))
+
+def sub(num1, num2):
+    result = num1 - num2
+    print("The substraction results are : {}".format(result))
+
+def mul(num1, num2):
+    result = num1 * num2
+    print("The multiplication results are : {}".format(result))
+
+def div(num1, num2):
+    result = num1 / num2
+    print("The division results are : {}".format(result))
+
+def mod(num1, num2):
+    result = num1 % num2
+    print("The modulus results are : {}".format(result))
