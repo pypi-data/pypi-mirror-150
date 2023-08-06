2c44cf9290cd4c057ad04885e68657fdb43fd958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:26:04 +0000

    Add package drf-user-activity-tracker-mongodb/1.0.4/drf-user-activity-tracker-mongodb-1.0.4.tar.gz

---
+++ packages/drf-user-activity-tracker-mongodb/drf-user-activity-tracker-mongodb-1.0.4.tar.gz/drf-user-activity-tracker-mongodb-1.0.4/drf_user_activity_tracker_mongodb/serializers.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from rest_framework import serializers
+
+
+class ActivityLogSerializer(serializers.Serializer):
+    event_name = serializers.SerializerMethodField(read_only=True)
+    created_time = serializers.DateTimeField()
+    client_ip_address = serializers.CharField()
+    url_name = serializers.CharField(max_length=255, write_only=True)
+
+    class Meta:
+        fields = ['event_name', 'client_ip_address', 'created_time']
+
+    def get_event_name(self, obj):
+        event_name = obj.get('url_name', '')
+        if hasattr(settings, 'DRF_ACTIVITY_TRACKER_EVENT_NAME'):
+            if isinstance(settings.DRF_ACTIVITY_TRACKER_EVENT_NAME, dict):
+                event_name = settings.DRF_ACTIVITY_TRACKER_EVENT_NAME.get(obj.get('url_name'), obj.get('url_name'))
+                if not event_name:
+                    event_name = obj.get('url_name', '')
+        return event_name
+
+
+class ActivityLogAdminSerializer(ActivityLogSerializer):
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        fields = ['user_id', 'event_name', 'client_ip_address', 'created_time']
