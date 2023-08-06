0aa4581c2641590b135ace23a70e43d7b0e3822d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:08 +0000

    Add package persuader-technology-automata-auth/0.0.6/persuader-technology-automata-auth-0.0.6.tar.gz

---
+++ packages/persuader-technology-automata-auth/persuader-technology-automata-auth-0.0.6.tar.gz/persuader-technology-automata-auth-0.0.6/coreauth/Authenticator.py
@@ -0,0 +1,33 @@
+from coreutility.collection.dictionary_utility import as_data
+
+from coreauth.exception.AuthenticatorError import AuthenticatorError
+from coreauth.exception.UnableToAuthenticateError import UnableToAuthenticateError
+from coreauth.repository.AuthRepository import AuthRepository
+
+AUTH_URL = 'AUTH_URL'
+
+
+class Authenticator:
+
+    def __init__(self, options):
+        self.auth_url = options[AUTH_URL]
+        self.repository = AuthRepository(options)
+
+    def authenticate(self):
+        pass
+
+    def obtain_auth_value(self, value):
+        auth_info = self.repository.retrieve()
+        if auth_info is None or len(auth_info) == 0:
+            raise UnableToAuthenticateError('AUTH_INFO not available')
+        auth_value = as_data(auth_info, value)
+        if auth_value is None or len(auth_value) == 0:
+            raise UnableToAuthenticateError(f'AUTH_INFO does not contain {value}')
+        return auth_value
+
+    @staticmethod
+    def should_update_url() -> bool:
+        return False
+
+    def update_url(self, url) -> str:
+        raise AuthenticatorError('Update URL needs to be implemented')
