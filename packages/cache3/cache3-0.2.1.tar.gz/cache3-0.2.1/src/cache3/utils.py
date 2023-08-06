0479b716768b34716e19e82a272e74709b1bf593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:17 +0000

    Add package cache3/0.2.1/cache3-0.2.1.tar.gz

---
+++ packages/cache3/cache3-0.2.1.tar.gz/cache3-0.2.1/src/cache3/utils.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# DATE: 2021/7/24
+# Author: clarkmonkey@163.com
+
+from contextlib import AbstractContextManager
+from typing import Any, NoReturn
+
+# Compatible with multiple types.
+empty: Any = object()
+
+
+class NullContext(AbstractContextManager):
+    """ Context manager that does no additional processing.
+
+    Used as a stand-in for a normal context manager, when a particular
+    block of code is only sometimes used with a normal context manager:
+
+    cm = optional_cm if condition else nullcontext()
+    with cm:
+        # Perform operation, using optional_cm if condition is True
+    """
+
+    def __enter__(self) -> Any:
+        """ empty method ... """
+
+    def __exit__(self, *exc: Any) -> NoReturn:
+        """ empty method ... """
+
+
+class cached_property:
+    """ Decorator that converts a method with a single self argument into a
+    property cached on the instance.
+    """
+    name = None
+
+    @staticmethod
+    def func(instance):
+        raise TypeError(
+            'Cannot use cached_property instance without calling '
+            '__set_name__() on it.'
+        )
+
+    def __init__(self, func, name=None):
+        self.real_func = func
+        self.__doc__ = getattr(func, '__doc__')
+
+    def __set_name__(self, owner, name):
+        if self.name is None:
+            self.name = name
+            self.func = self.real_func
+        elif name != self.name:
+            raise TypeError(
+                "Cannot assign the same cached_property to two different names "
+                "(%r and %r)." % (self.name, name)
+            )
+
+    def __get__(self, instance, cls=None):
+        """
+        Call the function and put the return value in instance.__dict__ so that
+        subsequent attribute access on the instance returns the cached value
+        instead of calling cached_property.__get__().
+        """
+        if instance is None:
+            return self
+        res = instance.__dict__[self.name] = self.func(instance)
+        return res
