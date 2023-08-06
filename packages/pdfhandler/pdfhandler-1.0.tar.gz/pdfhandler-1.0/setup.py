436c62e0ade4816ab995c33f085ac443d75667e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:44 +0000

    Add package pdfhandler/1.0/pdfhandler-1.0.tar.gz

---
+++ packages/pdfhandler/pdfhandler-1.0.tar.gz/pdfhandler-1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name='pdfhandler',
+    version=1.0,
+    long_description=Path('README.md').read_text(),
+    pakages= setuptools.find_packages(exclude=['tests','date'])
+
+)
