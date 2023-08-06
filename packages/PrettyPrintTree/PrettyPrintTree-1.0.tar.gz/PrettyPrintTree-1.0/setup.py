7654514633e6ff79083d01dc25deebf2aa848fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:42 +0000

    Add package PrettyPrintTree/1.0/PrettyPrintTree-1.0.tar.gz

---
+++ packages/PrettyPrintTree/PrettyPrintTree-1.0.tar.gz/PrettyPrintTree-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PrettyPrintTree',
+    version='1.0',
+    author="Aharon Sambol",
+    author_email='email@example.com',
+    packages=find_packages('prettyprinttree'),
+    package_dir={'': 'prettyprinttree'},
+    url='https://github.com/AharonSambol/PrettyPrintTree',
+    keywords=['tree', 'pretty', 'print', 'pretty-print', 'display'],
+)
