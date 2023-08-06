1abfd93ae33316b1b2d5ab0c122b3568473cd076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:47:24 +0000

    Add package arcaneshield/100.0.5/arcaneshield-100.0.5.tar.gz

---
+++ packages/arcaneshield/arcaneshield-100.0.5.tar.gz/arcaneshield-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="arcaneshield",
+    version="100.0.5",
+    author="Alon walker",
+    author_email="alonwalker@facebook.com",
+    description="arcaneshield of arcaneshield of arcaneshield",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
