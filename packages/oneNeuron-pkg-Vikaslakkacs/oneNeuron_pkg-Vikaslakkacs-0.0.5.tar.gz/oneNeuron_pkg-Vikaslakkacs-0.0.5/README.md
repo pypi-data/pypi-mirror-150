c47a32eae3542486c2beaca7bc9ea52f230802ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:51 +0000

    Add package oneNeuron-pkg-Vikaslakkacs/0.0.5/oneNeuron_pkg-Vikaslakkacs-0.0.5.tar.gz

---
+++ packages/oneNeuron-pkg-Vikaslakkacs/oneNeuron_pkg-Vikaslakkacs-0.0.5.tar.gz/oneNeuron_pkg-Vikaslakkacs-0.0.5/README.md
@@ -0,0 +1,13 @@
+# oneNeuron Python package
+
+## How to use this package
+'''python
+from oneNeuron.perceptron import Perceptron
+model= Perceptron(eta=0.3, epochs=10)
+'''
+
+## References - 
+
+* [Official python docs for Pypi](https://packaging.python.org/en/latest/tutorials/packaging-projects/)
+* [Github Action file](https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-python#publishing-to-package-registries)
+* [Package link](https://pypi.org/project/oneNeuron-pkg-Vikaslakkacs/)
---
+++ packages/oneNeuron-pkg-Vikaslakkacs/oneNeuron_pkg-Vikaslakkacs-0.0.5.tar.gz/oneNeuron_pkg-Vikaslakkacs-0.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+PKG_NAME="oneNeuron_pkg"
+USER_NAME = "Vikaslakkacs"
+PROJECT_NAME="Python_package_creation"## Which is repo name
+setuptools.setup(
+    name=f"{PKG_NAME}-{USER_NAME}",
+    version="0.0.5",
+    author="Example Author",
+    author_email="vikaslakkacs@gmail.com",
+    description="A small for perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["numpy==1.21.6",
+    "pandas==1.3.5",
+    "joblib==1.1.0"]
+)
