93c59622480c34fe8871b2e505a7cfcd67becd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:34 +0000

    Add package django-pfx/0.2.dev201/django-pfx-0.2.dev201.tar.gz

---
+++ packages/django-pfx/django-pfx-0.2.dev201.tar.gz/django-pfx-0.2.dev201/pfx/pfxcore/shortcuts.py
@@ -0,0 +1,39 @@
+
+import logging
+import re
+
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+
+logger = logging.getLogger(__name__)
+
+
+def f(tmpl, **kwargs):
+    return tmpl.format(**kwargs)
+
+
+def get_object(queryset, **kwargs):
+    from .exceptions import ModelNotFoundAPIError
+    try:
+        return queryset.get(**kwargs)
+    except ObjectDoesNotExist:
+        raise ModelNotFoundAPIError(queryset.model)
+
+
+def get_pk(obj):
+    if isinstance(obj, dict) and 'pk' in obj:
+        return obj['pk']
+    return obj
+
+
+def delete_token_cookie(response):
+    response.delete_cookie(
+            'token', domain=settings.PFX_COOKIE_DOMAIN, samesite='None')
+    return response
+
+
+RE_FALSE = re.compile(r'^(0|false|)$', re.IGNORECASE)
+
+
+def parse_bool(value):
+    return value and not RE_FALSE.match(value)
