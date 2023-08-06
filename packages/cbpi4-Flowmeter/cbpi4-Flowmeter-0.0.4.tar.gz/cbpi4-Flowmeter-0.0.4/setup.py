e5ac5af69b7656bd26a963495dd33584ba54fb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:36 +0000

    Add package cbpi4-Flowmeter/0.0.4/cbpi4-Flowmeter-0.0.4.tar.gz

---
+++ packages/cbpi4-Flowmeter/cbpi4-Flowmeter-0.0.4.tar.gz/cbpi4-Flowmeter-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='cbpi4-Flowmeter',
+      version='0.0.4',
+      description='CraftBeerPi4 Flowsensor / Step Plugin ',
+      author='Alexander Vollkopf',
+      author_email='avollkopf@web.de',
+      url='https://github.com/avollkopf/cbpi4-Flowmeter',
+      include_package_data=True,
+      package_data={
+        # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst', '*.yaml'],
+      'cbpi4-Flowmeter': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-Flowmeter'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+     )
