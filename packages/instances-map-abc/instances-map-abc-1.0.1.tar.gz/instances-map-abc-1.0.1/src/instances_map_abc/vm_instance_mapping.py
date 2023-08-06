e8a6d3dbef8ab7a64d7f9495557e227135fcdc2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:10 +0000

    Add package instances-map-abc/1.0.1/instances-map-abc-1.0.1.tar.gz

---
+++ packages/instances-map-abc/instances-map-abc-1.0.1.tar.gz/instances-map-abc-1.0.1/src/instances_map_abc/vm_instance_mapping.py
@@ -0,0 +1,20 @@
+from collections.abc import Mapping
+from typing import Iterator, TypeVar
+
+from .vm_instance_proxy import RemoteShellProxy, VmInstanceProxy
+
+Instance = TypeVar("Instance", VmInstanceProxy, RemoteShellProxy)
+
+
+class VmInstanceMappingBase(Mapping[str, Instance]):
+    # pytype: disable=bad-return-type
+    def __getitem__(self, name: str) -> Instance:
+        ...
+
+    # pytype: disable=bad-return-type
+    def __iter__(self) -> Iterator:
+        ...
+
+    # pytype: disable=bad-return-type
+    def __len__(self) -> int:
+        ...
