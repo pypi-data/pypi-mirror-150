a9c7e714483afaf6aecf8dadd2a40a59bfc86a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:48 +0000

    Add package vitessce/1.0.7/vitessce-1.0.7.tar.gz

---
+++ packages/vitessce/vitessce-1.0.7.tar.gz/vitessce-1.0.7/vitessce/_version.py
@@ -0,0 +1,7 @@
+# Module version
+py_version_info = (1, 0, 7)
+js_version_info = (0, 1, 12)
+
+# Module version accessible using vitessce.__version__
+__version__ = '%s.%s.%s' % (
+    py_version_info[0], py_version_info[1], py_version_info[2])
