884e570803b9f1032323ed2473e26636fd548b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:22 +0000

    Add package ds-io-utilities/0.0.2/ds-io-utilities-0.0.2.tar.gz

---
+++ packages/ds-io-utilities/ds-io-utilities-0.0.2.tar.gz/ds-io-utilities-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="ds-io-utilities",
+    version="0.0.2",
+    author="Alida research team",
+    author_email="salvatore.cipolla@eng.it",
+    description="Utils for datasets IO operations in Alida.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = [
+        "hdfs>=2.0.0",
+        "bda-service-utils"
+        ],
+)
