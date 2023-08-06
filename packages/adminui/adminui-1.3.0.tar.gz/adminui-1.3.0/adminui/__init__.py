3448d2edb812767d6696d9a390619d28c41d0202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:59 +0000

    Add package adminui/1.3.0/adminui-1.3.0.tar.gz

---
+++ packages/adminui/adminui-1.3.0.tar.gz/adminui-1.3.0/adminui/__init__.py
@@ -0,0 +1,8 @@
+from .form import *
+from .page import *
+from .layout import *
+from .table import *
+from .actions import *
+from .chart import *
+from .app import AdminApp, MenuItem, LoggedInUser, LoginFailed
+
