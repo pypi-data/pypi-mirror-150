c565e383999c8c25831611fc4b40594cf57954d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:32 +0000

    Add package mypy-boto3-forecast/1.23.0.post1/mypy-boto3-forecast-1.23.0.post1.tar.gz

---
+++ packages/mypy-boto3-forecast/mypy-boto3-forecast-1.23.0.post1.tar.gz/mypy-boto3-forecast-1.23.0.post1/mypy_boto3_forecast/__init__.py
@@ -0,0 +1,56 @@
+"""
+Main interface for forecast service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_forecast import (
+        Client,
+        ForecastServiceClient,
+        ListDatasetGroupsPaginator,
+        ListDatasetImportJobsPaginator,
+        ListDatasetsPaginator,
+        ListForecastExportJobsPaginator,
+        ListForecastsPaginator,
+        ListPredictorBacktestExportJobsPaginator,
+        ListPredictorsPaginator,
+    )
+
+    session = Session()
+    client: ForecastServiceClient = session.client("forecast")
+
+    list_dataset_groups_paginator: ListDatasetGroupsPaginator = client.get_paginator("list_dataset_groups")
+    list_dataset_import_jobs_paginator: ListDatasetImportJobsPaginator = client.get_paginator("list_dataset_import_jobs")
+    list_datasets_paginator: ListDatasetsPaginator = client.get_paginator("list_datasets")
+    list_forecast_export_jobs_paginator: ListForecastExportJobsPaginator = client.get_paginator("list_forecast_export_jobs")
+    list_forecasts_paginator: ListForecastsPaginator = client.get_paginator("list_forecasts")
+    list_predictor_backtest_export_jobs_paginator: ListPredictorBacktestExportJobsPaginator = client.get_paginator("list_predictor_backtest_export_jobs")
+    list_predictors_paginator: ListPredictorsPaginator = client.get_paginator("list_predictors")
+    ```
+"""
+from .client import ForecastServiceClient
+from .paginator import (
+    ListDatasetGroupsPaginator,
+    ListDatasetImportJobsPaginator,
+    ListDatasetsPaginator,
+    ListForecastExportJobsPaginator,
+    ListForecastsPaginator,
+    ListPredictorBacktestExportJobsPaginator,
+    ListPredictorsPaginator,
+)
+
+Client = ForecastServiceClient
+
+
+__all__ = (
+    "Client",
+    "ForecastServiceClient",
+    "ListDatasetGroupsPaginator",
+    "ListDatasetImportJobsPaginator",
+    "ListDatasetsPaginator",
+    "ListForecastExportJobsPaginator",
+    "ListForecastsPaginator",
+    "ListPredictorBacktestExportJobsPaginator",
+    "ListPredictorsPaginator",
+)
