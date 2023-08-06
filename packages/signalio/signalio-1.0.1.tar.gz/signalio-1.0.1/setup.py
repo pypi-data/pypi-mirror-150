8c60ed5a916052b82cda4855640f020e528ba890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:40 +0000

    Add package signalio/1.0.1/signalio-1.0.1.tar.gz

---
+++ packages/signalio/signalio-1.0.1.tar.gz/signalio-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="signalio",
+    version="1.0.1",
+    description="A simple Python networking package",
+    url="https://github.com/suchasaltylemon/Signal-IO",
+    author="SuchASaltyLemon",
+    author_email="suchasaltylemon@mailbox.org"
+)
