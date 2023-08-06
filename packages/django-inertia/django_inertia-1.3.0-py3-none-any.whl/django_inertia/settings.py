396d69f63c512e58caba686a8db58c584a5efaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:00 +0000

    Add package django-inertia/1.3.0/django_inertia-1.3.0-py3-none-any.whl

---
+++ packages/django-inertia/django_inertia-1.3.0-py3-none-any.whl/django_inertia/settings.py
@@ -0,0 +1,17 @@
+from django.conf import settings as django_settings
+
+__all__ = ["settings"]
+
+
+class LazySettings:
+    INERTIA_ROOT_VIEW = "app.html"
+    INERTIA_PAGE_CONTEXT = "page__"
+
+    def __getattribute__(self, name):
+        try:
+            return getattr(django_settings, name)
+        except AttributeError:
+            return super().__getattribute__(name)
+
+
+settings = LazySettings()
