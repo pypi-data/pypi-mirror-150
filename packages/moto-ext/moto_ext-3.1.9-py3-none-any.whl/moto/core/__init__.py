062c5a37ffb93ae51c96d1631b6d50aaf05bd5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:09 +0000

    Add package moto-ext/3.1.9/moto_ext-3.1.9-py3-none-any.whl

---
+++ packages/moto-ext/moto_ext-3.1.9-py3-none-any.whl/moto/core/__init__.py
@@ -0,0 +1,8 @@
+from .models import BaseModel, BaseBackend, get_account_id, ACCOUNT_ID  # noqa
+from .models import CloudFormationModel, CloudWatchMetricProvider  # noqa
+from .models import patch_client, patch_resource  # noqa
+from .responses import ActionAuthenticatorMixin
+
+set_initial_no_auth_action_count = (
+    ActionAuthenticatorMixin.set_initial_no_auth_action_count
+)
