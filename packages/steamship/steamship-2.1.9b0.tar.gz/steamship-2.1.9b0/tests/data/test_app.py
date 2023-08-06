9ec6209771d20c475e77ee6273629d395609b7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:00 +0000

    Add package steamship/2.1.9b0/steamship-2.1.9b0.tar.gz

---
+++ packages/steamship/steamship-2.1.9b0.tar.gz/steamship-2.1.9b0/tests/data/test_app.py
@@ -0,0 +1,17 @@
+from steamship import App
+
+__copyright__ = "Steamship"
+__license__ = "MIT"
+
+from tests.utils.client import get_steamship_client
+
+
+def test_app_create():
+    client = get_steamship_client()
+
+    app = App.create(client)
+    assert app.error is None
+    assert app.data is not None
+
+    _ = app.data.delete()
+    assert app.error is None
---
+++ packages/steamship/steamship-2.1.9b0.tar.gz/steamship-2.1.9b0/tests/plugin/test_blockifier.py
@@ -0,0 +1,16 @@
+from steamship.plugin.inputs.raw_data_plugin_input import RawDataPluginInput
+from steamship.plugin.service import PluginRequest
+from tests.demo_apps.plugins.blockifiers.blockifier import DummyBlockifierPlugin
+
+__copyright__ = "Steamship"
+__license__ = "MIT"
+
+
+def test_resp():
+    blockifier = DummyBlockifierPlugin()
+    request = PluginRequest(data=RawDataPluginInput(data="Hi there"))
+    response = blockifier.run(request).data
+    assert response.file is not None
+    assert len(response.file.blocks) == 4
+
+    # Now test as an HTTP Call
