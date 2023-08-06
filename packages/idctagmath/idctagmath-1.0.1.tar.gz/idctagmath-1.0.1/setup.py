26b97a62ccc2a9942f121f95d3b7d0a9a755ae3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:48 +0000

    Add package idctagmath/1.0.1/idctagmath-1.0.1.tar.gz

---
+++ packages/idctagmath/idctagmath-1.0.1.tar.gz/idctagmath-1.0.1/setup.py
@@ -0,0 +1,3 @@
+import setuptools
+from pathlib import Path
+setuptools.setup(name="idctagmath",version='1.0.1', long_description="README.md", packages= setuptools.find_packages(exclude=[".vscode"]))
