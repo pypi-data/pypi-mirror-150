d8d552367757629342b2188dcd43057951bfae6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:37 +0000

    Add package minos-microservice-saga/0.7.0/minos-microservice-saga-0.7.0.tar.gz

---
+++ packages/minos-microservice-saga/minos-microservice-saga-0.7.0.tar.gz/minos-microservice-saga-0.7.0/minos/saga/__init__.py
@@ -0,0 +1,80 @@
+"""The SAGA pattern of the Minos Framework."""
+
+__author__ = "Minos Framework Devs"
+__email__ = "hey@minos.run"
+__version__ = "0.7.0"
+
+from .context import (
+    SagaContext,
+)
+from .definitions import (
+    ConditionalSagaStep,
+    ElseThenAlternative,
+    IfThenAlternative,
+    LocalSagaStep,
+    RemoteSagaStep,
+    Saga,
+    SagaOperation,
+    SagaStep,
+)
+from .exceptions import (
+    AlreadyCommittedException,
+    AlreadyOnSagaException,
+    EmptySagaException,
+    EmptySagaStepException,
+    MultipleElseThenException,
+    MultipleOnErrorException,
+    MultipleOnExecuteException,
+    MultipleOnFailureException,
+    MultipleOnSuccessException,
+    SagaException,
+    SagaExecutionAlreadyExecutedException,
+    SagaExecutionException,
+    SagaExecutionNotFoundException,
+    SagaFailedCommitCallbackException,
+    SagaFailedExecutionException,
+    SagaFailedExecutionStepException,
+    SagaNotCommittedException,
+    SagaNotDefinedException,
+    SagaPausedExecutionStepException,
+    SagaResponseException,
+    SagaRollbackExecutionException,
+    SagaRollbackExecutionStepException,
+    SagaStepException,
+    SagaStepExecutionException,
+    UndefinedOnExecuteException,
+)
+from .executions import (
+    ConditionalSagaStepExecution,
+    DatabaseSagaExecutionRepository,
+    Executor,
+    LocalExecutor,
+    LocalSagaStepExecution,
+    RemoteSagaStepExecution,
+    RequestExecutor,
+    ResponseExecutor,
+    SagaExecution,
+    SagaExecutionDatabaseOperationFactory,
+    SagaExecutionRepository,
+    SagaStatus,
+    SagaStepExecution,
+    SagaStepStatus,
+    TransactionCommitter,
+)
+from .manager import (
+    SagaManager,
+)
+from .messages import (
+    SagaRequest,
+    SagaResponse,
+    SagaResponseStatus,
+)
+from .middleware import (
+    transactional_command,
+)
+from .services import (
+    SagaService,
+)
+from .utils import (
+    get_service_name,
+)
