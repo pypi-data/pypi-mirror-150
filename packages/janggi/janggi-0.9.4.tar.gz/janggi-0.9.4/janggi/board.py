406476ff21b809d45497ec80cd75a2628605d53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:34 +0000

    Add package janggi/0.9.4/janggi-0.9.4.tar.gz

---
+++ packages/janggi/janggi-0.9.4.tar.gz/janggi-0.9.4/janggi/board.py
@@ -0,0 +1,154 @@
+from __future__ import annotations
+import numpy as np
+from typing import List
+
+from .constants import MIN_ROW, MAX_ROW, MIN_COL, MAX_COL, NUM_ROWS, NUM_COLS
+from .piece import Piece
+from .camp import Camp
+from .location import Location
+
+
+class Board:
+    """
+    Simple board class used for the game of Janggi. Contains and handles a single 
+    10x9 two-dimensional list that contains either a Piece object or None.
+    """
+    def __init__(self, board=None):
+        if board:
+            self.__board = board
+        else:
+            self.__board = np.full((NUM_ROWS, NUM_COLS), None)
+
+    def __str__(self) -> str:
+        """Generate colored and structured string representation of the board."""
+        print_str = ""
+        for row in range(-1, MAX_ROW+1):
+            for col in range(-1, MAX_COL+1):
+                if row == -1 and col >= 0:
+                    print_str += " " + str(col)
+                elif row >= 0 and col == -1:
+                    print_str += " " + str(row % 10)
+                elif row >= 0 and col >= 0:
+                    if self.__board[row][col]:
+                        print_str += str(self.__board[row][col])
+                    else:
+                        print_str += "  "
+                else:
+                    print_str += " "
+                print_str += " "
+            print_str += "\n"
+        return print_str        
+
+    def put(self, row: int, col: int, piece: Piece):
+        """
+        Put piece into board at the given (row,col) location.
+        Used row and col as inputs instead of Location to make it easier to generate
+        initial boards in formation.py.
+
+        Args:
+            row (int): Row that the given piece that will placed on.
+            col (int): Column that the given piece that will be placed on.
+            piece (Piece): Piece that will be placed on the board.
+        """
+        self.__board[row][col] = piece
+
+    def merge(self, board: Board):
+        """
+        Merge the given board into self.__board by overwriting.
+
+        Args:
+            board (Board): Input board that will be merged into self.__board.
+        """
+        for row in range(MIN_ROW, MAX_ROW+1):
+            for col in range(MIN_COL, MAX_COL+1):
+                if board.get(row, col):
+                    self.__board[row][col] = board.get(row, col)
+
+    def get(self, row: int, col: int) -> Piece:
+        """
+        Return the piece that is located at the given location of the board.
+
+        Args:
+            row (int): Row of piece.
+            col (int): Column of piece.
+
+        Returns:
+            Piece: Piece located at (row,col) on the board. Can be None.
+        """
+        return self.__board[row][col]
+
+    def remove(self, row: int, col: int):
+        """
+        Remove piece at the given location of the board.
+
+        Args:
+            row (int): Row of the piece to be removed.
+            col (int): Column of the piece to be removed.
+        """
+        self.__board[row][col] = None
+
+    def copy(self) -> Board:
+        """
+        Return a copied Board class
+
+        Returns:
+            Board: Copied version of the board
+        """
+        return Board(self.__board.copy())
+
+    def rotate(self):
+        """Rotate the board 180 degrees and update self.__board."""
+        new_board = [[None for i in range(NUM_ROWS)]
+                     for j in range(NUM_COLS)]
+        for row in range(MIN_ROW, MAX_ROW+1):
+            for col in range(MIN_COL, MAX_COL+1):
+                if self.__board[row][col]:
+                    new_board[NUM_ROWS-row-1][NUM_COLS-col-1] = self.__board[row][col]
+        self.__board = new_board
+
+    def mark_camp(self, camp: Camp):
+        """
+        Mark all pieces on the board with the given camp (CHO or HAN).
+        Used to mark half-boards when setting up initial boards.
+
+        Args:
+            camp (Camp): Camp enum to mark pieces with.
+        """
+        for row in range(MIN_ROW, MAX_ROW+1):
+            for col in range(MIN_COL, MAX_COL+1):
+                if self.__board[row][col]:
+                    self.__board[row][col].camp = camp
+
+    def get_score(self, camp: Camp) -> int:
+        """
+        Return score for the player who's playing the given camp.
+
+        Args:
+            camp (Camp): Camp of the player whose score will be calculated.
+
+        Returns:
+            int: Score of the player who's playing the given camp.
+        """
+        score = 0
+        for row in range(MIN_ROW, MAX_ROW+1):
+            for col in range(MIN_COL, MAX_COL+1):
+                if self.__board[row][col] and self.__board[row][col].camp == camp:
+                    score += self.__board[row][col].value
+        return score
+
+    def get_piece_locations(self, camp: Camp) -> List[Location]:
+        """
+        Get locations of all pieces with the given camp.
+
+        Args:
+            camp (Camp): Camp of the pieces to fetch.
+
+        Returns:
+            List[Location]: List of all locations of the pieces with the given camp.
+        """
+        piece_locations = []
+        for row in range(MIN_ROW, MAX_ROW+1):
+            for col in range(MIN_COL, MAX_COL+1):
+                if self.__board[row][col] and self.__board[row][col].camp == camp:
+                    piece_locations.append(Location(row, col))
+        return piece_locations
---
+++ packages/janggi/janggi-0.9.4.tar.gz/janggi-0.9.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="janggi",
+    version="0.9.4",
+    author="Sungho Cho",
+    author_email="didog9595@gmail.com",
+    description="Python library for Korean chess Janggi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sungho-cho/pyjanggi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["janggi"],
+    python_requires=">=3.6",
+    install_requires=["termcolor==1.1.0"],
+)
