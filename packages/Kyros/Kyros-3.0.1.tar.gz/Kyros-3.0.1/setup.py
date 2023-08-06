d09841d93062a0fe2b72684770dab3eb5a12b4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:44 +0000

    Add package Kyros/3.0.1/Kyros-3.0.1.tar.gz

---
+++ packages/Kyros/Kyros-3.0.1.tar.gz/Kyros-3.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+from pathlib import Path
+
+setup(
+	name="Kyros",
+	packages=find_packages(include=["Kyros"]),
+	version="3.0.1",
+	description="A Class Based Python Library for Fractal Generation",
+	author="@some1and2",
+	license="GNUv3.0",
+	install_requires=["pillow >= 9.1.0", "numba"],
+	setup_requires=[],
+	long_description = ( Path(__file__).parent / "README.md" ).read_text(),
+	long_description_content_type='text/markdown'
+)
