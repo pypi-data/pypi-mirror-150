5fe157fefac48a373320c94af03fb2ac12111991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:31 +0000

    Add package pydit-jceresearch/0.0.3/pydit-jceresearch-0.0.3.tar.gz

---
+++ packages/pydit-jceresearch/pydit-jceresearch-0.0.3.tar.gz/pydit-jceresearch-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pydit', 'pydit.functions']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.1.3,<4.0.0', 'numpy>=1.21.5,<2.0.0', 'pandas>=1.4.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pydit-jceresearch',
+    'version': '0.0.3',
+    'description': 'Data cleansing tools for Internal Auditors',
+    'long_description': None,
+    'author': 'jceresearch',
+    'author_email': 'jceresearch@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
