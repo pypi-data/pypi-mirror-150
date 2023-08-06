cd4c1228ed0aecff145d5a826a0a902fbf72af9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:58 +0000

    Add package clusterysis/1.0.3/clusterysis-1.0.3.tar.gz

---
+++ packages/clusterysis/clusterysis-1.0.3.tar.gz/clusterysis-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='clusterysis',
+    packages=find_packages(include=['clusterysis']),
+    version='1.0.3',
+    description='A library for visualizing clusters.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    #long_description="Clusterysis",
+    author='Gabriel Di Pardi Arruda',
+    install_requires=[
+        'pandas',
+        'plotly',
+        'matplotlib'
+    ],
+    url='https://github.com/gabrieldi95/clusterysis',
+)
