91719a27f6fc050f9a1c84b884e5a6c44c106788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:21 +0000

    Add package fear-and-greed/0.3/fear_and_greed-0.3-py3-none-any.whl

---
+++ packages/fear-and-greed/fear_and_greed-0.3-py3-none-any.whl/fear_and_greed/cnn.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+
+import datetime
+import os.path
+import tempfile
+import typing
+
+import requests
+import requests_cache
+
+requests_cache.install_cache(
+    cache_name=os.path.join(tempfile.gettempdir(), "cnn_cache"),
+    expire_after=datetime.timedelta(minutes=1),
+)
+
+URL = "https://production.dataviz.cnn.io/index/fearandgreed/graphdata"
+
+
+class FearGreedIndex(typing.NamedTuple):
+    value: float
+    description: str
+    last_update: datetime.datetime
+
+
+class Fetcher:
+    """Fetcher gets the HTML contents of CNN's Fear & Greed Index website."""
+
+    def __call__(self) -> dict:
+        headers = {
+            "User-Agent": "Mozilla",
+        }
+        r = requests.get(URL, headers=headers)
+        r.raise_for_status()
+        return r.json()
+
+
+def get(fetcher: Fetcher = None) -> FearGreedIndex:
+    """Returns CNN's Fear & Greed Index."""
+
+    if fetcher is None:
+        fetcher = Fetcher()
+
+    response = fetcher()["fear_and_greed"]
+    return FearGreedIndex(
+        value=response["score"],
+        description=response["rating"],
+        last_update=datetime.datetime.fromisoformat(response["timestamp"]),
+    )
