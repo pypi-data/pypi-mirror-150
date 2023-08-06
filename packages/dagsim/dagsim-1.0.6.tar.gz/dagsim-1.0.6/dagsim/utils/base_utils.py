dcfa829d4930fd237d5cbb84b65971b255a63d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:50 +0000

    Add package dagsim/1.0.6/dagsim-1.0.6.tar.gz

---
+++ packages/dagsim/dagsim-1.0.6.tar.gz/dagsim-1.0.6/dagsim/utils/base_utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def get_args_str(doc_string, n):
+    start = doc_string.find("(")
+    end = doc_string.find(",")
+    while end >= 0 and n > 1:
+        end = doc_string.find(",", end + 1)
+        n -= 1
+    if end == -1:
+        end = doc_string.find(")")
+    return doc_string[start + 1:end]
+
+
+def get_args_list(args_str):
+    pass
+
+
+def get_args_st(doc, n):
+    start = doc.find("(")
+    end = doc.find(")")
+    doc = doc[doc.find("("): doc.find(")")]
+    feq = doc.find("=")
+    print(doc[:feq])
+
+
+if __name__ == "__main__":
+    # doc = "fds(dasd,dasd dasd) dasd sdaftqwe "
+    doc = np.add.__doc__
+    args = get_args_st(doc, 2)
+    print(args)
